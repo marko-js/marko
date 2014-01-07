ff475c6e62b01096712d396c9515d31c8e4992cd
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-07 16:59:30 -0700

    Initial code commit before migration to RaptorJS 3

---
+++ test/raptor-templates-tests.js
@@ -0,0 +1,29 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var path = require('path');
+
+
+describe('raptor-templates' , function() {
+
+    beforeEach(function(done) {
+        for (var k in require.cache) {
+            if (require.cache.hasOwnProperty(k)) {
+                delete require.cache[k];
+            }
+        }
+
+        require('raptor-logging').configureLoggers({
+            'raptor-templates': 'INFO'
+        });
+
+        done();
+    });
+
+    it('should do something', function(done) {
+        done();
+    });
+});
+
