14d49ed17c3747b019ca577b45d77ae6c9869afd
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-06 19:46:45 -0700

    More async test cases

--- test/render-async-tests.js
+++ test/render-async-tests.js
@@ -88,7 +88,7 @@ describe('raptor-templates/rhtml-async' , function() {
         done();
     });
 
-    it('should render a simple with async fragments correctly (1)', function(done) {
+    it('should render a simple template with async fragments correctly (1)', function(done) {
         testRender('test-project/rhtml-templates/async-fragment-ordering.rhtml', {}, done, {
             dataProviders: {
                 'D1': delayedDataProvider(100),
@@ -99,7 +99,7 @@ describe('raptor-templates/rhtml-async' , function() {
         });        
     });
 
-    it('should render a simple with async fragments correctly (2)', function(done) {
+    it('should render a simple template with async fragments correctly (2)', function(done) {
         testRender('test-project/rhtml-templates/async-fragment-ordering.rhtml', {}, done, {
             dataProviders: {
                 'D1': delayedDataProvider(100),
@@ -110,7 +110,7 @@ describe('raptor-templates/rhtml-async' , function() {
         });        
     });
 
-    it('should render a simple with async fragments correctly (3)', function(done) {
+    it('should render a simple template with async fragments correctly (3)', function(done) {
         testRender('test-project/rhtml-templates/async-fragment-ordering.rhtml', {}, done, {
             dataProviders: {
                 'D1': delayedDataProvider(800),
@@ -121,7 +121,7 @@ describe('raptor-templates/rhtml-async' , function() {
         });        
     });
 
-    it('should render a simple with async fragments correctly (4)', function(done) {
+    it('should render a simple template with async fragments correctly (4)', function(done) {
         testRender('test-project/rhtml-templates/async-fragment-ordering.rhtml', {}, done, {
             dataProviders: {
                 'D1': delayedDataProvider(800),
@@ -130,6 +130,78 @@ describe('raptor-templates/rhtml-async' , function() {
                 'D4': delayedDataProvider(100)
             }
         });        
+    });
+
+    it('should render a less simple template with async fragments correctly (1)', function(done) {
+        testRender('test-project/rhtml-templates/async-fragment-ordering2.rhtml', {}, done, {
+            dataProviders: {
+                'D1': delayedDataProvider(100),
+                'D2': delayedDataProvider(300),
+                'D3': delayedDataProvider(200),
+                'D4': delayedDataProvider(800),
+                'D5': delayedDataProvider(900),
+                'D6': delayedDataProvider(100),
+                'D7': delayedDataProvider(50)
+            }
+        });        
+    });
+
+    it('should render a less simple template with async fragments correctly (2)', function(done) {
+        testRender('test-project/rhtml-templates/async-fragment-ordering2.rhtml', {}, done, {
+            dataProviders: {
+                'D1': delayedDataProvider(100),
+                'D2': delayedDataProvider(300),
+                'D3': delayedDataProvider(200),
+                'D4': delayedDataProvider(800),
+                'D5': delayedDataProvider(900),
+                'D6': delayedDataProvider(100),
+                'D7': delayedDataProvider(200)
+            }
+        });        
+    });
+
+    it('should render a less simple template with async fragments correctly (3)', function(done) {
+        testRender('test-project/rhtml-templates/async-fragment-ordering2.rhtml', {}, done, {
+            dataProviders: {
+                'D1': delayedDataProvider(900),
+                'D2': delayedDataProvider(300),
+                'D3': delayedDataProvider(200),
+                'D4': delayedDataProvider(800),
+                'D5': delayedDataProvider(100),
+                'D6': delayedDataProvider(100),
+                'D7': delayedDataProvider(200)
+            }
+        });        
+    });
+
+    it("should allow for using macros inside async fragments", function(done) {
+        testRender('test-project/rhtml-templates/async-fragment-macros.rhtml', {}, done, {
+            dataProviders: {
+                'D1': delayedDataProvider(100)
+            }
+        });  
+    });
+
+    it("should allow for shared and context-specific data providers", function(done) {
+        require('raptor-data-providers').register({
+            'sharedData': function(args, done) {
+                var deferred = require('raptor-promises').defer();
+
+                setTimeout(function() {
+                    deferred.resolve({
+                        name: 'testSharedData'
+                    });
+                }, 100);
+
+                return deferred.promise;
+            }
+        });
+
+        testRender('test-project/rhtml-templates/async-fragment-data-providers.rhtml', {}, done, {
+            dataProviders: {
+                'contextData': delayedDataProvider(100, {name: "testContextData"})
+            }
+        });
     });
 
 });
