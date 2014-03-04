99380d29872243d054b1ae1c2de96e63baf5349e
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-03-03 17:05:50 -0700

    Removed whitespace

--- optimizer-plugin/lib/raptor-optimizer-plugin.js
+++ optimizer-plugin/lib/raptor-optimizer-plugin.js
@@ -1,3 +1,3 @@
 module.exports = function(optimizer, config) {
-    optimizer.dependencies.registerPackageType('rhtml', require('./dependency-rhtml'));   
+    optimizer.dependencies.registerPackageType('rhtml', require('./dependency-rhtml'));
 };
