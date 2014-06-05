9ce5eadce3467b841bf59e23d511bbbc15e7d01d
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-04 22:10:39 -0600

    Expose helpers that work for Dust

--- dust/index.js
+++ dust/index.js
@@ -1,8 +1,11 @@
 var raptorDust = require('raptor-dust');
+var raptorWidgets = require('../');
+var attrs = require('raptor-util/attrs');
 
 exports.registerHelpers = function(dust) {
     raptorDust.registerHelpers({
-            'widget': {
+        'init-widgets': require('../taglib/init-widgets-tag'),
+        'widget': {
             buildInput: function(chunk, context, bodies, params, renderContext) {
                 params.invokeBody = function(widgetDef) {
                     context = context.push({
@@ -16,9 +19,45 @@ exports.registerHelpers = function(dust) {
 
                 return params;
             },
-            render: require('../taglib/widget-tag')
-        },
+            render: function(data, context) {
+                var widgetsContext = raptorWidgets.getWidgetsContext(context);
+                var tagName = data.tagName || 'div';
+                var widgetConfig = data.config;
+                var assignedId = data.assignedId;
+                var modulePath = data.module;
+                var invokeBody = data.invokeBody;
+                var scope = data.scope || context.getAttribute('widget');
 
-        'init-widgets': require('../taglib/init-widgets-tag')
+                console.log('WIDGET: ', modulePath, scope, context._dustContext);
+
+                delete data.config;
+                delete data.tagName;
+                delete data.module;
+                delete data.invokeBody;
+
+                var widgetDef = widgetsContext.beginWidget({
+                    module: modulePath,
+                    id: data.id,
+                    assignedId: assignedId,
+                    config: widgetConfig,
+                    scope: scope
+                });
+
+                data.id = widgetDef.id;
+
+                var widgetAttrs = raptorWidgets.attrs(widgetDef);
+
+                context.write('<' + tagName);
+                context.write(attrs(data));
+                context.write(attrs(widgetAttrs));
+                context.write('>');
+
+                invokeBody(widgetDef);
+
+                context.write('</' + tagName + '>');
+
+                widgetDef.end();
+            }
+        }
     }, dust);
 };
