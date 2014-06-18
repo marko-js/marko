6685c03251a0357b3e0028758b81d2216f06b3ed
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-18 13:59:33 -0600

    Added support for resolving widget module relative to templateName (assume file path)

--- dust/index.js
+++ dust/index.js
@@ -1,12 +1,46 @@
+require('raptor-polyfill/string/startsWith');
+require('raptor-polyfill/string/endsWith');
+
 var raptorDust = require('raptor-dust');
 var raptorWidgets = require('../');
 var attrs = require('raptor-util/attrs');
+var path = require('path');
+var isServer = typeof window === 'undefined';
 
 exports.registerHelpers = function(dust) {
     raptorDust.registerHelpers({
         'init-widgets': require('../taglib/init-widgets-tag'),
         'widget': {
             buildInput: function(chunk, context, bodies, params, renderContext) {
+                var module = params.module;
+
+                if (!module) {
+                    throw new Error('"module" is a required argument for the @widget helper');
+                }
+
+                if (module.startsWith('.')) {
+                    module = path.join(path.dirname(context.templateName), module);
+
+                    var hasExt = module.endsWith('.js');
+
+                    // Hack alert:
+                    // This code is fragile and depends on the module loader being used.
+                    if (isServer) {
+                        // Make sure the extension is included on server since it needs to be a path to a real file
+                        if (!hasExt) {
+                            module += '.js';
+                        }
+                    } else {
+                        // Remove the extension on the client since we know that the CommonJS module loader implemented
+                        // by raptor-modules/client does not keep file extensions.
+                        if (hasExt) {
+                            module = module.slice(0, -3);
+                        }
+                    }
+
+                    params.module = module;
+                }
+
                 params.invokeBody = function(widgetDef) {
                     context = context.push({
                             widget: {
