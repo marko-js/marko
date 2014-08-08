9768afe255e8e4b0b5fba64ffbe48934c5dcf132
Patrick Steele-Idem <pnidem@gmail.com>
2014-08-08 16:35:47 -0600

    Updated cached-fragment-tag based on new raptor-cache module

--- taglibs/caching/cached-fragment-tag.js
+++ taglibs/caching/cached-fragment-tag.js
@@ -1,31 +1,39 @@
 'use strict';
-
-var FRAGMENT_CACHE_CONFIG = {
-    store: 'memory'
-};
-
 var raptorCache;
+var defaultCacheManager;
+var req = require; // Fool the raptor-optimizer
+
 
 module.exports = {
     render: function (input, context) {
         if (raptorCache === undefined) {
             try {
-                raptorCache = require('raptor-cache');
+                raptorCache = req('raptor-cache');
             }
             catch(e) {
                 throw new Error('The "raptor-cache" module should be installed as an application-level dependency when using caching tags');
             }
+
+            defaultCacheManager = raptorCache.createCacheManager({
+                profiles: {
+                    '*': {
+                        'raptor-templates/cached-fragment': {
+                            store: 'memory',
+                            encoding: 'utf8'
+                        }
+                    }
+                }
+            });
         }
         
         var cacheKey = input.cacheKey;
         if (!cacheKey) {
             throw new Error('cache-key is required for <cached-fragment>');
         }
-    
-        // use the default cache manager
-        var cacheManager = raptorCache.getDefaultCacheManager(context);
 
-        var cache = cacheManager.getCacheByName(input.cacheName, FRAGMENT_CACHE_CONFIG);
+        var cacheManager = input.cacheManager || defaultCacheManager;
+
+        var cache = cacheManager.getCache(input.cacheName || 'raptor-templates/cached-fragment');
         
         var asyncContext = context.beginAsync();
         
