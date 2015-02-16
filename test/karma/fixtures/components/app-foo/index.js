39347196e34187c747ad08a91635703e15ac8a29
Patrick Steele-Idem <pnidem@gmail.com>
2015-02-16 14:24:25 -0700

    Fixed code comment

--- test/karma/fixtures/components/app-foo/index.js
+++ test/karma/fixtures/components/app-foo/index.js
@@ -1,4 +1,4 @@
-// Export a render(input, callback) method that can be used
+// Export a render(input) method that can be used
 // to render this UI component on the client or server
 exports.render = require('raptor-renderer')
     .createRenderFunc(require('./renderer'));
