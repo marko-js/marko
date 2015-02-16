b852a9ea690d3bbc02d51e94b6270e9ad2634042
Patrick Steele-Idem <pnidem@gmail.com>
2015-02-16 13:23:22 -0700

    Updated test for compiled output

--- test/test-project/custom-tag.marko.expected.js
+++ test/test-project/custom-tag.marko.expected.js
@@ -2,11 +2,12 @@ exports.create = function(__helpers) {
   var str = __helpers.s,
       empty = __helpers.e,
       notEmpty = __helpers.ne,
-      hello_renderer = require("./hello-renderer"),
-      _tag = __helpers.t;
+      __renderer = __helpers.r,
+      hello_renderer = __renderer(require("./hello-renderer")),
+      __tag = __helpers.t;
 
   return function render(data, out) {
-    _tag(out,
+    __tag(out,
       hello_renderer,
       {
         "name": "World"
