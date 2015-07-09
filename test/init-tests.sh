ed11eeaad34be6c01e3985af9370a1805b9b6ee3
Patrick Steele-Idem <pnidem@gmail.com>
2015-07-09 15:02:13 -0600

    More fixes to init-tests.sh

--- test/init-tests.sh
+++ test/init-tests.sh
@@ -1,4 +1,9 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd ${DIR}
+
 if [ ! -L "./node_modules/marko-widgets" ]; then
+    echo "Creating node_modules"
     mkdir -p node_modules
     ln -s ../../ node_modules/marko-widgets
 fi
