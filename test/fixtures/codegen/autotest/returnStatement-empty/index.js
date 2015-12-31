3ac2e0dacad996b40938cb8e4544907484d0c75f
Patrick Steele-Idem <pnidem@gmail.com>
2015-12-31 15:08:23 -0700

    New test for return statement

---
+++ test/fixtures/codegen/autotest/returnStatement-empty/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(builder) {
+    return builder.functionDeclaration(
+        builder.identifier('foo'),
+        [
+            builder.identifier('bar')
+        ],
+        [
+            builder.returnStatement()
+        ]
+    );
+};
