307ddc1b06b6abe4ef6b8cc83a93a5f30edb06d6
Patrick Steele-Idem <pnidem@gmail.com>
2016-01-18 20:14:38 -0700

    Marko v3: Allow compiler test to have a checkError() function

--- test/compiler-test.js
+++ test/compiler-test.js
@@ -5,14 +5,39 @@ chai.config.includeStack = true;
 var path = require('path');
 var compiler = require('../compiler');
 var autotest = require('./autotest');
+var fs = require('fs');
 
 describe('compiler', function() {
     var autoTestDir = path.join(__dirname, 'fixtures/compiler/autotest');
 
     autotest.scanDir(autoTestDir, function run(dir) {
         var templatePath = path.join(dir, 'template.marko');
-        var compiledSrc = compiler.compileFile(templatePath);
-        return compiledSrc;
+        var mainPath = path.join(dir, 'test.js');
+        var main;
+
+        if (fs.existsSync(mainPath)) {
+            main = require(mainPath);
+        }
+        if (main && main.checkError) {
+            var e;
+
+            try {
+                compiler.compileFile(templatePath);
+            } catch(_e) {
+                e = _e;
+            }
+
+            if (!e) {
+                throw new Error('Error expected');
+            }
+
+            main.checkError(e);
+            return '$PASS$';
+
+        } else {
+            var compiledSrc = compiler.compileFile(templatePath);
+            return compiledSrc;
+        }
     });
 
 });
