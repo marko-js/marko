9515cebe07e88b4bc430ae673970160b5a86b8e7
Patrick Steele-Idem <pnidem@gmail.com>
2016-01-19 10:51:18 -0700

    Marko v3: Minor code cleanup for var-tag.js

--- taglibs/core/var-tag.js
+++ taglibs/core/var-tag.js
@@ -2,9 +2,12 @@ module.exports = function nodeFactory(el, context) {
     var builder = context.builder;
 
     var declarations = el.attributes.map((attr) => {
+        var id = builder.identifier(attr.name);
+        var init = attr.value;
+
         return {
-            id: builder.identifier(attr.name),
-            init: builder.expression(attr.value)
+            id: id,
+            init
         };
     });
 
