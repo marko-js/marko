4c6d4d0661cd63ba93dcfee55014f7a06812ac8b
Patrick Steele-Idem <pnidem@gmail.com>
2016-02-02 15:56:35 -0700

    Marko v3: Do replacement on exit so that all nodes are walked

--- compiler/util/removeEscapeFunctions.js
+++ compiler/util/removeEscapeFunctions.js
@@ -2,7 +2,7 @@ var compiler = require('../');
 
 function removeEscapeFunctions(node) {
     var walker = compiler.createWalker({
-        enter: function(node, parent) {
+        exit: function(node, parent) {
             if (node.type === 'FunctionCall' &&
                 node.callee.type === 'Identifier' &&
                 (node.callee.name === '$noEscapeXml' || node.callee.name === '$escapeXml')) {
