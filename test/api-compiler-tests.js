1e84ec991a9d161f1e5d4fa4351d8a0d2562ec42
Patrick Steele-Idem <pnidem@gmail.com>
2016-06-03 13:56:18 -0600

    Test runner for `api-compiler` tests

---
+++ test/api-compiler-tests.js
@@ -0,0 +1,22 @@
+'use strict';
+require('./patch-module');
+require('marko/node-require').install();
+
+var chai = require('chai');
+chai.config.includeStack = true;
+
+var expect = require('chai').expect;
+var nodePath = require('path');
+require('../compiler');
+var autotest = require('./autotest');
+var marko = require('../');
+var markoCompiler = require('../compiler');
+
+describe('api' , function() {
+    var autoTestDir = nodePath.join(__dirname, 'autotests/api-compiler');
+
+    autotest.scanDir(autoTestDir, function run(dir, helpers, done) {
+        var test = require(nodePath.join(dir, 'test.js'));
+        test.check(marko, markoCompiler, expect, done);
+    });
+});
