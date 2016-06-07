49b86ffad109c700f0bd506bf520aa2ad5032782
Patrick Steele-Idem <pnidem@gmail.com>
2016-06-07 10:39:07 -0600

    Fixed test

--- test/autotests/taglib-lookup/core-attributes/test.js
+++ test/autotests/taglib-lookup/core-attributes/test.js
@@ -4,5 +4,5 @@ exports.check = function(markoCompiler, expect) {
     // console.log('LOOKUP: ', Object.keys(lookup.attributes));
     var ifAttr = lookup.getAttribute('div', 'if');
     expect(ifAttr != null).to.equal(true);
-    expect(ifAttr.type).to.equal('argument');
+    expect(ifAttr.type).to.equal('statement');
 };
