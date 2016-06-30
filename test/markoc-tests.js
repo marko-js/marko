32c820fe8f194975b69648b0adec732740cb7f53
Patrick Steele-Idem <pnidem@gmail.com>
2016-06-30 16:02:26 -0600

    Added tests for markoc

---
+++ test/markoc-tests.js
@@ -0,0 +1,36 @@
+'use strict';
+require('./patch-module');
+require('marko/node-require').install();
+
+var chai = require('chai');
+chai.config.includeStack = true;
+
+var nodePath = require('path');
+require('../compiler');
+var autotest = require('./autotest');
+var markocPath = require.resolve('../bin/markoc');
+var childProcess = require('child_process');
+var fs = require('fs');
+
+describe('markoc' , function() {
+    var autoTestDir = nodePath.join(__dirname, 'autotests/markoc');
+
+    autotest.scanDir(autoTestDir, function run(dir, helpers, done) {
+        var testModule = require(nodePath.join(dir, 'test.js'));
+        helpers.existsSync = function(filename) {
+            return fs.existsSync(nodePath.join(dir, filename));
+        };
+        helpers.spawnSync = function(args, options) {
+            options = options || {};
+            if (!options.cwd) {
+                options.cwd = dir;
+            }
+            return childProcess.spawnSync(markocPath, args, options);
+        };
+
+        helpers.spawnSync(['.', '--clean']);
+
+        testModule.test(helpers);
+        done();
+    });
+});
