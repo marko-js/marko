cd797322adf80f2890015de7b8f62301c2921a0a
Patrick Steele-Idem <pnidem@gmail.com>
2016-06-30 14:55:04 -0600

    Fixes #318 - Make configuration a singleton object shared by all marko instances

---
+++ test/autotests/api-compiler/configure-singleton/test.js
@@ -0,0 +1,14 @@
+exports.check = function(marko, markoCompiler, expect, done) {
+    var configModulePath = require.resolve('../../../../compiler/config');
+    var config = require(configModulePath);
+
+    var globalConfig = global.__MARKO_CONFIG;
+    expect(config).to.equal(globalConfig);
+
+    delete require.cache[configModulePath];
+
+    config = require(configModulePath);
+    expect(config).to.equal(globalConfig);
+
+    done();
+};
---
+++ test/autotests/api/writeToDisk-false-include/test.js
@@ -0,0 +1,28 @@
+var nodePath = require('path');
+var fs = require('fs');
+
+exports.check = function(marko, markoCompiler, expect, done) {
+
+    markoCompiler.configure({
+        writeToDisk: false
+    });
+
+    var templatePath = nodePath.join(__dirname, 'template.marko');
+    var template = marko.load(templatePath);
+    expect(template.renderSync({name: 'Frank'})).to.equal('Hello Frank!');
+
+    markoCompiler.configure({
+        writeToDisk: false
+    });
+
+    var compiledTemplatePath = nodePath.join(__dirname, 'template.marko.js');
+    var compiledIncludeTemplatePath = nodePath.join(__dirname, 'include-target.marko.js');
+
+    expect(fs.existsSync(compiledTemplatePath)).to.equal(false);
+    expect(fs.existsSync(compiledIncludeTemplatePath)).to.equal(false);
+
+    // Revert back to defaults
+    markoCompiler.configure();
+
+    done();
+};
