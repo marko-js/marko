f6137d25e8c4f62d05f8c4d9b1b028a37c8b067c
Patrick Steele-Idem <pnidem@gmail.com>
2016-07-10 11:40:13 -0600

    Added test case for the bodyText property

---
+++ test/autotests/render/bodyText/tags/test-bodyText/code-generator.js
@@ -0,0 +1,10 @@
+function compile(bodyText) {
+    return bodyText.toUpperCase();
+}
+
+module.exports = function generateCode(elNode, codegen) {
+    var builder = codegen.builder;
+    return builder.htmlElement('script', {}, [
+        builder.text(builder.literal(compile(elNode.bodyText)))
+    ]);
+};
