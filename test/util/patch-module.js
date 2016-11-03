5ef2c1ed8c8dcd7616ae17aec4ebc05b20fd3103
Patrick Steele-Idem <pnidem@gmail.com>
2016-11-02 22:17:40 -0600

    Updated patch-module.js to fix tests

--- test/util/patch-module.js
+++ test/util/patch-module.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var nodePath = require('path');
 
 var Module = require('module').Module;
@@ -5,16 +6,21 @@ var oldResolveFilename = Module._resolveFilename;
 
 var rootDir = nodePath.join(__dirname, '../../');
 
+var markoInstalledDir = nodePath.join(rootDir, 'node_modules/marko');
+if (fs.existsSync(markoInstalledDir)) {
+    fs.renameSync(markoInstalledDir, nodePath.join(rootDir, 'node_modules/~marko'));
+}
+
 Module._resolveFilename = function(request, parent, isMain) {
     if (request.charAt(0) !== '.') {
-       var firstSlash = request.indexOf('/');
-       var targetPackageName = firstSlash === -1 ? request : request.substring(0, firstSlash);
+        var firstSlash = request.indexOf('/');
+        var targetPackageName = firstSlash === -1 ? request : request.substring(0, firstSlash);
 
-       if (targetPackageName === 'marko') {
-           request = request.substring('marko'.length);
-           request = rootDir + request;
-       }
-   }
+        if (targetPackageName === 'marko') {
+            request = request.substring('marko'.length);
+            request = rootDir + request;
+        }
+    }
 
     return oldResolveFilename.call(this, request, parent, isMain);
 };
