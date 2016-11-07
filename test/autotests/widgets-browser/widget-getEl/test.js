233ed4d5c3f6ceeb4f8391bc772db68f83e1397f
abiyasa <ilham.suhardi@gmail.com>
2016-11-07 09:58:15 +0100

    Add more tests
    Test to ensure getEl() still working

--- test/autotests/widgets-browser/widget-getEl/test.js
+++ test/autotests/widgets-browser/widget-getEl/test.js
@@ -4,4 +4,5 @@ module.exports = function(helpers) {
     var widget = helpers.mount(require('./index'), {});
     expect(widget.getEl('foo').className).to.equal('foo');
     expect(widget.getEl('bar').className).to.equal('bar');
+    expect(widget.getEl('foo-bar').className).to.equal('foo-bar');
 };
