72ac9d0250795435f3ac0f0f36d5e131dde60f63
Ethan Marmaduke <ethanmarmaduke@outlook.com>
2016-11-13 13:13:45 -0500

    Added 'const' and 'let' tags

---
+++ taglibs/core/const-tag.js
@@ -0,0 +1,30 @@
+var isValidJavaScriptVarName = require('../../compiler/util/isValidJavaScriptVarName');
+
+module.exports = function nodeFactory(el, context) {
+    var builder = context.builder;
+    var hasError = false;
+
+    var declarations = el.attributes.map((attr) => {
+        var varName = attr.name;
+
+        if (!isValidJavaScriptVarName(varName)) {
+            context.addError(el, 'Invalid JavaScript variable name: ' + varName, 'INVALID_VAR_NAME');
+            hasError = true;
+            return;
+        }
+
+        var id = builder.identifier(varName);
+        var init = attr.value;
+
+        return {
+            id: id,
+            init
+        };
+    });
+
+    if (hasError) {
+        return el;
+    }
+
+    return context.builder.vars(declarations, 'const');
+};
