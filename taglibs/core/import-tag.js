62c5a1e5ee226e6dab9c7894a62c38f69591ce17
Scott Davis <sc@ttdavis.com>
2016-11-23 16:58:35 -0500

    move parsing logic out

--- taglibs/core/import-tag.js
+++ taglibs/core/import-tag.js
@@ -1,105 +1,22 @@
-const isValidJavaScriptVarName = require('../../compiler/util/isValidJavaScriptVarName');
-
-function getSpecifiers(importDeclaration) {
-    var match = /^(.+)\bfrom\s*('.*'|".*")$/.exec(importDeclaration) ;
-
-    if(!match) {
-        return { moduleSpecifier: importDeclaration.trim() };
-    }
-
-    return {
-        importSpecifierSet:match[1].trim(),
-        moduleSpecifier:match[2].trim()
-    };
-}
-
-function getImportSpecifierGroups(importSpecifierSet) {
-    var defaultImport = importSpecifierSet;
-    var decomposedImports = /(?:,\s*)?{(.*)}$/.exec(importSpecifierSet) || [];
-
-    if(decomposedImports.length) {
-        defaultImport = defaultImport.replace(decomposedImports[0], '');
-        decomposedImports = decomposedImports[1].split(',').map(specifier => specifier.trim());
-    }
-
-    return {
-        defaultImport:defaultImport,
-        decomposedImports:decomposedImports
-    };
-}
-
-function getVariableName(moduleSpecifier) {
-    var withoutQuotes = /^(?:'|")(.*?)(?:'|")$/.exec(moduleSpecifier)[1] ;
-    var withoutPath = /([^\/\\]+)$/.exec(withoutQuotes)[1];
-    var withoutExtension = withoutPath.replace(/\.[a-z0-9]+$/i, '');
-    return withoutExtension.replace(/[^a-z0-9]+([a-z])/gi, (_, p1) => p1.toUpperCase());
-}
-
-function getNames(importSpecifier) {
-       var names = importSpecifier.split(/\bas\b/);
-
-       if(names.length == 1) {
-            names[1] = names[0];
-       }
-
-       return {
-            exported:names[0].trim(),
-            local:names[1].trim()
-       };
-}
-
-function importToAssignments(importStatement) {
-    var importDeclaration = importStatement.replace(/^import/, '');
-    var specifiers = getSpecifiers(importDeclaration);
-    var importSpecifierSet = specifiers.importSpecifierSet;
-    var moduleSpecifier = specifiers.moduleSpecifier;
-
-    if(!importSpecifierSet) {
-        return ['require('+moduleSpecifier+')'];
-    }
-
-    var importGroups = getImportSpecifierGroups(importSpecifierSet);
-    var rootVariable = getNames(importGroups.defaultImport).local || getVariableName(moduleSpecifier);
-    var specifierList = importGroups.decomposedImports.map(getNames).map(names => {
-        return {
-            name:names.local,
-            value: {
-                object:rootVariable,
-                property:names.exported
-            }
-        };
-    });
-
-    return [{ name:rootVariable, value:'require('+moduleSpecifier+')' }].concat(specifierList) ;
-}
-
-function importToAssignmentsWithTags(importStatement) {
-    var assignments = importToAssignments(importStatement);
-    return assignments.map((assignment, currentIndex) => {
-        if(/^<.*>$/.test(assignment.name)) {
-            var tagName = assignment.name;
-            assignment.name = /^<(.*)>$/.exec(assignment.name)[1];
-            assignment.isTag = true;
-
-            for(var i = currentIndex+1; i < assignments.length; i++) {
-                if(assignments[i].value.object === tagName) {
-                    assignments[i].value.object = assignment.name;
-                }
-            }
-        }
-
-        return assignment;
-    });
-}
+var isValidJavaScriptVarName = require('../../compiler/util/isValidJavaScriptVarName');
 
 var resolveFrom = require('resolve-from');
+var parseImport = require('./util/parseImport');
 
 module.exports = function nodeFactory(el, context) {
     var builder = context.builder;
-    var args = importToAssignmentsWithTags(el.argument);
+    var args = parseImport(el.argument);
+    var hasError = false;
     var vars = {};
 
     args = args.map(arg => {
+        var varName = arg.name;
+        if (!isValidJavaScriptVarName(varName)) {
+            context.addError(el, 'Invalid JavaScript variable name: ' + varName, 'INVALID_VAR_NAME');
+            hasError = true;
+            return;
+        }
+
         if (typeof arg.value === "string") {
             // "require('./foo')"
             var value = builder.parseExpression(arg.value);
@@ -112,21 +29,22 @@ module.exports = function nodeFactory(el, context) {
                 return;
             }
             var result = builder.require(builder.literal(path));
-            vars[arg.name] = result;
-            context.addStaticVar(arg.name, result);
+            vars[varName] = result;
+            context.addStaticVar(varName, result);
             return {
-                id: builder.identifier(arg.name),
+                id: builder.identifier(varName),
                 init: result
             };
         } else {
             // { bar } from "./bar"
-            var prop = vars[arg.value.object];
+            var prop = vars[arg.value.object]; // any way around this?
             if (!prop) {
                 context.addError('Variable not found: ' + arg.value.object);
+                hasError = true;
                 return;
             }
             return {
-                id: builder.identifier(arg.name),
+                id: builder.identifier(varName),
                 init: builder.memberExpression(
                     prop,
                     builder.identifier(arg.value.property)
@@ -135,6 +53,10 @@ module.exports = function nodeFactory(el, context) {
 
         }
     });
+
+    if (hasError) {
+        return el;
+    }
 
     // var args = [ { name: 'bar', value: 'require("./bar")' } ]
     // var exp = {
