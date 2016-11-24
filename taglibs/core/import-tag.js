bc3bf7a47eb18044c2d60f6bc0d752f49bad9401
Scott Davis <sc@ttdavis.com>
2016-11-23 21:03:30 -0500

    switch to code generator

--- taglibs/core/import-tag.js
+++ taglibs/core/import-tag.js
@@ -3,70 +3,46 @@ var isValidJavaScriptVarName = require('../../compiler/util/isValidJavaScriptVar
 var resolveFrom = require('resolve-from');
 var parseImport = require('./util/parseImport');
 
-module.exports = function nodeFactory(el, context) {
-    var builder = context.builder;
+module.exports = function codeGenerator(el, codegen) {
+    var builder = codegen.builder;
     var args = parseImport(el.argument);
-    var hasError = false;
     var vars = {};
 
-    args = args.map(arg => {
+    args.forEach(arg => {
         var varName = arg.name;
+
         if (!isValidJavaScriptVarName(varName)) {
-            context.addError(el, 'Invalid JavaScript variable name: ' + varName, 'INVALID_VAR_NAME');
-            hasError = true;
+            codegen.addError(el, 'Invalid JavaScript variable name: ' + varName, 'INVALID_VAR_NAME');
             return;
         }
 
         if (arg.module) {
-            // "require('./foo')"
-            var dirname = context.dirname;
+            // needs to be require()'d
+            var dirname = codegen.context.dirname;
             var path;
             try {
                 path = resolveFrom(dirname, arg.value);
             } catch(e) {
-                console.log(e);
-                context.addError('File not found: ' + path);
+                codegen.addError('File not found: ' + path);
                 return;
             }
             var result = builder.require(builder.literal(path));
             vars[varName] = result;
-            context.addStaticVar(varName, result);
-            return {
-                id: builder.identifier(varName),
-                init: result
-            };
+
+            codegen.addStaticVar(varName, result);
         } else {
-            // { bar } from "./bar"
-            var prop = vars[arg.value.object]; // any way around this?
+            // ie: { bar } from "./bar"
+            var prop = vars[arg.value.object];
             if (!prop) {
-                context.addError('Variable not found: ' + arg.value.object);
-                hasError = true;
+                codegen.addError('Variable not found: ' + arg.value.object);
                 return;
             }
-            return {
-                id: builder.identifier(varName),
-                init: builder.memberExpression(
-                    prop,
-                    builder.identifier(arg.value.property)
-                )
-            };
-
+            codegen.addStaticVar(varName, builder.memberExpression(
+                prop,
+                builder.identifier(arg.value.property)
+            ));
         }
     });
 
-    if (hasError) {
-        return el;
-    }
-
-    // var args = [ { name: 'bar', value: 'require("./bar")' } ]
-    // var exp = {
-    //     type: 'FunctionCall',
-    //     callee: {
-    //         type: 'Identifier',
-    //         name: 'require' },
-    //     args: [ {
-    //         type: 'Literal',
-    //         value: './bar' } ] };
-
-    return builder.vars(args);
+    return null;
 };
