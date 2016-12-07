4b1c76e50f7c68feac1b661680cb688bf3d9d95a
Michael Rawlings <ml.rawlings@gmail.com>
2016-12-07 10:37:18 -0800

    remove new test from deprecated directory

--- test/autotests/widgets-browser-deprecated/widget-config-onInput/test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-module.exports = function(helpers) {
-    var widget = helpers.mount(require('./index.marko'), {
-        name: 'Frank'
-    });
-
-    expect(widget.onMountCalled).to.eql(true);
-};
