5ec588c291b8b2bcc9c24272b8094529a244c6d1
Patrick Steele-Idem <pnidem@gmail.com>
2017-02-08 23:30:27 -0800

    Fixes #504 - Module build failed: Error: Node does not have a `writeCode` method

---
+++ test/autotests/vdom-compiler/doctype/expected.js
@@ -0,0 +1,12 @@
+var marko_template = module.exports = require("marko/vdom").t(),
+    marko_helpers = require("marko/runtime/vdom/helpers"),
+    marko_createElement = marko_helpers.e,
+    marko_const = marko_helpers.const,
+    marko_const_nextId = marko_const("a51026"),
+    marko_node0 = marko_createElement("div", null, 0, marko_const_nextId());
+
+function render(input, out) {
+  out.n(marko_node0);
+}
+
+marko_template._ = render;
