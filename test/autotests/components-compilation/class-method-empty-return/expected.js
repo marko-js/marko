ed12109fe5a20b2c0c669fe0a9f2c4e7f6b9a637
Patrick Steele-Idem <pnidem@gmail.com>
2017-03-07 13:32:44 -0700

    Fixed test for #612

--- test/autotests/components-compilation/class-method-empty-return/expected.js
+++ test/autotests/components-compilation/class-method-empty-return/expected.js
@@ -1,1 +1,38 @@
-TBD
+"use strict";
+
+var marko_template = module.exports = require("marko/html").t(__filename),
+    marko_component = {
+        test: function(e) {
+          return;
+        }
+      },
+    marko_components = require("marko/components"),
+    marko_registerComponent = marko_components.rc,
+    marko_componentType = marko_registerComponent("/marko-test$1.0.0/autotests/components-compilation/class-method-empty-return/index.marko", function() {
+      return module.exports;
+    }),
+    marko_helpers = require("marko/runtime/html/helpers"),
+    marko_attr = marko_helpers.a;
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.w("<div class=\"test\"" +
+    marko_attr("id", __component.id) +
+    "></div>");
+}
+
+marko_template._ = marko_components.r(render, {
+    type: marko_componentType
+  }, marko_component);
+
+marko_template.Component = marko_components.c(marko_component, marko_template._);
+
+marko_template.meta = {
+    deps: [
+      {
+          type: "require",
+          path: "./"
+        }
+    ]
+  };
