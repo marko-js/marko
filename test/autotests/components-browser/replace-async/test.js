fcb4bf67328267d0d152e5b44f127589322971bd
Patrick Steele-Idem <pnidem@gmail.com>
2017-03-09 20:54:50 -0700

    Added test cases for #615 (passing)

---
+++ test/autotests/components-browser/replace-async/test.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+
+module.exports = function(helpers, done) {
+    var component = helpers.mount(require('./index.marko'));
+    var hello = require('./components/hello');
+
+    var targetEl = component.getEl('target');
+    hello.render({name: 'John'})
+        .then(function(result) {
+            result.replace(targetEl);
+            expect(component.el.firstChild.className).to.equal('hello');
+            expect(component.el.firstChild.innerHTML).to.equal('Hello John');
+            done();
+        })
+        .catch(function(err) {
+            done(err);
+        });
+};
