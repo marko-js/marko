698746ff4dad691b2cec2a0ffdba740c7c1566ed
Patrick Steele-Idem <pnidem@gmail.com>
2017-04-03 16:58:06 -0600

    Fixes #649 - New line always added to textarea and other elements for single line/delimited HTML blocks

--- test/autotests/api/renderToString-callback-async/test.js
+++ test/autotests/api/renderToString-callback-async/test.js
@@ -3,7 +3,7 @@ var nodePath = require('path');
 exports.check = function(marko, markoCompiler, expect, done) {
     var template = marko.load(nodePath.join(__dirname, 'template.marko'));
     template.renderToString({ name: 'John' }, function(err, html) {
-        expect(html).to.equal('Hello John! [async]');
+        expect(html).to.equal('Hello John![async]');
         done();
     });
 
