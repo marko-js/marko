d2b9dfa26eafba16fe72b83fe817c2acf5f052bf
Michael Rawlings <ml.rawlings@gmail.com>
2017-04-05 14:52:11 -0700

    transform layout tags to include tags

--- taglibs/layout/placeholder-tag.js
+++ taglibs/layout/placeholder-tag.js
@@ -1,15 +1,21 @@
-module.exports = function render(input, out) {
-    var contentMap = input.content;
-    var content = contentMap ? contentMap[input.name] : null;
-    if (content) {
-        if (content.value) {
-            out.write(content.value);
-        } else if (content.renderBody) {
-            content.renderBody(out);
-        }
+module.exports = function render(oldNode, context) {
+    context.deprecate('The <layout-placeholder> tag is deprecated. Please use <include> instead. See: https://github.com/marko-js/marko/issues/452');
+    
+    var name = oldNode.getAttributeValue('name');
+    var builder = context.builder;
+    var content = builder.memberExpression('input', name.value);
+
+    var newNode = context.createNodeForEl('include');
+    newNode.addProps({ _target:content });
+
+    if (oldNode.firstChild) {
+        var ifNode = builder.ifStatement(content, [newNode]);
+        var elseNode = builder.elseStatement();
+        oldNode.moveChildrenTo(elseNode);
+
+        oldNode.replaceWith(ifNode);
+        ifNode.insertSiblingAfter(elseNode);
     } else {
-        if (input.renderBody) {
-            input.renderBody(out);
-        }
+        oldNode.replaceWith(newNode);
     }
 };
--- taglibs/layout/put-tag.js
+++ taglibs/layout/put-tag.js
@@ -1,5 +1,18 @@
-module.exports = function render(input, context) {
-    var layout = input.layout;
-    var handlePutTag = layout.handlePutTag;
-    handlePutTag(input);
+module.exports = function render(oldNode, context) {
+    context.deprecate('The <layout-put> tag is deprecated. Please use <include> instead. See: https://github.com/marko-js/marko/issues/452');
+
+    var name = oldNode.getAttributeValue('into').value;
+    var value = oldNode.getAttributeValue('value');
+
+    oldNode.removeAttribute('into');
+    oldNode.removeAttribute('value');
+
+    var newNode = context.createNodeForEl('@'+name, oldNode.getAttributes());
+
+    if (value) {
+        newNode.appendChild(context.builder.text(value));
+    }
+
+    oldNode.moveChildrenTo(newNode);
+    oldNode.replaceWith(newNode);
 };
