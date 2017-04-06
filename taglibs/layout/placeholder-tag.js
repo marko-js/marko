ee80f1d3bb0bfaf5797bfa988ca7ae56ba7cb40e
Michael Rawlings <ml.rawlings@gmail.com>
2017-04-06 10:33:18 -0700

    fix issue with dashed layout names

--- taglibs/layout/placeholder-tag.js
+++ taglibs/layout/placeholder-tag.js
@@ -1,9 +1,17 @@
+var removeDashes = require('../../compiler/util/removeDashes');
+
 module.exports = function render(oldNode, context) {
     context.deprecate('The <layout-placeholder> tag is deprecated. Please use <include> instead. See: https://github.com/marko-js/marko/issues/452');
-    
+
     var name = oldNode.getAttributeValue('name');
     var builder = context.builder;
-    var content = builder.memberExpression('input', name.value);
+    var content;
+
+    if (name.type === 'Literal' && typeof name.value === 'string') {
+        content = builder.memberExpression('input', removeDashes(name.value));
+    } else {
+        content = builder.memberExpression('input', name, true);
+    }
 
     var newNode = context.createNodeForEl('include');
     newNode.addProps({ _target:content });
