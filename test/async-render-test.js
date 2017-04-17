25c779024c266299a799e426dc789a42176cb847
Patrick Steele-Idem <pnidem@gmail.com>
2017-04-17 13:37:59 -0600

    mocha describe should match filename

--- test/async-render-test.js
+++ test/async-render-test.js
@@ -8,7 +8,7 @@ var path = require('path');
 var autotest = require('./autotest');
 var runRenderTest = require('./util/runRenderTest');
 
-describe('async render', function() {
+describe('async-render', function() {
     var autoTestDir = path.join(__dirname, 'autotests/async-render');
 
     autotest.scanDir(
