5ba149f1ad449b2aa2850d4f7ccd1c7ce5ec4ffe
Austin Kelleher <austin.kell47@gmail.com>
2017-07-18 18:13:03 -0400

    Fix key suffix test assertions.

--- test/autotests/components-compilation/key-suffix-with-deprecated-attr-error/test.js
+++ test/autotests/components-compilation/key-suffix-with-deprecated-attr-error/test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
 
 exports.checkError = function(err) {
-    expect(err.toString()).to.contain('The "for-key" tag cannot be used with "for:key". (test/autotests/components-compilation/key-suffix-with-deprecated-attr-error/index.marko:8:4)');
+    expect(err.toString()).to.contain('The "for-key" tag cannot be used with "for:key".');
 };
