4f8979b4d7e91d4e7750d382de7879775913f410
Michael Rawlings <mlrawlings@embarkweb.com>
2017-08-01 15:41:47 -0700

    Fix express tests (#779)
    * delete package-lock.json before installing
    
    * ignore package-lock.json
    
    * don't let npm run in production mode, refactor

--- test/util/express-test.js
+++ test/util/express-test.js
@@ -4,26 +4,42 @@ const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
+const semver = require('semver');
 const testsPath = path.resolve(__dirname, '../autotests/express');
-const filePath = testsPath + '/package.json';
+const nodeModulesPath = path.join(testsPath, 'node_modules');
+const packageLockPath = path.join(testsPath, 'package-lock.json');
+const nodeModulesRegExp = new RegExp(nodeModulesPath);
 
-exports.installExpressVersion = (version) => {
+exports.installExpressVersion = (versionRequest) => {
     let currentPath = process.cwd();
-    let command = 'npm install';
+    let command = 'npm install --no-save';
 
-    if (version) {
-        command += (' express@' + version);
+    if (versionRequest) {
+        command += (' express@' + versionRequest);
     }
 
+    // delete previous node_modules and package-lock
     try {
-        fs.unlinkSync(path.join(testsPath, 'node_modules'));
+        fs.unlinkSync(nodeModulesPath);
+        fs.unlinkSync(packageLockPath);
     } catch(e) {}
-    
+
+    let env = { PATH: process.env.PATH };
+
     process.chdir(testsPath);
-    childProcess.execSync(command);
+    childProcess.execSync(command, { env });
     process.cwd(currentPath);
+
+    // Purge the require cache
+    Object.keys(require.cache)
+        .filter(key => nodeModulesRegExp.test(key))
+        .forEach(key => delete require.cache[key]);
+
+    // This is a test to self validate.  Probably should be a separate unit test.
+    let installedVersion = require(path.join(nodeModulesPath, 'express/package.json')).version;
+
+    if (!semver.satisfies(installedVersion, versionRequest)) {
+        throw new Error(`Installed version of express (${installedVersion}) does not match requested version (${versionRequest}).`);
+    }
 };
 
-exports.getExpressPackageFile = () => {
-    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
-};
