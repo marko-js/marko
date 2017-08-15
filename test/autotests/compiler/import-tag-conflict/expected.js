f37c622e5cb9ddfb884057b1e1c998959363978f
Dylan Piercey <pierceydylan@gmail.com>
2017-08-15 14:34:31 -0700

    Add better interop for importing modules with default exports (#803)

--- test/autotests/compiler/import-tag-conflict/expected.js
+++ test/autotests/compiler/import-tag-conflict/expected.js
@@ -1,10 +1,11 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    asset_module = require("./test1/asset"),
-    test = asset_module.asset,
-    asset_module2 = require("./test2/asset"),
-    asset = asset_module2.asset;
+    module_asset_module = require("./test1/asset"),
+    asset_module = module_asset_module.default || module_asset_module,
+    test = module_asset_module.asset,
+    module_asset_module2 = require("./test2/asset"),
+    asset = module_asset_module2.asset;
 
 function render(input, out) {
   var data = input;
