1f0063870d9076d670223936e6de14e149c4d621
Yoni Medoff <yonimedoff@gmail.com>
2017-09-23 11:28:02 -0700

    add no-arg tests for core tags

---
+++ test/autotests/render/unless-no-arg/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+exports.templateData = {};
+
+exports.checkError = function(err) {
+    expect(err.toString()).to.contain('Invalid <unless> tag. Argument is missing.');
+};
