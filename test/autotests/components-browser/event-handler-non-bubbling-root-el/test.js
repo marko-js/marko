9ff4c905e5bece6802075f899ef4ad973388b013
Patrick Steele-Idem <pnidem@gmail.com>
2017-10-15 15:48:50 -0600

    Added test for non-bubbling event listener on root el

---
+++ test/autotests/components-browser/event-handler-non-bubbling-root-el/test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+module.exports = function(helpers) {
+    var component = helpers.mount(require('./index'), {});
+
+    expect(component.mouseMoveEvent).to.equal(undefined);
+
+    var rootEl = helpers.targetEl.querySelector('div');
+
+	helpers.triggerMouseMove(rootEl);
+
+    expect(component.mouseMoveEvent).to.equal(true);
+};
