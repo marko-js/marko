aed62f503f35c3e572b53240843786dfd8d45715
Dylan Piercey <pierceydylan@gmail.com>
2017-11-20 13:37:30 -0800

    Issue #914 & #920 fix (#927)
    * Resolve issue #920.
    
    * Resolve issue #914
    
    * Improve test.

--- src/morphdom/index.js
+++ src/morphdom/index.js
@@ -105,6 +105,10 @@ function morphdom(
             isPrevText = isText;
         }
 
+        if (endNode === startNode) {
+            return insertAfter(createMarkerComment(), startNode, parentNode);
+        }
+
         return endNode;
     }
 
@@ -120,21 +124,23 @@ function morphdom(
 
         var beforeChild = startNode.previousSibling;
         var afterChild = endNode.nextSibling;
+        var tempChild;
+
+        if (!beforeChild) {
+            tempChild = beforeChild = insertBefore(createMarkerComment(), startNode, parentFromNode);
+        }
 
         morphChildren(parentFromNode, startNode, afterChild, vComponent, component);
 
         endNode = undefined;
 
-        if (beforeChild) {
-            startNode = beforeChild.nextSibling;
-            if (!startNode || startNode === afterChild) {
-                startNode = endNode = insertAfter(createMarkerComment(), beforeChild, parentFromNode);
-            }
-        } else {
-            startNode = parentFromNode.firstChild;
-            if (!startNode) {
-                startNode = endNode = insertAfter(createMarkerComment(), beforeChild, parentFromNode);
-            }
+        startNode = beforeChild.nextSibling;
+        if (!startNode || startNode === afterChild) {
+            startNode = endNode = insertAfter(createMarkerComment(), beforeChild, parentFromNode);
+        }
+
+        if (tempChild) {
+            parentFromNode.removeChild(tempChild);
         }
 
         if (!endNode) {
@@ -154,6 +160,7 @@ function morphdom(
         if (endNode.___markoDetached !== undefined || endNode.___endNode) {
             endNode = insertAfter(createMarkerComment(), endNode, parentFromNode);
         }
+
 
         startNode.___markoComponent = component;
         endNode.___endNode = true;
