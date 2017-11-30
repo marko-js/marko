8b8fc2cdccee82bd5cbaab498534be09fcda7db6
Michael Rawlings <mlrawlings@embarkweb.com>
2017-11-29 18:19:59 -0800

    add jquery to legacy widgets (#937)

--- test/deprecated-components-browser/index.test.js
+++ test/deprecated-components-browser/index.test.js
@@ -8,30 +8,19 @@ if (typeof window !== 'undefined') {
     describe('deprecated-components-browser', function () {
         require('../__util__/autotest').runTests(require('./fixtures/autotests.tests'), function run(testFunc, done) {
             var helpers = new BrowserHelpers();
-
-            require('marko/jquery').patchComponent(window.$);
-            require('marko/ready').patchComponent();
-
-            function cleanup() {
-                delete require('marko/components/Component').prototype.$;
-                delete require('marko/components/Component').prototype.ready;
-            }
-
             try {
                 if (testFunc.length === 1) {
                     testFunc(helpers);
                     helpers._cleanup();
-                    cleanup();
                     done();
                 } else {
                     testFunc(helpers, function (err) {
                         helpers._cleanup();
-                        cleanup();
                         done(err);
                     });
                 }
             } catch (e) {
-                cleanup();
+                helpers._cleanup();
                 throw e;
             }
         });
