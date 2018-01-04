4c952e5abd73d9c161a777230477aac2f80bc08f
Dylan Piercey <pierceydylan@gmail.com>
2018-01-03 17:21:02 -0800

    Fix expected file for tests.

--- test/compiler/fixtures/static-tag-custom-root-element/expected.js
+++ test/compiler/fixtures/static-tag-custom-root-element/expected.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/marko-test$1.0.0/compiler/fixtures/static-tag-custom-tag/template.marko",
+    marko_componentType = "/marko-test$1.0.0/compiler/fixtures/static-tag-custom-root-element/template.marko",
     components_helpers = require("marko/src/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c;
@@ -27,5 +27,5 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/marko-test$1.0.0/compiler/fixtures/static-tag-custom-tag/template.marko"
+    id: "/marko-test$1.0.0/compiler/fixtures/static-tag-custom-root-element/template.marko"
   };
