d484de9f14416243a7458d84e6372eb83e7c8903
Dylan Piercey <pierceydylan@gmail.com>
2018-02-06 15:59:42 -0800

    Fixes top level static for custom tags. (#959)
    * Fixes top level static for custom tags.
    
    * Switch static tag from code-generator to transformer to increase it's priority.
    
    * Rename test.
    
    * Fix expected file for tests.

---
+++ test/compiler/fixtures/static-tag-custom-root-element/components/wrapper/transformer.js
@@ -0,0 +1,13 @@
+module.exports = function (el, context) {
+  var builder = context.builder;
+  var rootNode = builder.containerNode(function () {
+    return context.createNodeForEl({
+      tagName: "div",
+      body: rootNode.body
+    });
+  });
+
+  context.root.moveChildrenTo(rootNode);
+  context.root.appendChild(rootNode);
+  el.detach();
+}
