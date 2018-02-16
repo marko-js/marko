8d8cc80b3b803d9b609bc855daf6c94ed5d6bbf7
Scott Davis <scodavis@ebay.com>
2018-02-15 19:36:47 -0500

    add custom event tests

---
+++ test/components-browser/fixtures/custom-events-declarative-multiple-once/test.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+
+module.exports = function (helpers) {
+    var component = helpers.mount(require('./index'), {});
+
+    component.getComponent('bar').emitTestEvent1();
+    component.getComponent('bar').emitTestEvent1();
+
+    expect(component.testEvent1Fired).to.equal(1);
+
+    component.getComponent('bar').emitTestEvent2();
+    component.getComponent('bar').emitTestEvent2();
+
+    expect(component.testEvent2Fired).to.equal(1);
+};
---
+++ test/components-browser/fixtures/custom-events-declaritive-once/test.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+
+module.exports = function (helpers) {
+    var component = helpers.mount(require('./index'), {});
+
+    var numTestEventFired = 0;
+
+    component.handleTestEvent1 = function (a, b) {
+        expect(a).to.equal('a');
+        expect(b).to.equal('b');
+        numTestEventFired++;
+    };
+
+    component.getComponent('bar').emitTestEvent();
+    component.getComponent('bar').emitTestEvent();
+
+    expect(numTestEventFired).to.equal(1);
+};
