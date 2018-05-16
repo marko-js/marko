048a563a868f5f85a93aab94d56bf33283ea8f3d
Dylan Piercey <dpiercey@ebay.com>
2018-05-16 09:53:13 -0700

    Add failing test cases from branches.

---
+++ test/components-pages/fixtures/preserve-no-update-for-loop/tests.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var expect = require("chai").expect;
+
+describe(path.basename(__dirname), function() {
+    it.fails("should update correctly", function() {
+        var component = window.component;
+        var $el = component.getEl("root");
+
+        expect($el.innerHTML).to.eql("");
+
+        component.append("a");
+        component.update();
+        expect($el.innerHTML).to.eql('<li id="a"><span>a</span></li>');
+
+        component.append("b");
+        component.update();
+        expect($el.innerHTML).to.eql(
+            '<li id="a"><span>a</span></li><li id="b"><span>b</span></li>'
+        );
+    }).details =
+        "Issue #952";
+});
