3732d24d3e3205fe6fc13879a8405a15b2725570
Michael Rawlings <mirawlings@ebay.com>
2018-07-14 10:23:09 -0700

    fixes #1022 - transcluded keys dont match, stateful component not preserved

--- test/components-browser/fixtures/transclusion-rerender-stateful/test.js
+++ test/components-browser/fixtures/transclusion-rerender-stateful/test.js
@@ -5,17 +5,15 @@ module.exports = function(helpers) {
     var container = root.getComponent("container");
     var counter = root.getComponent("counter");
 
-    expect(document.body.textContent).to.equal("0");
+    expect(helpers.targetEl.textContent).to.equal("0");
 
     counter.increment();
     counter.update();
 
-    expect(document.body.textContent).to.equal("1");
+    expect(helpers.targetEl.textContent).to.equal("1");
 
     container.forceUpdate();
     container.update();
 
-    expect(document.body.textContent).to.equal("1");
+    expect(helpers.targetEl.textContent).to.equal("1");
 };
-
-module.exports.fails = "issue #1022";
