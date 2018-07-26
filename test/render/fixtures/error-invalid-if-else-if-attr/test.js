2a9e9a1dcae2d5765f1690e135f472af0fc92406
Dylan Piercey <dpiercey@ebay.com>
2018-07-26 10:16:20 -0700

    Remove extra parenthesis from compiled javascript.

--- test/render/fixtures/error-invalid-if-else-if-attr/test.js
+++ test/render/fixtures/error-invalid-if-else-if-attr/test.js
@@ -4,5 +4,5 @@ exports.templateData = {};
 
 exports.checkError = function(e) {
     var message = e.toString();
-    expect(message).to.contain("Unexpected identifier: (true sds)");
+    expect(message).to.contain("Unexpected identifier: true sds");
 };
