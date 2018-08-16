1740ac7c24475cb914d3416df6d58e05fd532486
Michael Rawlings <mirawlings@ebay.com>
2018-08-16 09:45:02 -0700

    add re-rendering the intermediate container to one of the new tests

--- test/components-browser/fixtures/transclusion-repeated-nested/test.js
+++ test/components-browser/fixtures/transclusion-repeated-nested/test.js
@@ -3,6 +3,7 @@ var expect = require("chai").expect;
 module.exports = function(helpers) {
     var root = helpers.mount(require.resolve("./index"));
     var counters = root.getComponents("counter");
+    var containers = root.getComponents("container");
 
     expect(helpers.targetEl.textContent).to.equal("000");
 
@@ -17,4 +18,16 @@ module.exports = function(helpers) {
     counters[2].update();
 
     expect(helpers.targetEl.textContent).to.equal("123");
+
+    containers[1].forceUpdate();
+    containers[1].update();
+    containers[2].forceUpdate();
+    containers[2].update();
+
+    expect(helpers.targetEl.textContent).to.equal("123");
+
+    counters[2].increment();
+    counters[2].update();
+
+    expect(helpers.targetEl.textContent).to.equal("124");
 };
