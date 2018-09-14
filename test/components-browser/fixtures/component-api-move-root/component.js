b0d91c6da9a8ba04f51fe08630a5b03064789595
Michael Rawlings <mirawlings@ebay.com>
2018-09-14 12:56:39 -0700

    update test to ensure the component is working after move

--- test/components-browser/fixtures/component-api-move-root/component.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    onMount: function() {}
-};
--- test/components-browser/fixtures/component-api-move-root/test.js
+++ test/components-browser/fixtures/component-api-move-root/test.js
@@ -4,6 +4,11 @@ module.exports = function(helpers) {
     var component = helpers.mount(require.resolve("./index"), {});
     var newTarget = document.createElement("div");
     document.body.appendChild(newTarget);
+    component.increment();
+    component.update();
     component.appendTo(newTarget);
-    expect(newTarget.innerHTML).to.equal("Hello World");
+    expect(newTarget.innerHTML).to.equal("Hello World 1");
+    component.increment();
+    component.update();
+    expect(newTarget.innerHTML).to.equal("Hello World 2");
 };
