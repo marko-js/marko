ca2cd01821679f1107320ecaff5076741890ea6c
Dylan Piercey <dpiercey@ebay.com>
2018-09-27 14:35:41 -0700

    Add failing test cases for repeated at tag hydration with preserved content

---
+++ test/components-pages/fixtures/preserve-looped-attribute-tag/tests.js
@@ -0,0 +1,10 @@
+var path = require("path");
+var expect = require("chai").expect;
+
+describe(path.basename(__dirname), function() {
+    it.fails("should update correctly", function() {
+        var $el = document.getElementById("root");
+        expect($el.innerHTML).to.eql("<ul><li>A</li><li>B</li><li>C</li></ul>");
+    }).details =
+        "issue #1134";
+});
