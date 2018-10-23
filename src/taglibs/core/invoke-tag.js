9c067fd52c2f27084a4e7b5d0643aae11ae24653
timlauter <tlauter@ebay.com>
2018-10-23 17:09:27 -0400

    started invoke transformer

--- src/taglibs/core/invoke-tag.js
+++ src/taglibs/core/invoke-tag.js
@@ -1,27 +1,30 @@
-module.exports = function codeGenerator(elNode, codegen) {
-    var context = codegen.context;
+module.exports = function codeGenerator(elNode, context) {
+    const functionAttr = elNode.attributes[0];
+    const arg = functionAttr.argument;
+    const functionName = functionAttr.name;
+    const functionCallExpression = functionName + "(" + arg + ")";
+    const newNode = context.builder.scriptlet({
+        value: functionCallExpression
+    });
+
     context.deprecate(
         'The "<invoke>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
     );
 
-    var functionAttr = elNode.attributes[0];
     if (!functionAttr) {
-        codegen.addError(
+        context.addError(
             'Invalid <invoke> tag. Missing function attribute. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
-    var arg = functionAttr.argument;
-
     if (arg === undefined) {
-        codegen.addError(
+        context.addError(
             'Invalid <invoke> tag. Missing function arguments. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
-    var functionName = functionAttr.name;
-    var functionCallExpression = functionName + "(" + arg + ")";
-    return codegen.builder.parseExpression(functionCallExpression);
+    elNode.insertSiblingBefore(newNode);
+    elNode.detach();
 };
