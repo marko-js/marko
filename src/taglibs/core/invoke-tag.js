ca4f6420c1fc9ad3cd81faeb77ed9f4d630f2722
timlauter <tlauter@ebay.com>
2018-10-25 12:34:53 -0400

    updated removed try catch to resolve var-scoped2 error

--- src/taglibs/core/invoke-tag.js
+++ src/taglibs/core/invoke-tag.js
@@ -1,30 +1,57 @@
-module.exports = function codeGenerator(elNode, context) {
-    const functionAttr = elNode.attributes[0];
-    const arg = functionAttr.argument;
-    const functionName = functionAttr.name;
-    const functionCallExpression = functionName + "(" + arg + ")";
-    const newNode = context.builder.scriptlet({
-        value: functionCallExpression
-    });
+// module.exports = function codeGenerator(elNode, context) {
+//     const functionAttr = elNode.attributes[0];
+//     const arg = functionAttr.argument;
+//     const functionName = functionAttr.name;
+//     const functionCallExpression = functionName + "(" + arg + ")";
+//     const newNode = context.builder.scriptlet({
+//         value: functionCallExpression
+//     });
 
+//     context.deprecate(
+//         'The "<invoke>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
+//     );
+
+//     if (!functionAttr) {
+//         context.addError(
+//             'Invalid <invoke> tag. Missing function attribute. Expected: <invoke console.log("Hello World")'
+//         );
+//         return;
+//     }
+
+//     if (arg === undefined) {
+//         context.addError(
+//             'Invalid <invoke> tag. Missing function arguments. Expected: <invoke console.log("Hello World")'
+//         );
+//         return;
+//     }
+
+//     elNode.insertSiblingBefore(newNode);
+//     elNode.detach();
+// };
+module.exports = function codeGenerator(elNode, codegen) {
+    var context = codegen.context;
     context.deprecate(
         'The "<invoke>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
     );
 
+    var functionAttr = elNode.attributes[0];
     if (!functionAttr) {
-        context.addError(
+        codegen.addError(
             'Invalid <invoke> tag. Missing function attribute. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
+    var arg = functionAttr.argument;
+
     if (arg === undefined) {
-        context.addError(
+        codegen.addError(
             'Invalid <invoke> tag. Missing function arguments. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
-    elNode.insertSiblingBefore(newNode);
-    elNode.detach();
+    var functionName = functionAttr.name;
+    var functionCallExpression = functionName + "(" + arg + ")";
+    return codegen.builder.parseExpression(functionCallExpression);
 };
--- src/taglibs/core/var-tag.js
+++ src/taglibs/core/var-tag.js
@@ -2,7 +2,6 @@ var isValidJavaScriptVarName = require("../../compiler/util/isValidJavaScriptVar
 
 module.exports = function nodeFactory(elNode, context) {
     const builder = context.builder;
-    let vars = undefined;
     let newNode = null;
     let hasError = false;
 
@@ -10,15 +9,15 @@ module.exports = function nodeFactory(elNode, context) {
         'The "<var>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
     );
 
-    try {
-        vars = context.builder.parseStatement(elNode.tagString);
-    } catch (e) {
-        /* ignore error */
-    }
+    // try {
+    //     vars = context.builder.parseStatement(elNode.tagString);
+    // } catch (e) {
+    //     /* ignore error */
+    // }
 
-    if (vars) {
-        return vars;
-    }
+    // if (vars) {
+    //     return vars;
+    // }
 
     elNode.attributes.map(attr => {
         if (!isValidJavaScriptVarName(attr.name)) {
@@ -31,7 +30,7 @@ module.exports = function nodeFactory(elNode, context) {
         }
 
         newNode = builder.scriptlet({
-            value: "var " + attr.name + "=" + attr.rawValue
+            value: "var " + attr.name + "=" + attr.rawValue + ";"
         });
 
         elNode.insertSiblingBefore(newNode);
@@ -41,5 +40,60 @@ module.exports = function nodeFactory(elNode, context) {
         return elNode;
     }
 
+    elNode.body.forEach(node => {
+        elNode.insertSiblingBefore(node);
+    });
+
     elNode.detach();
 };
+// var isValidJavaScriptVarName = require("../../compiler/util/isValidJavaScriptVarName");
+
+// module.exports = function nodeFactory(el, context) {
+//     context.deprecate(
+//         'The "<var>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
+//     );
+
+//     var vars;
+
+//     try {
+//         vars = context.builder.parseStatement(el.tagString);
+//     } catch (e) {
+//         /* ignore error */
+//     }
+
+//     if (vars) {
+//         return vars;
+//     }
+
+//     var builder = context.builder;
+//     var hasError = false;
+
+//     var declarations = el.attributes.map(attr => {
+//         var varName = attr.name;
+
+//         if (!isValidJavaScriptVarName(varName)) {
+//             context.addError(
+//                 "Invalid JavaScript variable name: " + varName,
+//                 "INVALID_VAR_NAME"
+//             );
+//             hasError = true;
+//             return;
+//         }
+
+//         var id = builder.identifier(varName);
+//         var init = attr.value;
+
+//         return {
+//             id: id,
+//             init
+//         };
+//     });
+
+//     if (hasError) {
+//         return el;
+//     }
+
+//     var x = context.builder.vars(declarations);
+
+//     return x
+// };
