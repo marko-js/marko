3e6b954584607c856cd8dc6f4b611e02f3412e65
timlauter <tlauter@ebay.com>
2018-11-26 12:23:21 -0500

    [MARKO-14] - <invoke> Migration Transform (#1165)
    Convert `<invoke>` tag to translate to modern Marko features (including imperative rendering to dynamic tag)

--- src/taglibs/core/invoke-tag.js
+++ src/taglibs/core/invoke-tag.js
@@ -1,27 +1,89 @@
-module.exports = function codeGenerator(elNode, codegen) {
-    var context = codegen.context;
+module.exports = function codeGenerator(elNode, context) {
+    const functionAttr = elNode.attributes[0];
+    const attrs = elNode.attributes;
+    const args = context.builder.parseJavaScriptArgs(functionAttr.argument);
+    const argsLength = args.length;
+    const functionName = functionAttr.name;
+
+    let outIsFirstIndex = false;
+    let argsContainsOut = false;
+    let functionCallExpression = null;
+    let newNode = null;
+
+    args.map((arg, i) => {
+        if (arg.name === "out") {
+            if (i === 0) {
+                outIsFirstIndex = true;
+            }
+
+            argsContainsOut = true;
+        }
+    });
+
+    // Removes HtmlAtrribute
+    attrs.splice(0, 1);
+
+    if (
+        (functionName === "data.renderBody" ||
+            functionName === "input.renderBody") &&
+        argsContainsOut &&
+        outIsFirstIndex
+    ) {
+        // Handles cases for the following:
+        // 1. <invoke data.renderBody(out) w-id="barTest"/> --> <${data.renderBody} w-id="barTest"/>
+
+        attrs.unshift({
+            value: args[0],
+            spread: true
+        });
+
+        newNode = context.createNodeForEl(
+            context.builder.parseExpression(functionName),
+            attrs
+        );
+    } else if (argsLength > 1 && argsContainsOut && !outIsFirstIndex) {
+        // Handles cases for the following:
+        // 1. <invoke data.barRenderer({}, out) w-id="barTest"/> --> <${{ render:data.barRenderer }} ...{} w-id="barTest"/>
+        // 2. <invoke data.template.render({}, out) w-id="barTest"/> --> <${{ render: data.template.render }} ...{} w-id="barTest"/>
+        // 3. <invoke data.template.renderer({}, out) w-id="barTest"/> --> <${{ render: data.template.renderer }} ...{} w-id="barTest"/>
+
+        attrs.unshift({
+            value: args[0],
+            spread: true
+        });
+
+        newNode = context.createNodeForEl(
+            context.builder.parseExpression("{renderer:" + functionName + "}"),
+            attrs
+        );
+    } else {
+        // Handles all other cases:
+        // 1. e.g. <invoke console.log('hello') /> --> console.log(arguement/s)
+
+        functionCallExpression = functionName + "(" + args + ");";
+        newNode = context.builder.scriptlet({
+            value: functionCallExpression
+        });
+    }
+
     context.deprecate(
         'The "<invoke>" tag is deprecated. Please use "$ <js_code>" for JavaScript in the template. See: https://github.com/marko-js/marko/wiki/Deprecation:-var-assign-invoke-tags'
     );
 
-    var functionAttr = elNode.attributes[0];
     if (!functionAttr) {
-        codegen.addError(
+        context.addError(
             'Invalid <invoke> tag. Missing function attribute. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
-    var arg = functionAttr.argument;
-
-    if (arg === undefined) {
-        codegen.addError(
+    if (args === undefined) {
+        context.addError(
             'Invalid <invoke> tag. Missing function arguments. Expected: <invoke console.log("Hello World")'
         );
         return;
     }
 
-    var functionName = functionAttr.name;
-    var functionCallExpression = functionName + "(" + arg + ")";
-    return codegen.builder.parseExpression(functionCallExpression);
+    elNode.insertSiblingBefore(newNode);
+    elNode.detach();
 };
