9bd83b58cc73904d077bdc7d4da85a47daf902e4
Patel, Bhavinkumar(bhavinpatel) <bhavinpatel@ebay.com>
2018-12-06 17:02:10 -0800

    more test cases for async fragment

---
+++ test/compiler/fixtures-html/async-fragment-invalid-var/test.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+
+exports.templateData = {};
+
+exports.checkError = function(e) {
+    expect(Array.isArray(e.errors)).to.equal(true);
+    expect(e.errors.length).to.equal(1);
+
+    var message = e.toString();
+    expect(message).to.contain("Invalid <aync-fragment> tag");
+    expect(message).to.contain("should be a valid JavaScript identifier");
+    expect(message).to.contain(
+        '<async-fragment data-provider=data.userInfo var="userInfo" />'
+    );
+};
