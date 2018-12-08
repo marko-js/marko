ae4ad7fc5ed60dd528b72fe779fd631a6445e6fa
Patel, Bhavinkumar(bhavinpatel) <bhavinpatel@ebay.com>
2018-12-07 16:55:53 -0800

    added event-handler to migrate w-on and code restructure

--- src/taglibs/migrate/common/event-handlers.js
+++ src/taglibs/migrate/common/event-handlers.js
@@ -1,9 +1,13 @@
-module.exports = function migrate(el) {
+module.exports = function migrate(el, context) {
     el.forEachAttribute(attr => {
         const name = attr.name;
-        if (!name.startsWith("w-on")) {
+        if (!name || !name.startsWith("w-on")) {
             return;
         }
+
+        context.deprecate(
+            `The "w-on-*" attribute is deprecated. Please use the on-* attribute instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-control-flow-directive`
+        );
 
         attr.name = name.substring("w-".length);
         attr.argument = attr.value.toString();
