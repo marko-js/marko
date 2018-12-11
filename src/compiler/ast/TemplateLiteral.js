6722bee023acffd1d2aa4fa6c417d53e62245750
Michael Rawlings <mirawlings@ebay.com>
2018-12-10 16:18:43 -0800

    fix undefined for alternate quote

--- src/compiler/ast/TemplateLiteral.js
+++ src/compiler/ast/TemplateLiteral.js
@@ -67,6 +67,8 @@ function escapeQuasi(quasi, quote) {
                 return "\\u2028";
             case "\u2029":
                 return "\\u2029";
+            default:
+                return match;
         }
     });
 }
