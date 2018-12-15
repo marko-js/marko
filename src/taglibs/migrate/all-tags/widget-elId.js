410c0d88c7aee58c23f33632f5070994d859a434
Dylan Piercey <dpiercey@ebay.com>
2018-12-14 17:03:52 -0800

    Fix widget el id migration

--- src/taglibs/migrate/all-tags/widget-elId.js
+++ src/taglibs/migrate/all-tags/widget-elId.js
@@ -1,18 +1,23 @@
 module.exports = function migrate(el, context) {
     el.forEachAttribute(attr => {
-        let name = attr.name;
-        const value = attr.value ? attr.value.toString() : undefined;
-        if (!value || !value.startsWith("widget.elId")) {
+        const value = attr.value;
+
+        if (
+            !value ||
+            value.type !== "FunctionCall" ||
+            value.callee.type !== "MemberExpression" ||
+            (value.callee.object.name !== "widget" &&
+                value.callee.object.name !== "component") ||
+            value.callee.property.name !== "elId"
+        ) {
             return;
         }
 
-        const argument = attr.argument;
         context.deprecate(
             `The "*=widget.elId("someId")" is deprecated. Please use "*:scoped="someId"" modifier instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w-*-Atrributes`
         );
 
-        name = `${attr.name}:scoped`;
-        el.setAttributeValue(name, argument);
-        el.removeAttribute(attr.name);
+        attr.name += ":scoped";
+        attr.value = value.args[0];
     });
 };
