7186f07a58a232fabd3e3d06991d399f1fb05fa0
Patel, Bhavinkumar(bhavinpatel) <bhavinpatel@ebay.com>
2018-12-17 17:46:56 -0800

    w-body migrate

--- src/taglibs/migrate/common/w-body.js
+++ src/taglibs/migrate/common/w-body.js
@@ -1,7 +1,9 @@
+const printJS = require("../util/printJS");
+
 module.exports = function migrate(el, context) {
     if (el.hasAttribute("w-body")) {
         context.deprecate(
-            `The "w-body" attribute is deprecated. Please use "<\${dynamicTag}>" attribute instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w-*-Atrributes`
+            `The "w-body" attribute is deprecated. Please use "<\${dynamicTag}/>" attribute instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w-*-Atrributes`
         );
 
         const builder = context.builder;
@@ -9,11 +11,25 @@ module.exports = function migrate(el, context) {
             el.getAttributeValue("w-body") || builder.identifier("input");
         el.removeAttribute("w-body");
 
-        const childEl = builder.ifStatement(
-            `typeof ${builder.identifier("renderBody")} === 'string'`,
-            [builder.identifier("renderBody")],
-            bodyValue
+        const ifExpressionArg = `typeof ${builder.identifier(
+            "renderBody"
+        )} === 'string'`;
+        const ifTag = builder.htmlElement(
+            "if",
+            undefined,
+            [builder.text(bodyValue)],
+            printJS(ifExpressionArg, context),
+            false,
+            false
         );
-        el.appendChild(childEl);
+        const elseTag = builder.htmlElement(
+            "else",
+            undefined,
+            [bodyValue],
+            undefined,
+            false,
+            false
+        );
+        el.appendChildren([ifTag, elseTag]);
     }
 };
