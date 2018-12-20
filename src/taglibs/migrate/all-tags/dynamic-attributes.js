0afda36e8e3fd9157095021a05b7bb6fc71fb689
Patel <bhavinpatel@lm-sea-11014085.corp.ebay.com>
2018-12-20 15:17:45 -0800

    migrate dynamic attrs

--- src/taglibs/migrate/all-tags/dynamic-attributes.js
+++ src/taglibs/migrate/all-tags/dynamic-attributes.js
@@ -1,19 +1,18 @@
 module.exports = function migrate(el, context) {
-    el.forEachAttribute(attr => {
+    el.forEachAttribute((attr, index) => {
         const name = attr.name;
         const value = attr.value;
         if (
             name == undefined &&
-            (value.type == "MemberExpression" || value.type == "Identifier")
+            (value.type == "MemberExpression" || value.type == "Identifier") &&
+            !attr.spread
         ) {
             context.deprecate(
                 'The "${attributes}" is deprecated. Please use "...attributes" modifier instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w-*-Atrributes'
             );
             attr.spread = true;
-            if (value.property && value.property.value == "*") {
-                el.dynamicAttributes = [];
-                if (el.addDynamicAttributes) el.addDynamicAttributes(value);
-            }
+            el.attributes.splice(index, 1);
+            el.addAttribute(attr);
         }
     });
 };
