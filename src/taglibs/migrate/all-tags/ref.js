b5a58ca2b781d4942b16f1727d92d012271289dc
bhavinpatel@ebay.com <bhavin.ptl04@gmail.com>
2018-12-27 16:34:59 -0800

    refactor code

--- src/taglibs/migrate/all-tags/ref.js
+++ src/taglibs/migrate/all-tags/ref.js
@@ -1,17 +1,13 @@
 module.exports = function migrate(el, context) {
-    if (el.hasAttribute("ref")) {
-        el.forEachAttribute(attr => {
-            let name = attr.name;
-            if (!name || name != "ref") {
-                return;
-            }
-
-            context.deprecate(
-                `The "ref" attribute is deprecated. Please use "key" attribute instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-ref-attribute`
-            );
-
-            el.setAttributeValue("key", attr.value);
-            el.removeAttribute(attr.name);
-        });
+    const attr = el.getAttribute("ref");
+    if (!attr) {
+        return;
     }
+
+    context.deprecate(
+        `The "ref" attribute is deprecated. Please use "key" attribute instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-ref-attribute`
+    );
+
+    el.setAttributeValue("key", attr.value);
+    el.removeAttribute(attr.name);
 };
