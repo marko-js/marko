0cd660626bef517c7fdf7413fc73a39eb6549caa
Dylan Piercey <dpiercey@ebay.com>
2019-01-11 09:44:17 -0800

    Add migrator for legacy nested tag syntax

---
+++ src/taglibs/migrate/all-tags/legacy-nested-tag.js
@@ -0,0 +1,16 @@
+const legacyNestedTagSyntax = /^.*:(.*)$/;
+module.exports = function migrate(el, context) {
+    const match = el.tagName && el.tagName.match(legacyNestedTagSyntax);
+
+    if (!match || !match[1]) {
+        return;
+    }
+
+    const nestedTagName = match[1];
+    el.tagName = `@${nestedTagName}`;
+
+    context.deprecate(
+        'The "<my-tag:nested>" tagName syntax is deprecated. Please use the "<@nested>" tagName syntax instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-legacy-nested-tag',
+        el
+    );
+};
