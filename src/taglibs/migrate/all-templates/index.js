2482427ef2abe0a610b15c54477d0e2943caa1bc
Michael Rawlings <mirawlings@ebay.com>
2019-01-23 15:31:29 -0800

    small changes to allow bundling the migrators with lasso (needed for try online) (#1242)

--- src/taglibs/migrate/all-templates/index.js
+++ src/taglibs/migrate/all-templates/index.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const path = require("path");
-const commonMigrators = fs
-    .readdirSync(__dirname)
-    .filter(entry => entry !== "index.js")
-    .map(entry => require(path.join(__dirname, entry)));
+const commonMigrators = [
+    require("./non-standard-template-literals"),
+    require("./render-calls"),
+    require("./widget-get-template-data")
+];
 
 module.exports = function(root, context) {
     commonMigrators.forEach(migrator => migrator(root, context));
