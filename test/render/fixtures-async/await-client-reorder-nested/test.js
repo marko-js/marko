91f54c5b9bef80f33e6683bfd46eff5dae3fc919
Michael Rawlings <mirawlings@ebay.com>
2019-02-02 01:26:17 -0800

    update render-async tests to show flush points, skip vdom client-reorder tests

--- test/render/fixtures-async/await-client-reorder-nested/test.js
+++ test/render/fixtures-async/await-client-reorder-nested/test.js
@@ -36,3 +36,5 @@ exports.checkEvents = function(events, snapshot, out) {
 
     snapshot(events, out.isVDOM ? "-events-vdom.json" : "-events.json");
 };
+
+exports.skip_vdom = "client-reorder/placeholders are not supported in vdom";
