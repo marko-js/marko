d2a0de5a2ea9e50e53e881b0bd1d1aac570c4d75
Michael Rawlings <mirawlings@ebay.com>
2019-02-14 16:26:31 -0800

    fix <html-comment> tag (#1261)

--- src/taglibs/html/html-comment-tag.js
+++ src/taglibs/html/html-comment-tag.js
@@ -1,10 +1,17 @@
 "use strict";
-module.exports = function render(input, out) {
-    if (out.write) {
-        out.write("<!--");
-        if (input.renderBody) {
-            input.renderBody(out);
-        }
-        out.write("-->");
-    }
+
+module.exports = function codeGenerator(elNode, codegen) {
+    const builder = codegen.builder;
+    const body = elNode.body.array;
+    return builder.functionCall(
+        builder.memberExpression(
+            builder.identifier("out"),
+            builder.identifier("comment")
+        ),
+        [
+            Array.from(body.slice(1)).reduce((expr, current) => {
+                return builder.binaryExpression(expr, "+", current.argument);
+            }, body[0].argument)
+        ]
+    );
 };
