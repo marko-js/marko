c2386884eb2c99625e01e4fe1afeb2c2d9659f84
Michael Rawlings <mirawlings@ebay.com>
2019-05-14 11:39:13 -0700

    cleanup render/fixtures-async

---
+++ test/render/fixtures-async-deprecated/await-placeholder-message-attr/test.js
@@ -0,0 +1,16 @@
+var expect = require("chai").expect;
+
+exports.templateData = {
+    testDataProvider: function(done) {
+        setTimeout(function() {
+            done(null, { name: "Frank" });
+        }, 200);
+    }
+};
+
+exports.checkHtml = function(html) {
+    expect(html).to.contain("Loading main...");
+    expect(html).to.contain("Hello Frank");
+};
+
+exports.skip_vdom = "client-reorder/placeholders are not supported in vdom";
