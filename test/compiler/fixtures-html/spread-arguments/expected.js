b17cee0547192a27e7cf23e35aa0e2a5a987829f
Michael Rawlings <mirawlings@ebay.com>
2019-05-15 12:28:51 -0700

    arguments on dynamic tags are not deprecated

--- test/compiler/fixtures-html/spread-arguments/expected.js
+++ test/compiler/fixtures-html/spread-arguments/expected.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/marko-test$1.0.0/compiler/fixtures-html-deprecated/spread-arguments/template.marko",
+    marko_componentType = "/marko-test$1.0.0/compiler/fixtures-html/spread-arguments/template.marko",
     components_helpers = require("marko/src/runtime/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
@@ -24,5 +24,5 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/marko-test$1.0.0/compiler/fixtures-html-deprecated/spread-arguments/template.marko"
+    id: "/marko-test$1.0.0/compiler/fixtures-html/spread-arguments/template.marko"
   };
