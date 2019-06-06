9a4630a9c7076e1a0b1068bbaa705d26b6fb7b21
Andrew Gliga <agliga@ebay.com>
2019-06-06 12:02:17 -0700

    Fixed relative path issue and added a better variable collision lookup

--- src/core-tags/migrate/util/import-tag.js
+++ src/core-tags/migrate/util/import-tag.js
@@ -1,12 +1,13 @@
 const camelCase = require("camelcase");
 const nameReg = /\/([^/]+?)(?:\/index)?(?:\.[^/]+)?$/;
 
-module.exports = function importTag(importPath, context) {
+module.exports = function importTag(baseImportPath, context) {
     const builder = context.builder;
     const migrateImports = (context._migrateImports = context._migrateImports || {
         identifiers: {},
         paths: {}
     });
+    const importPath = context.getRelativePath(baseImportPath);
 
     if (migrateImports.paths[importPath]) {
         return migrateImports.paths[importPath];
@@ -19,13 +20,27 @@ module.exports = function importTag(importPath, context) {
     const requestedTagName = camelCase((match && match[1]) || "Template", {
         pascalCase: true
     }).replace(/^[^$A-Z_]|[^0-9A-Z_$]/gi, "_");
+    const symbols = [];
+    const walker = context.createWalker({
+        enter(node) {
+            // Try to fetch all symbols and then put them in an array
+            // We will check if any symbols or imports have matching items
+            // in order to prevent variable collisions
+            if (node.type === "HtmlElement" && node.tagName === "import") {
+                symbols.push(node.tagString);
+            } else if (node.type === "Scriptlet") {
+                symbols.push(node.code);
+            }
+        }
+    });
+    walker.walk(context.root);
 
     let identifier = requestedTagName;
     let i = 1;
 
     while (
         migrateImports.identifiers[identifier] ||
-        context.src.includes(identifier)
+        symbols.some(symbol => symbol.includes(identifier))
     ) {
         identifier = `${requestedTagName}_${i}`;
         i++;
