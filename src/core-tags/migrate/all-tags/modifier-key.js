4830662dc707623f19d0acf4e6c374f254daa3ee
Michael Rawlings <mirawlings@ebay.com>
2019-09-03 16:27:57 -0700

    improve migration

--- src/core-tags/migrate/all-tags/modifier-key.js
+++ src/core-tags/migrate/all-tags/modifier-key.js
@@ -1,25 +1,26 @@
 const addIdScopedAttr = require("../util/addIdScopedAttr");
-const componentElId = /^(component\.(?:getE|e)lId)\((.*)\)$/;
+const componentElId = /^component\.(?:getE|e)lId\(.*\)$/;
 
 module.exports = function migrate(el, context) {
     el.forEachAttribute(attr => {
         let name = attr.name;
         let value = attr.value;
-        let match;
 
         if (name && name.endsWith(":key")) {
             context.deprecate(
                 `The ":key" modifier is deprecated. Please use ":scoped" modifier instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w‐*-Attributes`
             );
             name = name.slice(0, 0 - ":key".length) + ":scoped";
-        } else if ((match = componentElId.exec(attr.rawValue))) {
+        } else if (
+            value &&
+            value.type === "FunctionCall" &&
+            componentElId.test(attr.rawValue)
+        ) {
             context.deprecate(
-                `Using ${
-                    match[1]
-                } is deprecated. Please use ":scoped" modifier instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w‐*-Attributes`
+                `Using component.${value.callee.property.name} as an attribute is deprecated. Please use ":scoped" modifier instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-w‐*-Attributes`
             );
             name += ":scoped";
-            value = context.builder.parseExpression(match[2]);
+            value = value.args[0];
         } else {
             // skip this attribute
             return;
