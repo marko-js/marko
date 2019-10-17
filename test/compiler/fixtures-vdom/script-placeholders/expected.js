e4acb23739cb9a4639a37f9ddd69d3e535509d1c
Michael Rawlings <mirawlings@ebay.com>
2019-10-17 15:59:28 -0700

    scripts now always generate a single text child - even if they have placeholders (#1429)

---
+++ test/compiler/fixtures-vdom/script-placeholders/expected.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var marko_template = module.exports = require("marko/src/vdom").t(),
+    components_helpers = require("marko/src/runtime/components/helpers"),
+    marko_registerComponent = components_helpers.rc,
+    marko_componentType = marko_registerComponent("/marko-test$1.0.0/compiler/fixtures-vdom/script-placeholders/template.marko", function() {
+      return module.exports;
+    }),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/vdom/helpers"),
+    marko_createElement = marko_helpers.e,
+    marko_const = marko_helpers.const,
+    marko_const_nextId = marko_const("da453e"),
+    marko_node0 = marko_createElement("script", null, null, null, 0, 0, {
+        i: marko_const_nextId()
+      });
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.e("script", null, null, null, 1)
+    .t(("\n    var x = \"" + input.value) + "\";\n");
+
+  out.e("script", null, null, null, 1)
+    .t(("\n    var x = " + JSON.stringify(input.value)) + "\n");
+
+  out.e("script", null, null, null, 1)
+    .t(("" + input.a) + input.b);
+
+  out.n(marko_node0, component);
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
