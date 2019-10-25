a89f9575527789a57d6eddb5d32794d43f45d8b8
Michael Rawlings <mirawlings@ebay.com>
2019-10-25 09:49:10 -0700

    add legacy properties to RenderResult and add legacy getInitWidgetsCode

--- src/runtime/components/legacy/index.js
+++ src/runtime/components/legacy/index.js
@@ -10,3 +10,6 @@ exports.makeRenderable = exports.renderable = require("../../renderable");
 exports.writeInitWidgetsCode = modernMarko.writeInitComponentsCode;
 exports.getRenderedWidgets = exports.getRenderedWidgetIds =
     modernMarko.getRenderedComponents;
+exports.getInitWidgetsCode = function(out) {
+    return modernMarko.___getInitComponentsCode(out, out);
+};
