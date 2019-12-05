3e82f1d0656c6dd74a3a36200e2d809b35f98b76
Andrew Gliga <agliga@ebay.com>
2019-12-05 10:46:05 -0800

    fix: added test to check null setState

---
+++ test/components-browser/fixtures/set-null-state/test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+module.exports = function(helpers) {
+    var component = helpers.mount(require.resolve("./index"), {});
+
+    expect(component.state).to.equal(null);
+    helpers.triggerEvent(component.getEl("button"), "click");
+    expect(component.state.initNull).to.equal(true);
+};
