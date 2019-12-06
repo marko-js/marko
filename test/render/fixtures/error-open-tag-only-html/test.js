a547a54e722fa31f28d939d65f2c1c357d965782
Eamon Caton <ecaton@ebay.com>
2019-12-06 14:39:30 -0800

    fix: 'error-open-tag-only' tests no longer check for isArray (not the case in Marko 5 compiler)

--- test/render/fixtures/error-open-tag-only-html/test.js
+++ test/render/fixtures/error-open-tag-only-html/test.js
@@ -1,13 +1,10 @@
-var expect = require("chai").expect;
+import { expect } from "chai";
 
-exports.templateData = {};
+export const templateData = {};
 
-exports.checkError = function(e) {
-    expect(Array.isArray(e.errors)).to.equal(true);
-    expect(e.errors.length).to.equal(1);
-
-    var message = e.toString();
+export function checkError(e) {
+    const message = e.toString();
     expect(message).to.contain(
         'The closing "test-open-tag-only" tag was not expected'
     );
-};
+}
