8cbde13abbb3183dc680b6f90b1f87193cdf4411
Michael Rawlings <mirawlings@ebay.com>
2020-01-10 14:00:21 -0800

    fix legacy render lifecycle order to match marko-widgets@6 (#1472)

---
+++ test/components-browser/fixtures-deprecated/widget-config-render-lifecycle/test.js
@@ -0,0 +1,20 @@
+var expect = require("chai").expect;
+
+module.exports = function(helpers) {
+    var widget = helpers.mount(require.resolve("./index"), {
+        name: "Frank"
+    });
+
+    const expectedConfig = [
+        "getInitialProps",
+        "getInitialState",
+        "getTemplateData",
+        "getWidgetConfig"
+    ];
+
+    if (helpers.isHydrate) {
+        expectedConfig.push("getTemplateData");
+    }
+
+    expect(widget.config).to.deep.equal(expectedConfig);
+};
