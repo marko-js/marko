03392555269208f588dc1e2e848b739a481f2bc2
Andrew Gliga <agliga@ebay.com>
2020-02-12 11:39:16 -0800

    Changed dynamic tags to add data-marko-key and to selfclose properly (#1499)

--- src/core-tags/components/helpers/markoKeyAttr.js
+++ src/core-tags/components/helpers/markoKeyAttr.js
@@ -4,7 +4,8 @@ var FLAG_WILL_RERENDER_IN_BROWSER = 1;
 
 module.exports = function markoKeyAttr(key, componentDef) {
     if (
-        (componentDef.___flags & FLAG_WILL_RERENDER_IN_BROWSER) === 0 ||
+        (componentDef.___renderBoundary &&
+            (componentDef.___flags & FLAG_WILL_RERENDER_IN_BROWSER) === 0) ||
         componentDef.___globalComponentsContext.___isPreserved
     ) {
         return componentDef.___nextKey(key) + " " + componentDef.id;
