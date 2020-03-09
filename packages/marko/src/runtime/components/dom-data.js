94b9b1c76322fa84ad75e1d7fd8d03d67f147ff2
Dylan Piercey <dpiercey@ebay.com>
2020-03-09 16:43:10 -0700

    fix: issue with keys under ssr no-update root

--- packages/marko/src/runtime/components/dom-data.js
+++ packages/marko/src/runtime/components/dom-data.js
@@ -19,5 +19,6 @@ module.exports = {
   ___vElementByDOMNode: new WeakMap(),
   ___componentByDOMNode: new WeakMap(),
   ___detachedByDOMNode: new WeakMap(),
-  ___keyByDOMNode: new WeakMap()
+  ___keyByDOMNode: new WeakMap(),
+  ___ssrKeyedElementsByComponentId: {}
 };
