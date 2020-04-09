eae44a792b052543dd3c126156de44492e239788
Michael Rawlings <mirawlings@ebay.com>
2020-04-09 12:16:49 -0700

    test: add failing test for hydration of preserved elements on mismatch (#1534)
    Co-authored-by: Dylan Piercey <dpiercey@ebay.com>
    (cherry picked from commit db56ff95f2554d0182669d62049326aee5093ca7)

---
+++ packages/marko/test/components-browser/fixtures/preserve-dom-server-mismatch/test.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+
+module.exports = function(helpers) {
+  var counter = 0;
+
+  var component = helpers.mount(require.resolve("./index"), {
+    counter: counter
+  });
+
+  expect(component.getEl("list").textContent).to.equal("123");
+};
+
+module.exports.fails_hydrate = true;
