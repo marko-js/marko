b0b36f3e13e3d1e3537f4dce782d812e2d059a1d
Dylan Piercey <dpiercey@ebay.com>
2020-04-09 12:04:35 -0700

    feat: improve serialization across multiple writes (#1542)
    * feat: improve serialization across multiple writes
    
    (cherry picked from commit 81c4da75c8e272d6b2d145e225078695c931dfd3)

---
+++ packages/marko/src/core-tags/components/preferred-script-location-tag.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function forceScriptTagAtThisPoint(out) {
+  const writer = out.writer;
+
+  out.global.___isLastFlush = true;
+  const htmlSoFar = writer.toString();
+  out.global.___isLastFlush = undefined;
+
+  writer.clear();
+  writer.write(htmlSoFar);
+}
+
+module.exports = function render(input, out) {
+  if (out.isSync() === true) {
+    forceScriptTagAtThisPoint(out);
+  } else {
+    const asyncOut = out.beginAsync({ last: true, timeout: -1 });
+    out.onLast(function(next) {
+      forceScriptTagAtThisPoint(asyncOut);
+      asyncOut.end();
+      next();
+    });
+  }
+};
