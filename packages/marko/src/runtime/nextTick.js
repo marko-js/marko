76e2ab910dcdec1af750ca40bc287d147ec82e3f
Dylan Piercey <dpiercey@ebay.com>
2020-04-15 09:57:14 -0700

    fix: improve micro task helper

--- packages/marko/src/runtime/nextTick.js
+++ packages/marko/src/runtime/nextTick.js
@@ -1,1 +1,15 @@
-module.exports = process.nextTick;
+var promise;
+module.exports =
+  typeof queueMicrotask === "function"
+    ? queueMicrotask
+    : typeof Promise === "function" && (promise = Promise.resolve())
+    ? function(cb) {
+        promise.then(cb).catch(rethrow);
+      }
+    : setTimeout;
+
+function rethrow(err) {
+  setTimeout(function() {
+    throw err;
+  });
+}
