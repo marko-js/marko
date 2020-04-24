5bb800e088455a03ccd951c9fcc582da062b499c
Dylan Piercey <dpiercey@ebay.com>
2020-04-24 14:46:36 -0700

    fix: allow legacy widgets to update after being destroyed (#1560)

---
+++ packages/marko/test/components-browser/fixtures-deprecated/widget-destroy-update/index.js
@@ -0,0 +1,13 @@
+module.exports = require("marko/legacy-components").defineComponent({
+  template: require.resolve("./template.marko"),
+
+  getInitialState: function() {
+    return {
+      x: 1
+    };
+  },
+
+  update_x() {
+    this.updatedX = true;
+  }
+});
