74aadf2adb64297456575a30acb33f2fbdd35767
Michael Rawlings <mirawlings@ebay.com>
2020-05-19 16:18:19 -0700

    fix: window.MessageChannel not supported by JSDOM

---
+++ packages/marko/src/runtime/queueMicrotask.js
@@ -0,0 +1,14 @@
+var promise;
+module.exports =
+  typeof queueMicrotask === "function"
+    ? queueMicrotask
+    : typeof Promise === "function" && (promise = Promise.resolve())
+    ? function(cb) {
+        promise.then(cb).catch(rethrow);
+      }
+    : setTimeout;
+function rethrow(err) {
+  setTimeout(function() {
+    throw err;
+  });
+}
