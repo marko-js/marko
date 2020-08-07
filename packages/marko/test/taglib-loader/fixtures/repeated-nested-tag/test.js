36c501ce440f90f6409394b38ed49e185e82d239
Dylan Piercey <dpiercey@ebay.com>
2020-08-07 14:36:01 -0700

    fix: add support for nested tag long hand properties (#1592)
    (cherry picked from commit 8f595fd493d4462f8cc2b103d8ea77e04616edea)

---
+++ packages/marko/test/taglib-loader/fixtures/repeated-nested-tag/test.js
@@ -0,0 +1,20 @@
+var nodePath = require("path");
+
+exports.check = function(taglibLoader, expect) {
+  var taglib = taglibLoader.loadTaglibFromFile(
+    nodePath.join(__dirname, "marko.json")
+  );
+  expect(taglib != null).to.equal(true);
+
+  expect(taglib.tags)
+    .has.property("longhand-tabs")
+    .with.property("nestedTags")
+    .with.property("tab");
+
+  const tab = taglib.tags["longhand-tabs"].nestedTags.tab;
+  expect(tab).has.property("isRepeated", true);
+  expect(tab).has.property("targetProperty", "tabs");
+  expect(tab)
+    .has.property("attributes")
+    .with.property("label");
+};
