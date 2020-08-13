d656e3954121d214d31ed1162ab10ad8709f019f
Dylan Piercey <dpiercey@ebay.com>
2020-08-13 12:43:49 -0700

    chore: deprecate marko/express entry

--- packages/marko/express.js
+++ packages/marko/express.js
@@ -2,6 +2,10 @@
 
 var isDebug = require("./env").isDebug;
 var target = isDebug ? "marko/src/express" : "marko/dist/express";
+isDebug &&
+  require("complain").log(
+    "marko/express has moved into it's own module, install @marko/express instead."
+  );
 
 module.exports = module.parent
   ? module.parent.require(target)
