585d6be9c288b2a0306d2ffcd36d15e8e17a03d9
Dylan Piercey <dpiercey@ebay.com>
2020-09-17 11:18:16 -0700

    perf: prevent bundlers from pulling in setImmediate shim
    (cherry picked from commit 970a788259c74b39eb65d2f9a8d2610bb285125f)

--- packages/marko/src/runtime/setImmediate.js
+++ packages/marko/src/runtime/setImmediate.js
@@ -1,22 +1,21 @@
 module.exports =
-  typeof setImmediate === "function"
-    ? setImmediate
-    : (function() {
-        var queue = [];
-        var win = window;
-        var msg = "" + Math.random();
-        win.addEventListener("message", function(ev) {
-          if (ev.data === msg) {
-            var callbacks = queue;
-            queue = [];
-            for (var i = 0; i < callbacks.length; i++) {
-              callbacks[i]();
-            }
-          }
-        });
-        return function(callback) {
-          if (queue.push(callback) === 1) {
-            win.postMessage(msg, "*");
-          }
-        };
-      })();
+  global.setImmediate ||
+  (function() {
+    var queue = [];
+    var win = window;
+    var msg = "" + Math.random();
+    win.addEventListener("message", function(ev) {
+      if (ev.data === msg) {
+        var callbacks = queue;
+        queue = [];
+        for (var i = 0; i < callbacks.length; i++) {
+          callbacks[i]();
+        }
+      }
+    });
+    return function(callback) {
+      if (queue.push(callback) === 1) {
+        win.postMessage(msg, "*");
+      }
+    };
+  })();
