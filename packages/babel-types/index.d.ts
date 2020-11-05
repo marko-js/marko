6e4be6325c2fd858202a5e7ca98985550f6f4e0b
Ryan Carniato <rcarniato@ebay.com>
2020-11-04 16:37:49 -0800

    feat: add Marko specific traverse types

--- packages/babel-types/index.d.ts
+++ packages/babel-types/index.d.ts
@@ -1,2 +1,7 @@
 import * as types from "./dist/types";
+
+declare module "./dist/types" {
+  export const MARKO_TYPES: string[];
+}
+
 export { types };
--- scripts/types-babel-traverse.js
+++ scripts/types-babel-traverse.js
@@ -1,22 +1,57 @@
 const fs = require("fs");
 const path = require("path");
+require("../packages/babel-types/dist/types/patch");
+const {
+  MARKO_TYPES
+} = require("../packages/babel-types/dist/types/definitions");
+
+const IMPORT = "import * as t from '@babel/types'";
+const IS =
+  "//#region ------------------------- isXXX -------------------------";
+const ASSERT =
+  "//#region ------------------------- assertXXX -------------------------";
+const BREAK = "\n    ";
 
 fs.readFile(
   path.join(__dirname, "../node_modules/@types/babel__traverse/index.d.ts"),
   "utf8",
   (err, data) => {
-    if (err) return console.log(err);
-    var result = data.replace(
-      `import * as t from '@babel/types';`,
-      `import { types as t } from '@marko/babel-types';`
-    );
+    if (err) return console.error(err);
+
+    [IMPORT, IS, ASSERT].forEach(str => {
+      if (data.indexOf(str) === -1) {
+        console.error(
+          `Unable to find \`${str}\` in @types/babel__traverse/index.d.ts`
+        );
+        process.exit(1);
+      }
+    });
+
+    var result = data
+      .replace(IMPORT, `import { types as t } from '@marko/babel-types'`)
+      .replace(
+        IS,
+        IS +
+          BREAK +
+          MARKO_TYPES.map(
+            t => `is${t}(props?: object | null): this is NodePath<t.${t}>;`
+          ).join(BREAK)
+      )
+      .replace(
+        ASSERT,
+        ASSERT +
+          BREAK +
+          MARKO_TYPES.map(t => `assert${t}(props?: object | null): void;`).join(
+            BREAK
+          )
+      );
 
     fs.writeFile(
       path.join(__dirname, "../packages/babel-types/traverse.d.ts"),
       result,
       "utf8",
       err => {
-        if (err) return console.log(err);
+        if (err) return console.error(err);
       }
     );
   }
