420fe60d1f4bd70a9007275e0f858be50bb28e5f
Ryan Carniato <rcarniato@ebay.com>
2020-11-09 15:48:23 -0800

    perf: hoist visitor in static optimization

--- packages/translator-default/src/tag/vdom-optimizer.js
+++ packages/translator-default/src/tag/vdom-optimizer.js
@@ -5,48 +5,53 @@ import { getKeyManager } from "../util/key-manager";
 import write from "../util/vdom-out-write";
 import { tagArguments } from "./native-tag[vdom]";
 
+const staticVisitor = {
+  MarkoText(path, state) {
+    const { node } = path;
+    state.currentRoot = t.callExpression(
+      t.memberExpression(state.currentRoot, t.identifier("t")),
+      [t.stringLiteral(decode(node.value))]
+    );
+  },
+  MarkoPlaceholder(path, state) {
+    const { value } = path.get("value").evaluate();
+    state.currentRoot = t.callExpression(
+      t.memberExpression(state.currentRoot, t.identifier("t")),
+      [t.stringLiteral(value != null ? value.toString() : "")]
+    );
+  },
+  MarkoTag(path, state) {
+    if (path.node.attributes.find(a => a.name === "key"))
+      getKeyManager(path).resolveKey(path);
+    const writeArgs = tagArguments(path, true);
+    state.currentRoot = t.callExpression(
+      t.memberExpression(state.currentRoot, t.identifier("e")),
+      writeArgs
+    );
+  }
+};
+
 export default function VDOMOptimizer(path) {
   const {
     hub: { file }
   } = path;
   const identifier = path.scope.generateUidIdentifier("marko_node");
   const writeArgs = tagArguments(path, true);
+  const state = {
+    currentRoot: t.callExpression(
+      importDefault(
+        file,
+        "marko/src/runtime/vdom/helpers/v-element",
+        "marko_createElement"
+      ),
+      writeArgs
+    )
+  };
 
-  let create = t.callExpression(
-    importDefault(
-      file,
-      "marko/src/runtime/vdom/helpers/v-element",
-      "marko_createElement"
-    ),
-    writeArgs
-  );
-
-  path.traverse({
-    MarkoText(path) {
-      const { node } = path;
-      create = t.callExpression(t.memberExpression(create, t.identifier("t")), [
-        t.stringLiteral(decode(node.value))
-      ]);
-    },
-    MarkoPlaceholder(path) {
-      const { value } = path.get("value").evaluate();
-      create = t.callExpression(t.memberExpression(create, t.identifier("t")), [
-        t.stringLiteral(value != null ? value.toString() : "")
-      ]);
-    },
-    MarkoTag(path) {
-      if (path.node.attributes.find(a => a.name === "key"))
-        getKeyManager(path).resolveKey(path);
-      const writeArgs = tagArguments(path, true);
-      create = t.callExpression(
-        t.memberExpression(create, t.identifier("e")),
-        writeArgs
-      );
-    }
-  });
+  path.traverse(staticVisitor, state);
 
   const d = t.variableDeclaration("const", [
-    t.variableDeclarator(identifier, create)
+    t.variableDeclarator(identifier, state.currentRoot)
   ]);
   file.path.node.body.push(d);
   path.replaceWith(write("n", identifier, t.identifier("component")));
