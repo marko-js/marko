458ca75e74db78edb33a92549fd34b013a485c02
Dylan Piercey <dpiercey@ebay.com>
2020-12-11 12:49:18 -0700

    feat(translator-fluurt): style tag now extracted as static content (#58)

--- packages/translator/test/fixtures/placeholders/snapshots/html-expected.js
+++ packages/translator/test/fixtures/placeholders/snapshots/html-expected.js
@@ -2,6 +2,4 @@ import { escapeXML as _escapeXML, toString as _toString, write as _write } from 
 
 _write(`<div>${_escapeXML(input.x)}Hello world &lt;a/>${_toString(input.x)}Hello world <a/><script>
     Hello &lt;b> &lt;/script>
-  </script><style>
-    Hello &lt;b> &lt;/style>
-  </style></div>`);
+  </script></div>`);
