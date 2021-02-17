5d0a74a616d6f5d6bdf97a9c041b0c56d9a0d862
Dylan Piercey <dpiercey@ebay.com>
2021-02-17 12:51:22 -0700

    fix(marko): simplify client-reorder runtime loading login to inline code (#1661)

--- packages/marko/src/core-tags/core/await/client-reorder.js
+++ packages/marko/src/core-tags/core/await/client-reorder.js
@@ -1,14 +1,6 @@
-var code;
-var fs = require("fs");
-
 exports.isSupported = true;
 
 exports.getCode = function() {
-  if (!code) {
-    code = fs.readFileSync(
-      require.resolve("./client-reorder-runtime.min.js"),
-      "utf8"
-    );
-  }
-  return code;
+  // Minified version of ./client-reorder-runtime.js
+  return `function $af(d,a,e,l,g,h,k,b,f,c){c=$af;if(a&&!c[a])(c[a+="$"]||(c[a]=[])).push(d);else{e=document;l=e.getElementById("af"+d);g=e.getElementById("afph"+d);h=e.createDocumentFragment();k=l.childNodes;b=0;for(f=k.length;b<f;b++)h.appendChild(k.item(0));g&&g.parentNode.replaceChild(h,g);c[d]=1;if(a=c[d+"$"])for(b=0,f=a.length;b<f;b++)c(a[b])}}`;
 };
