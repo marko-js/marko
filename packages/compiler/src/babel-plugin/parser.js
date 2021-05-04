8aaf9105323587fae4f40dd69a2497f114a867c5
Dylan Piercey <dpiercey@ebay.com>
2021-05-04 11:05:39 -0700

    fix(compiler): var name typo

--- packages/compiler/src/babel-plugin/parser.js
+++ packages/compiler/src/babel-plugin/parser.js
@@ -317,7 +317,7 @@ export function parseMarko(file) {
 
       if (tagDef && tagDef.parser) {
         if (tagDef.parser.path) {
-          watchFiles.push(tagDef.parse.path);
+          watchFiles.push(tagDef.parser.path);
         }
         /* istanbul ignore next */
         (tagDef.parser.hook.default || tagDef.parser.hook)(tag, t);
