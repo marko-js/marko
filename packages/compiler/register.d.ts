ecdafa047cdcbe87b941856102a00da34d8efe25
Dylan Piercey <dpiercey@ebay.com>
2021-08-06 21:46:30 -0700

    fix: improve compiler register hook types

--- packages/compiler/register.d.ts
+++ packages/compiler/register.d.ts
@@ -1,6 +1,6 @@
 import { Config } from ".";
 
-type Extensions = typeof require.extensions;
+type Extensions = Partial<typeof require.extensions>;
 export default function register(
   config: Config & { extensions?: Extensions }
 ): Extensions;
