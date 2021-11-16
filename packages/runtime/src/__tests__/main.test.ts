d449cc46e3e1226da13713ec322b55fda63dccda
Michael Rawlings <mirawlings@ebay.com>
2021-11-16 14:58:21 -0800

    chore: update mocha-snap, move snapshots under individual fixtures

--- packages/runtime/src/__tests__/main.test.ts
+++ packages/runtime/src/__tests__/main.test.ts
@@ -34,121 +34,129 @@ describe("runtime", () => {
       (skipSSR ? it.skip : it)(
         `ssr${skipHydrate ? "" : " + hydrate"}`,
         async () => {
-          await snap(async () => {
-            const serverTemplate = require(serverFile);
-            const render = createRenderer(serverTemplate.default, true);
+          await snap(
+            async () => {
+              const serverTemplate = require(serverFile);
+              const render = createRenderer(serverTemplate.default, true);
 
-            let buffer = "";
-            let flushCount = 0;
+              let buffer = "";
+              let flushCount = 0;
 
+              const browser = createBrowser({ dir: __dirname });
+              const document = browser.window.document;
+              const test = browser.require(testFile);
+              const hydrateIndex = test.hydrateFlush;
+              const input = test.default[0];
+
+              document.open();
+
+              const tracker = createTrackMutations(browser.window, document);
+
+              await render(input, {
+                write(data: string) {
+                  buffer += data;
+                  tracker.log(
+                    `# Write\n${indent(
+                      data.replace(reorderRuntimeString, "REORDER_RUNTIME")
+                    )}`
+                  );
+                },
+                flush() {
+                  if (!skipHydrate && hydrateIndex === flushCount++) {
+                    browser.require(hydrateFile);
+                    tracker.logUpdate("Hydrate");
+                  }
+                  // tracker.logUpdate("Flush");
+                  // document.write(buffer);
+                  // buffer = "";
+                },
+                end(data?: string) {
+                  document.write(buffer + (data || ""));
+                  document.close();
+                  tracker.logUpdate("End");
+                  if (!skipHydrate && hydrateIndex == null) {
+                    browser.require(hydrateFile);
+                    tracker.logUpdate("Hydrate");
+                  }
+                },
+                emit(type: string, ...args: unknown[]) {
+                  tracker.log(
+                    `# Emit ${type}${args.map((arg) => `\n${indent(arg)}`)}`
+                  );
+                },
+              } as Writable & { flush(): void });
+
+              hydratedHTML = getNormalizedHtml(document.body);
+
+              if (!skipHydrate) {
+                const { run } = browser.require(
+                  "../dom/index"
+                ) as typeof import("../dom/index");
+
+                for (const update of test.default.slice(1)) {
+                  if (isWait(update)) {
+                    await update();
+                  } else if (typeof update === "function") {
+                    update(document.documentElement);
+                    run();
+                    tracker.logUpdate(update);
+                  } else {
+                    // if new input is detected, stop testing
+                    // this will be covered by the client tests
+                    break;
+                  }
+                }
+              }
+
+              return tracker.getLogs();
+            },
+            ".md",
+            resolve("./")
+          );
+        }
+      );
+      (skipCSR ? it.skip : it)("csr", async () => {
+        await snap(
+          async () => {
             const browser = createBrowser({ dir: __dirname });
             const document = browser.window.document;
             const test = browser.require(testFile);
-            const hydrateIndex = test.hydrateFlush;
             const input = test.default[0];
+            const { run } = browser.require(
+              "../dom/index"
+            ) as typeof import("../dom/index");
+            const render = browser.require(browserFile).default;
+            const container = Object.assign(document.createElement("div"), {
+              TEST_ROOT: true,
+            });
+            const tracker = createTrackMutations(browser.window, container);
 
-            document.open();
+            document.body.appendChild(container);
 
-            const tracker = createTrackMutations(browser.window, document);
+            const instance = render(input);
+            container.appendChild(instance);
 
-            await render(input, {
-              write(data: string) {
-                buffer += data;
-                tracker.log(
-                  `# Write\n${indent(
-                    data.replace(reorderRuntimeString, "REORDER_RUNTIME")
-                  )}`
-                );
-              },
-              flush() {
-                if (!skipHydrate && hydrateIndex === flushCount++) {
-                  browser.require(hydrateFile);
-                  tracker.logUpdate("Hydrate");
-                }
-                // tracker.logUpdate("Flush");
-                // document.write(buffer);
-                // buffer = "";
-              },
-              end(data?: string) {
-                document.write(buffer + (data || ""));
-                document.close();
-                tracker.logUpdate("End");
-                if (!skipHydrate && hydrateIndex == null) {
-                  browser.require(hydrateFile);
-                  tracker.logUpdate("Hydrate");
-                }
-              },
-              emit(type: string, ...args: unknown[]) {
-                tracker.log(
-                  `# Emit ${type}${args.map((arg) => `\n${indent(arg)}`)}`
-                );
-              },
-            } as Writable & { flush(): void });
+            initialHTML = getNormalizedHtml(container);
+            tracker.logUpdate(input);
 
-            hydratedHTML = getNormalizedHtml(document.body);
-
-            if (!skipHydrate) {
-              const { run } = browser.require(
-                "../dom/index"
-              ) as typeof import("../dom/index");
-
-              for (const update of test.default.slice(1)) {
-                if (isWait(update)) {
-                  await update();
-                } else if (typeof update === "function") {
-                  update(document.documentElement);
-                  run();
-                  tracker.logUpdate(update);
-                } else {
-                  // if new input is detected, stop testing
-                  // this will be covered by the client tests
-                  break;
-                }
+            for (const update of test.default.slice(1)) {
+              if (isWait(update)) {
+                await update();
+              } else if (typeof update === "function") {
+                update(document.documentElement);
+                run();
+                tracker.logUpdate(update);
+              } else {
+                instance.update(update);
+                tracker.logUpdate(update);
               }
             }
 
             return tracker.getLogs();
-          }, ".md");
-        }
-      );
-      (skipCSR ? it.skip : it)("csr", async () => {
-        await snap(async () => {
-          const browser = createBrowser({ dir: __dirname });
-          const document = browser.window.document;
-          const test = browser.require(testFile);
-          const input = test.default[0];
-          const { run } = browser.require(
-            "../dom/index"
-          ) as typeof import("../dom/index");
-          const render = browser.require(browserFile).default;
-          const container = Object.assign(document.createElement("div"), {
-            TEST_ROOT: true,
-          });
-          const tracker = createTrackMutations(browser.window, container);
-
-          document.body.appendChild(container);
-
-          const instance = render(input);
-          container.appendChild(instance);
-
-          initialHTML = getNormalizedHtml(container);
-          tracker.logUpdate(input);
-
-          for (const update of test.default.slice(1)) {
-            if (isWait(update)) {
-              await update();
-            } else if (typeof update === "function") {
-              update(document.documentElement);
-              run();
-              tracker.logUpdate(update);
-            } else {
-              instance.update(update);
-              tracker.logUpdate(update);
-            }
-          }
-
-          return tracker.getLogs();
-        }, ".md");
+          },
+          ".md",
+          resolve("./")
+        );
       });
       (skipSSR || skipCSR ? it.skip : it)("equivalent", () => {
         assert.strictEqual(hydratedHTML, initialHTML);
