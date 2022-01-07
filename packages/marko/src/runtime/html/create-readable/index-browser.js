8611e42fc90c5f9ec3f39e9da5e040a531f72052
Dylan Piercey <dpiercey@ebay.com>
2022-01-06 20:04:34 -0700

    fix: improve error handling in web workers

--- packages/marko/src/runtime/html/create-readable/index-browser.js
+++ packages/marko/src/runtime/html/create-readable/index-browser.js
@@ -1,22 +1,28 @@
 var encoder = new TextEncoder();
+var noop = function () {};
 
 module.exports = function (data) {
   // eslint-disable-next-line no-undef
   var transformStream = new TransformStream();
   var writer = transformStream.writable.getWriter();
+  var facade = {
+    write: function (string) {
+      writer.write(encoder.encode(string));
+    },
+    end: function () {
+      writer.close();
+    }
+  };
   var out = this.createOut(
     data && data.$global,
-    {
-      write: function (string) {
-        writer.write(encoder.encode(string));
-      },
-      end: function () {
-        writer.close();
-      }
-    },
+    facade,
     undefined,
     this.___shouldBuffer
   );
+  out.once("error", err => {
+    facade.write = facade.end = noop;
+    writer.abort(err);
+  });
   this.render(data, out);
   out.end();
 
