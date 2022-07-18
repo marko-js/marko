2f6459d2c421ac82c4627c90f1c50cb229a99d33
Dylan Piercey <dpiercey@ebay.com>
2022-07-18 15:59:20 -0700

    fix: issue with return statement inside attr method shorthand (#1824)

---
+++ packages/translator-default/test/fixtures/attr-method-with-return/snapshots/html-expected.js
@@ -0,0 +1,21 @@
+import { t as _t } from "marko/src/runtime/html/index.js";
+
+const _marko_componentType = "packages/translator-default/test/fixtures/attr-method-with-return/template.marko",
+      _marko_template = _t(_marko_componentType);
+
+export default _marko_template;
+import _test from "./components/test.marko";
+import _marko_tag from "marko/src/runtime/helpers/render-tag.js";
+import _marko_renderer from "marko/src/runtime/components/renderer.js";
+const _marko_component = {};
+_marko_template._ = _marko_renderer(function (input, out, _componentDef, _component, state) {
+  _marko_tag(_test, {
+    "doThing": function () {
+      return 1;
+    }
+  }, out, _componentDef, "0");
+}, {
+  t: _marko_componentType,
+  i: true,
+  d: true
+}, _marko_component);
