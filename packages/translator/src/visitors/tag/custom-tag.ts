61299a42f627dd9036be209ccbc7d59498987a2b
Michael Rawlings <mirawlings@ebay.com>
2022-10-26 03:43:28 -0400

    refactor: split html/dom translator for custom-tag

--- packages/translator/src/visitors/tag/custom-tag.ts
+++ packages/translator/src/visitors/tag/custom-tag.ts
@@ -87,179 +87,191 @@ export default {
       }
     },
     exit(tag: t.NodePath<t.MarkoTag>) {
-      const tagSectionId = getSectionId(tag);
-      const tagBody = tag.get("body");
-      const tagBodySectionId = getSectionId(tagBody);
-      const isHTML = isOutputHTML();
-      const { node } = tag;
-      const write = writer.writeTo(tag);
-      const binding = node.extra.reserve!;
-      let tagIdentifier: t.Expression;
-      let tagAttrsIdentifier: t.Identifier | undefined;
-
-      if (isHTML) {
-        writer.flushInto(tag);
-        writeHTMLHydrateStatements(tagBody);
-      }
-
-      if (t.isStringLiteral(node.name)) {
-        const { file } = tag.hub;
-        const tagName = node.name.value;
-        const tags = file.metadata.marko.tags;
-        const tagDef = getTagDef(tag);
-        const template = tagDef?.template;
-        const relativePath = template && resolveRelativePath(file, template);
-        const childFile = loadFileForTag(tag)!;
-        const childProgram = childFile.ast.program;
-
-        if (!relativePath) {
-          throw tag
-            .get("name")
-            .buildCodeFrameError(
-              `Unable to find entry point for custom tag <${tagName}>.`
-            );
-        }
-
-        if (isHTML) {
-          tagIdentifier = importDefault(file, relativePath, tagName);
-        } else {
-          tagIdentifier = importNamed(file, relativePath, "setup", tagName);
-          if (childProgram.extra.attrs) {
-            tagAttrsIdentifier = importNamed(
-              file,
-              relativePath,
-              "attrs",
-              `${tagName}_attrs`
-            );
-          }
-          write`${importNamed(
-            file,
-            relativePath,
-            "template",
-            `${tagName}_template`
-          )}`;
-          walks.injectWalks(
-            tag,
-            binding.id,
-            importNamed(file, relativePath, "walks", `${tagName}_walks`)
-          );
-        }
-
-        if (!tags.includes(relativePath)) {
-          tags.push(relativePath);
-        }
+      if (isOutputHTML()) {
+        translateHTML(tag);
       } else {
-        tagIdentifier = node.name;
-      }
-
-      const tagVar = node.var;
-      const attrsObject = attrsToObject(tag, true);
-      const renderBodyProp = getRenderBodyProp(attrsObject);
-
-      if (isHTML && node.extra.tagNameNullable) {
-        let renderBodyId: t.Identifier | undefined = undefined;
-        let renderTagExpr: t.Expression = callExpression(
-          tagIdentifier,
-          attrsToObject(tag)
-        );
-
-        if (renderBodyProp) {
-          renderBodyId = tag.scope.generateUidIdentifier("renderBody");
-          const [renderBodyPath] = tag.insertBefore(
-            t.functionDeclaration(
-              renderBodyId,
-              renderBodyProp.params,
-              renderBodyProp.body
-            )
-          );
-
-          renderBodyPath.skip();
-
-          (attrsObject as t.ObjectExpression).properties[
-            (attrsObject as t.ObjectExpression).properties.length - 1
-          ] = t.objectProperty(t.identifier("renderBody"), renderBodyId);
-        }
-
-        if (tagVar) {
-          translateVar(
-            tag,
-            t.unaryExpression("void", t.numericLiteral(0)),
-            "let"
-          );
-          renderTagExpr = t.assignmentExpression("=", tagVar, renderTagExpr);
-        }
-
-        tag
-          .replaceWith(
-            t.ifStatement(
-              tagIdentifier,
-              t.expressionStatement(renderTagExpr),
-              renderBodyId && callStatement(renderBodyId)
-            )
-          )[0]
-          .skip();
-      } else {
-        if (isHTML) {
-          if (tagVar) {
-            translateVar(tag, callExpression(tagIdentifier, attrsObject));
-            tag.remove();
-          } else {
-            tag
-              .replaceWith(callStatement(tagIdentifier, attrsObject))[0]
-              .skip();
-          }
-        } else {
-          if (renderBodyProp) {
-            const { walks, writes } = writer.getSectionMeta(tagBodySectionId);
-            (attrsObject as t.ObjectExpression).properties.pop();
-            (attrsObject as t.ObjectExpression).properties.push(
-              t.objectProperty(
-                t.identifier("renderBody"),
-                callRuntime(
-                  "createRenderer",
-                  writes || t.stringLiteral(""),
-                  walks || t.stringLiteral(""),
-                  t.arrowFunctionExpression(
-                    renderBodyProp.params,
-                    renderBodyProp.body
-                  )
-                )
-              )
-            );
-          }
-          addStatement(
-            "apply",
-            tagSectionId,
-            undefined,
-            t.expressionStatement(
-              t.callExpression(tagIdentifier, [callRead(binding, tagSectionId)])
-            )
-          );
-          if (attrsObject && tagAttrsIdentifier) {
-            getSignal(
-              tagSectionId,
-              (tag.node.extra.attrsReferences as ReferenceGroup).references
-            ).subscribers.push(tagAttrsIdentifier);
-            addStatement(
-              "apply",
-              tagSectionId,
-              tag.node.extra.attrsReferences as ReferenceGroup,
-              t.expressionStatement(
-                callRuntime(
-                  "setSource",
-                  callRead(binding, tagSectionId),
-                  t.identifier(tagAttrsIdentifier.name),
-                  attrsObject
-                )
-              )
-            );
-          }
-          tag.remove();
-        }
+        translateDOM(tag);
       }
     },
   },
 };
+
+function translateHTML(tag: t.NodePath<t.MarkoTag>) {
+  const tagBody = tag.get("body");
+  const { node } = tag;
+  let tagIdentifier: t.Expression;
+
+  writer.flushInto(tag);
+  writeHTMLHydrateStatements(tagBody);
+
+  if (t.isStringLiteral(node.name)) {
+    const { file } = tag.hub;
+    const tagName = node.name.value;
+    const relativePath = getTagRelativePath(tag);
+
+    tagIdentifier = importDefault(file, relativePath, tagName);
+  } else {
+    tagIdentifier = node.name;
+  }
+
+  const tagVar = node.var;
+  const attrsObject = attrsToObject(tag, true);
+  const renderBodyProp = getRenderBodyProp(attrsObject);
+
+  if (node.extra.tagNameNullable) {
+    let renderBodyId: t.Identifier | undefined = undefined;
+    let renderTagExpr: t.Expression = callExpression(
+      tagIdentifier,
+      attrsToObject(tag)
+    );
+
+    if (renderBodyProp) {
+      renderBodyId = tag.scope.generateUidIdentifier("renderBody");
+      const [renderBodyPath] = tag.insertBefore(
+        t.functionDeclaration(
+          renderBodyId,
+          renderBodyProp.params,
+          renderBodyProp.body
+        )
+      );
+
+      renderBodyPath.skip();
+
+      (attrsObject as t.ObjectExpression).properties[
+        (attrsObject as t.ObjectExpression).properties.length - 1
+      ] = t.objectProperty(t.identifier("renderBody"), renderBodyId);
+    }
+
+    if (tagVar) {
+      translateVar(tag, t.unaryExpression("void", t.numericLiteral(0)), "let");
+      renderTagExpr = t.assignmentExpression("=", tagVar, renderTagExpr);
+    }
+
+    tag
+      .replaceWith(
+        t.ifStatement(
+          tagIdentifier,
+          t.expressionStatement(renderTagExpr),
+          renderBodyId && callStatement(renderBodyId)
+        )
+      )[0]
+      .skip();
+  } else if (tagVar) {
+    translateVar(tag, callExpression(tagIdentifier, attrsObject));
+    tag.remove();
+  } else {
+    tag.replaceWith(callStatement(tagIdentifier, attrsObject))[0].skip();
+  }
+}
+
+function translateDOM(tag: t.NodePath<t.MarkoTag>) {
+  const tagSectionId = getSectionId(tag);
+  const tagBody = tag.get("body");
+  const tagBodySectionId = getSectionId(tagBody);
+  const { node } = tag;
+  const write = writer.writeTo(tag);
+  const binding = node.extra.reserve!;
+  const { file } = tag.hub;
+  const tagName = (node.name as t.StringLiteral).value;
+  const relativePath = getTagRelativePath(tag);
+  const childFile = loadFileForTag(tag)!;
+  const childProgram = childFile.ast.program;
+  const tagIdentifier = importNamed(file, relativePath, "setup", tagName);
+  let tagAttrsIdentifier: t.Identifier | undefined;
+  if (childProgram.extra.attrs) {
+    tagAttrsIdentifier = importNamed(
+      file,
+      relativePath,
+      "attrs",
+      `${tagName}_attrs`
+    );
+  }
+  write`${importNamed(file, relativePath, "template", `${tagName}_template`)}`;
+  walks.injectWalks(
+    tag,
+    binding.id,
+    importNamed(file, relativePath, "walks", `${tagName}_walks`)
+  );
+
+  const attrsObject = attrsToObject(tag, true);
+  const renderBodyProp = getRenderBodyProp(attrsObject);
+
+  if (renderBodyProp) {
+    const { walks, writes } = writer.getSectionMeta(tagBodySectionId);
+    (attrsObject as t.ObjectExpression).properties.pop();
+    (attrsObject as t.ObjectExpression).properties.push(
+      t.objectProperty(
+        t.identifier("renderBody"),
+        callRuntime(
+          "createRenderer",
+          writes || t.stringLiteral(""),
+          walks || t.stringLiteral(""),
+          t.arrowFunctionExpression(renderBodyProp.params, renderBodyProp.body)
+        )
+      )
+    );
+  }
+  addStatement(
+    "apply",
+    tagSectionId,
+    undefined,
+    t.expressionStatement(
+      t.callExpression(tagIdentifier, [callRead(binding, tagSectionId)])
+    )
+  );
+  if (attrsObject && tagAttrsIdentifier) {
+    getSignal(
+      tagSectionId,
+      (tag.node.extra.attrsReferences as ReferenceGroup).references
+    ).subscribers.push(tagAttrsIdentifier);
+    addStatement(
+      "apply",
+      tagSectionId,
+      tag.node.extra.attrsReferences as ReferenceGroup,
+      t.expressionStatement(
+        callRuntime(
+          "setSource",
+          callRead(binding, tagSectionId),
+          t.identifier(tagAttrsIdentifier.name),
+          attrsObject
+        )
+      )
+    );
+  }
+  tag.remove();
+}
+
+function getTagRelativePath(tag: t.NodePath<t.MarkoTag>) {
+  const {
+    node,
+    hub: { file },
+  } = tag;
+  const nameIsString = t.isStringLiteral(node.name);
+  let relativePath: string | undefined;
+
+  if (nameIsString) {
+    const tagDef = getTagDef(tag);
+    const template = tagDef?.template;
+    relativePath = template && resolveRelativePath(file, template);
+  }
+
+  if (!relativePath) {
+    throw tag
+      .get("name")
+      .buildCodeFrameError(
+        `Unable to find entry point for custom tag <${
+          nameIsString ? (node.name as t.StringLiteral).value : node.name
+        }>.`
+      );
+  }
+
+  const tags = file.metadata.marko.tags;
+  if (!tags.includes(relativePath)) {
+    tags.push(relativePath);
+  }
+
+  return relativePath;
+}
 
 function callStatement(
   id: t.Expression,
