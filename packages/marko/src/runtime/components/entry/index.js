5743386436a3b5a0faf6999f06a599db4fd7bb2f
Dylan Piercey <dpiercey@ebay.com>
2023-02-27 16:03:51 -0700

    fix: issue with Marko.Component registered too late

--- packages/marko/src/runtime/components/entry/index.js
+++ packages/marko/src/runtime/components/entry/index.js
@@ -271,7 +271,3 @@ exports.writeInitComponentsCode = writeInitComponentsCode;
 exports.getRenderedComponents = function (out) {
   return warp10.stringifyPrepare(getInitComponentsDataFromOut(out));
 };
-
-globalThis.Marko = {
-  Component: function () {}
-};
