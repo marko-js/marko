a65e3672cf7c69a1b964f18909182c50c81a5780
Dylan Piercey <dpiercey@ebay.com>
2023-07-06 10:52:24 -0700

    chore: update test runner to capture diagnostic snapshots

---
+++ packages/translator-default/test/fixtures/error-class-with-name/snapshots/htmlProduction-expected.js
@@ -0,0 +1,13 @@
+import { t as _t } from "marko/dist/runtime/html/index.js";
+const _marko_componentType = "/7Yakras",
+  _marko_template = _t(_marko_componentType);
+export default _marko_template;
+import _marko_renderer from "marko/dist/runtime/components/renderer.js";
+const _marko_component = {
+  onCreate() {}
+};
+_marko_template._ = _marko_renderer(function (input, out, _componentDef, _component, state, $global) {
+  out.w("<div></div>");
+}, {
+  t: _marko_componentType
+}, _marko_component);
---
+++ packages/translator-default/test/fixtures/error-class-with-name/snapshots/vdomProduction-expected.js
@@ -0,0 +1,19 @@
+import { t as _t } from "marko/dist/runtime/vdom/index.js";
+const _marko_componentType = "/7Yakras",
+  _marko_template = _t(_marko_componentType);
+export default _marko_template;
+import _marko_createElement from "marko/dist/runtime/vdom/helpers/v-element.js";
+const _marko_node = _marko_createElement("div", null, "0", null, 0, 0);
+import _marko_renderer from "marko/dist/runtime/components/renderer.js";
+import { r as _marko_registerComponent } from "marko/dist/runtime/components/registry";
+_marko_registerComponent(_marko_componentType, () => _marko_template);
+const _marko_component = {
+  onCreate() {}
+};
+_marko_template._ = _marko_renderer(function (input, out, _componentDef, _component, state, $global) {
+  out.n(_marko_node, _component);
+}, {
+  t: _marko_componentType
+}, _marko_component);
+import _marko_defineComponent from "marko/dist/runtime/components/defineComponent.js";
+_marko_template.Component = _marko_defineComponent(_marko_component, _marko_template._);
