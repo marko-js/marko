2703a81a8513a33485b8fa908c5350a8189fd877
Dylan Piercey <dpiercey@ebay.com>
2023-08-08 10:23:03 -0700

    fix: data migration on reassignment

---
+++ packages/translator-default/test/fixtures/data-reassigned/snapshots/cjs-expected.js
@@ -0,0 +1,31 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+var _index = require("marko/src/runtime/html/index.js");
+var _escapeXml = require("marko/src/runtime/html/helpers/escape-xml.js");
+var _renderer = _interopRequireDefault(require("marko/src/runtime/components/renderer.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const _marko_componentType = "packages/translator-default/test/fixtures/data-reassigned/template.marko",
+  _marko_template = (0, _index.t)(_marko_componentType);
+var _default = _marko_template;
+exports.default = _default;
+const _marko_component = {};
+_marko_template._ = (0, _renderer.default)(function (input, out, _componentDef, _component, state, $global) {
+  var data;
+  data = {
+    name: "foo"
+  };
+  out.w("<div>");
+  out.w("Hello ");
+  out.w((0, _escapeXml.x)(data.name));
+  out.w("<span>");
+  out.w("Hello ");
+  out.w((0, _escapeXml.x)(data));
+  out.w("</span>");
+  out.w("</div>");
+}, {
+  t: _marko_componentType,
+  i: true,
+  d: true
+}, _marko_component);
