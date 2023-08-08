ec21e799f39e74c3d5b0fcfb5839a3954fbc7ad0
Dylan Piercey <dpiercey@ebay.com>
2023-08-08 10:23:03 -0700

    fix: data migration on reassignment

---
+++ .changeset/strong-mugs-bow.md
@@ -0,0 +1,6 @@
+---
+"@marko/translator-default": patch
+"marko": patch
+---
+
+Fix issue where data (legacy alias of input) was overwritten by assignment but still being migrated.
---
+++ packages/translator-default/test/fixtures/data-reassigned/snapshots/html-expected.js
@@ -0,0 +1,25 @@
+import { t as _t } from "marko/src/runtime/html/index.js";
+const _marko_componentType = "packages/translator-default/test/fixtures/data-reassigned/template.marko",
+  _marko_template = _t(_marko_componentType);
+export default _marko_template;
+import { x as _marko_escapeXml } from "marko/src/runtime/html/helpers/escape-xml.js";
+import _marko_renderer from "marko/src/runtime/components/renderer.js";
+const _marko_component = {};
+_marko_template._ = _marko_renderer(function (input, out, _componentDef, _component, state, $global) {
+  var data;
+  data = {
+    name: "foo"
+  };
+  out.w("<div>");
+  out.w("Hello ");
+  out.w(_marko_escapeXml(data.name));
+  out.w("<span>");
+  out.w("Hello ");
+  out.w(_marko_escapeXml(data));
+  out.w("</span>");
+  out.w("</div>");
+}, {
+  t: _marko_componentType,
+  i: true,
+  d: true
+}, _marko_component);
