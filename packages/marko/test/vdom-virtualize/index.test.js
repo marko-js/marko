4d6c2149f6f7d3314bbf5592815e18ba301b3f9e
dpiercey <dpiercey@ebay.com>
2024-01-24 11:52:18 -0700

    chore: improve morphdom test perf

--- packages/marko/test/vdom-virtualize/index.test.js
+++ packages/marko/test/vdom-virtualize/index.test.js
@@ -2,7 +2,7 @@
 
 require("../__util__/test-init");
 var fs = require("fs");
-var { createBrowser } = require("jsdom-context-require");
+var { JSDOM } = require("jsdom");
 var virtualize = require("marko/runtime/vdom/vdom").___virtualize;
 var autotest = require("mocha-autotest").default;
 var toHTML = require("../__util__/toHTML");
@@ -15,12 +15,7 @@ autotest("fixtures", (fixture) => {
     var inputPath = resolve("input.html");
     if (fs.existsSync(inputPath)) {
       var inputHtml = fs.readFileSync(inputPath, { encoding: "utf8" });
-
-      var document = createBrowser({
-        dir: __dirname,
-        html: "<html><body>" + inputHtml + "</body></html>",
-      }).window.document;
-      var domNode = document.body.firstChild;
+      var domNode = JSDOM.fragment(inputHtml).firstChild;
       var vdomNode = virtualize(domNode);
       var vdomHTML = toHTML(vdomNode);
       snapshot(vdomHTML, {
