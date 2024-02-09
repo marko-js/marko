30561bcba744d9aab99f17f4161e97c2a473a08f
Ryan Turnquist <rturnq@gmail.com>
2024-02-09 12:18:38 -0800

    chore: add test

---
+++ packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/__snapshots__/html.expected/template.js
@@ -0,0 +1,14 @@
+const div = "span";
+const baz = "div";
+import { write as _write, dynamicTagInput as _dynamicTagInput, markResumeControlEnd as _markResumeControlEnd, writeScope as _writeScope, nextScopeId as _nextScopeId, createRenderer as _createRenderer, createTemplate as _createTemplate } from "@marko/runtime-tags/debug/html";
+const _renderer = /* @__PURE__ */_createRenderer((input, _tagVar) => {
+  const _scope0_id = _nextScopeId();
+  _write("<div></div>");
+  const _dynamicScope = _dynamicTagInput(baz, {});
+  _write(`${_markResumeControlEnd(_scope0_id, "#text/0")}`);
+  _writeScope(_scope0_id, {
+    "#text/0!": _dynamicScope,
+    "#text/0(": baz
+  });
+});
+export default /* @__PURE__ */_createTemplate(_renderer, "packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/template.marko");
