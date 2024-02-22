59cf2ff3b91c940e545e934c53893706a6f5da9e
dpiercey <dpiercey@ebay.com>
2024-02-22 13:33:32 -0700

    feat: improve attr tag handling with scriptlets

---
+++ .changeset/friendly-doors-sit.md
@@ -0,0 +1,7 @@
+---
+"@marko/translator-default": patch
+"marko": patch
+"@marko/compiler": patch
+---
+
+Improve nested attribute tag handling with scriptlets.
---
+++ packages/marko/src/runtime/helpers/attr-tag.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var ownerInput;
+
+exports.r = function repeatedAttrTag(targetProperty, attrTagInput) {
+  var prev = ownerInput[targetProperty];
+  if (prev) {
+    prev.push(attrTagInput);
+  } else {
+    ownerInput[targetProperty] = [attrTagInput];
+  }
+};
+exports.a = function repeatableAttrTag(targetProperty, attrTagInput) {
+  var prev = ownerInput[targetProperty];
+  if (prev) {
+    if (Array.isArray(prev)) {
+      prev.push(attrTagInput);
+    } else {
+      ownerInput[targetProperty] = [prev, attrTagInput];
+    }
+  } else {
+    attrTagInput[Symbol.iterator] = selfIterator;
+    ownerInput[targetProperty] = attrTagInput;
+  }
+};
+
+exports.i = function attrTagInput(render, input) {
+  var prevOwnerInput = ownerInput;
+  ownerInput = input || {};
+  try {
+    ownerInput.renderBody = render();
+    return ownerInput;
+  } finally {
+    ownerInput = prevOwnerInput;
+  }
+};
+
+function* selfIterator() {
+  yield this;
+}
