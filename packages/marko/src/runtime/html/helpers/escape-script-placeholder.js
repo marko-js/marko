0b23d4858f9c2e54664a19a03594ff91c99cfbca
dpiercey <dpiercey@ebay.com>
2024-04-13 15:30:47 -0700

    fix: avoid normalizing nullish values for script/style interpolation

--- packages/marko/src/runtime/html/helpers/escape-script-placeholder.js
+++ packages/marko/src/runtime/html/helpers/escape-script-placeholder.js
@@ -20,9 +20,5 @@ const escape = (str) =>
  * arbitrary code.
  */
 module.exports = function escapeScriptHelper(value) {
-  if (value == null) {
-    return "";
-  }
-
   return escape(value + "");
 };
