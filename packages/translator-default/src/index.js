6c84796353d24c1e5b23a089988bccde91f48569
LuLaValva <lukelavalva@gmail.com>
2024-04-19 13:08:17 -0700

    fix: add empty-component to runtime entry files

--- packages/translator-default/src/index.js
+++ packages/translator-default/src/index.js
@@ -504,6 +504,7 @@ export function getRuntimeEntryFiles(output, optimize) {
     `${base}runtime/helpers/render-tag.js`,
     `${base}runtime/helpers/style-value.js`,
     `${base}runtime/helpers/to-string.js`,
+    `${base}runtime/helpers/empty-component.js`,
     `${base}core-tags/components/preserve-tag.js`,
     ...(output === "html"
       ? [
