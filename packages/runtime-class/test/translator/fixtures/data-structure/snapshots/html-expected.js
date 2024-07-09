d1f11d9ed49bbb1d43b1314de669959bc8f4dd82
LuLaValva <lukelavalva@gmail.com>
2024-07-09 09:13:38 -0700

    test(translator): check for shadowing `data` with a tag destructure

---
+++ packages/runtime-class/test/translator/fixtures/data-structure/snapshots/html-expected.js
@@ -0,0 +1,30 @@
+import { t as _t } from "marko/src/runtime/html/index.js";
+const _marko_componentType = "__tests__/template.marko",
+  _marko_template = _t(_marko_componentType);
+export default _marko_template;
+import { a as _marko_repeatable_attr_tag, i as _marko_render_input } from "marko/src/runtime/helpers/attr-tag.js";
+import _await from "marko/src/core-tags/core/await/renderer.js";
+import _marko_tag from "marko/src/runtime/helpers/render-tag.js";
+import _marko_renderer from "marko/src/runtime/components/renderer.js";
+const _marko_component = {};
+_marko_template._ = _marko_renderer(function (input, out, _componentDef, _component, state, $global) {
+  _marko_tag(_await, _marko_render_input(() => {
+    _marko_repeatable_attr_tag("then", {
+      "renderBody": (out, {
+        data
+      }) => {
+        console.log(data);
+      }
+    });
+  }, {
+    "_provider": Promise.resolve({
+      data: true
+    }),
+    "_name": "Promise.resolve({\n  data: true\n})"
+  }), out, _componentDef, "0");
+  console.log(input);
+}, {
+  t: _marko_componentType,
+  i: true,
+  d: true
+}, _marko_component);
