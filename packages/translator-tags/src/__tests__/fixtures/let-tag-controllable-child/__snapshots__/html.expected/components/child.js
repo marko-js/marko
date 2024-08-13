c71bb861a71668fc1da4858908b7251a91b84335
LuLaValva <lukelavalva@gmail.com>
2024-08-13 08:55:50 -0700

    fix: update test

--- packages/translator-tags/src/__tests__/fixtures/let-tag-controllable-child/__snapshots__/html.expected/components/child.js
+++ packages/translator-tags/src/__tests__/fixtures/let-tag-controllable-child/__snapshots__/html.expected/components/child.js
@@ -4,12 +4,14 @@ const _renderer = /* @__PURE__ */_createRenderer((input, _tagVar) => {
   const state = input.value;
   const _state_change = input.valueChange;
   const otherState = input["value"];
+  const _otherState_change = input["value" + "Change"];
   _write(`<button>${_escapeXML(input.value)}${_markResumeNode(_scope0_id, "#text/1")}|<!>${_escapeXML(state)}${_markResumeNode(_scope0_id, "#text/2")}</button>${_markResumeNode(_scope0_id, "#button/0")}<button>${_escapeXML(input.value)}${_markResumeNode(_scope0_id, "#text/4")}|<!>${_escapeXML(otherState)}${_markResumeNode(_scope0_id, "#text/5")}</button>${_markResumeNode(_scope0_id, "#button/3")}`);
-  _writeEffect(_scope0_id, "packages/translator-tags/src/__tests__/fixtures/let-tag-controllable-child/components/child.marko_0_otherState");
+  _writeEffect(_scope0_id, "packages/translator-tags/src/__tests__/fixtures/let-tag-controllable-child/components/child.marko_0__otherState_change_otherState");
   _writeEffect(_scope0_id, "packages/translator-tags/src/__tests__/fixtures/let-tag-controllable-child/components/child.marko_0__state_change_state");
   _writeScope(_scope0_id, {
     "_state_change": _state_change,
     "state": state,
+    "_otherState_change": _otherState_change,
     "otherState": otherState
   });
 });
