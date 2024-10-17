0944f36163c3d03af9c9009f468e0babbcdb10ae
LuLaValva <lukelavalva@gmail.com>
2024-10-17 16:49:04 -0700

    fix: macos filenames are not case sensitive

--- packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/__snapshots__/dom.expected/template.js
+++ packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/__snapshots__/dom.expected/template.js
@@ -1,10 +1,13 @@
-export const _template_ = "<div></div><!>";
-export const _walks_ = /* over(1), replace, over(1) */"b%b";
+export const _template_ = `<div></div>${_foo_template}<!>`;
+export const _walks_ = /* over(1), beginChild, _foo_walks, endChild, replace, over(1) */`b/${_foo_walks}&%b`;
 const div = "span";
-const Baz = "div";
+const foo = "div";
+const Bar = "div";
+import { _setup_ as _foo, _template_ as _foo_template, _walks_ as _foo_walks } from "./components/foo.marko";
 import { conditional as _conditional, createRenderer as _createRenderer, createTemplate as _createTemplate } from "@marko/runtime-tags/debug/dom";
-const _dynamicTagName = /* @__PURE__ */_conditional("#text/0");
+const _dynamicTagName = /* @__PURE__ */_conditional("#text/1");
 export function _setup_(_scope) {
-  _dynamicTagName(_scope, Baz);
+  _foo(_scope["#childScope/0"]);
+  _dynamicTagName(_scope, Bar);
 }
 export default /* @__PURE__ */_createTemplate(/* @__PURE__ */_createRenderer(_template_, _walks_, _setup_), "packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/template.marko");
--- packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/__snapshots__/html.expected/template.js
+++ packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/__snapshots__/html.expected/template.js
@@ -1,15 +1,20 @@
 const div = "span";
-const Baz = "div";
-import { write as _write, dynamicTagInput as _dynamicTagInput, peekNextScope as _peekNextScope, markResumeControlEnd as _markResumeControlEnd, writeExistingScope as _writeExistingScope, writeScope as _writeScope, nextScopeId as _nextScopeId, createRenderer as _createRenderer, createTemplate as _createTemplate } from "@marko/runtime-tags/debug/html";
+const foo = "div";
+const Bar = "div";
+import { write as _write, peekNextScope as _peekNextScope, writeExistingScope as _writeExistingScope, dynamicTagInput as _dynamicTagInput, markResumeControlEnd as _markResumeControlEnd, writeScope as _writeScope, nextScopeId as _nextScopeId, createRenderer as _createRenderer, createTemplate as _createTemplate } from "@marko/runtime-tags/debug/html";
+import _foo from "./components/foo.marko";
 const _renderer = /* @__PURE__ */_createRenderer((input, _tagVar) => {
   const _scope0_id = _nextScopeId();
   _write("<div></div>");
+  const _childScope = _peekNextScope();
+  _foo._({});
   const _dynamicScope = _peekNextScope();
-  _dynamicTagInput(_dynamicScope, Baz, {});
-  _write(`${_markResumeControlEnd(_scope0_id, "#text/0")}`);
+  _dynamicTagInput(_dynamicScope, Bar, {});
+  _write(`${_markResumeControlEnd(_scope0_id, "#text/1")}`);
   _writeScope(_scope0_id, {
-    "#text/0!": _writeExistingScope(_dynamicScope),
-    "#text/0(": Baz
+    "#childScope/0": _writeExistingScope(_childScope),
+    "#text/1!": _writeExistingScope(_dynamicScope),
+    "#text/1(": Bar
   });
 });
 export default /* @__PURE__ */_createTemplate(_renderer, "packages/translator-tags/src/__tests__/fixtures/tag-resolution-priority/template.marko");
