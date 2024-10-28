46524003d436083734f972fa8adfb5ad9d30f700
dpiercey <dpiercey@ebay.com>
2024-10-28 10:03:01 -0700

    refactor: move if translator into single file, and use translateByTarget

---
+++ packages/translator-tags/src/core/if.ts
@@ -0,0 +1,472 @@
+import {
+  assertNoArgs,
+  assertNoParams,
+  assertNoVar,
+  type Tag,
+} from "@marko/babel-utils";
+import { types as t } from "@marko/compiler";
+import { AccessorChar, WalkCode } from "@marko/runtime-tags/common/types";
+
+import { getTagName } from "../util/get-tag-name";
+import { isConditionTag, isCoreTagName } from "../util/is-core-tag";
+import { isStatefulReferences } from "../util/is-stateful";
+import {
+  type Binding,
+  BindingType,
+  createBinding,
+  getScopeAccessorLiteral,
+  mergeReferences,
+} from "../util/references";
+import { callRuntime } from "../util/runtime";
+import {
+  checkStatefulClosures,
+  getOrCreateSection,
+  getScopeIdIdentifier,
+  getSection,
+  type Section,
+  setSectionParentIsOwner,
+} from "../util/sections";
+import {
+  addValue,
+  getClosures,
+  getResumeRegisterId,
+  getSerializedScopeProperties,
+  getSignal,
+  getSignalFn,
+  setForceResumeScope,
+  setSubscriberBuilder,
+  writeHTMLResumeStatements,
+} from "../util/signals";
+import toFirstStatementOrBlock from "../util/to-first-statement-or-block";
+import { translateByTarget } from "../util/visitors";
+import * as walks from "../util/walks";
+import * as writer from "../util/writer";
+import { scopeIdentifier } from "../visitors/program";
+
+const kBinding = Symbol("if node binding");
+const BRANCHES_LOOKUP = new WeakMap<
+  t.NodePath<t.MarkoTag>,
+  [tag: t.NodePath<t.MarkoTag>, bodySection: Section][]
+>();
+
+declare module "@marko/compiler/dist/types" {
+  export interface MarkoTagExtra {
+    [kBinding]?: Binding;
+  }
+}
+
+export const IfTag = {
+  analyze(tag) {
+    assertValidCondition(tag);
+
+    if (isRoot(tag)) {
+      const tagExtra = (tag.node.extra ??= {});
+      tagExtra[kBinding] = createBinding(
+        "#text",
+        BindingType.dom,
+        getOrCreateSection(tag),
+        undefined,
+        tagExtra,
+      );
+    }
+
+    const [isLast, branches] = getBranches(
+      tag,
+      getOrCreateSection(tag.get("body")),
+    );
+    if (isLast) {
+      const [rootTag] = branches[0];
+      const rootExtra = rootTag.node.extra!;
+      const mergeReferenceNodes: t.Node[] = [];
+      let singleNodeOptimization = true;
+
+      for (const [branchTag, branchBodySection] of branches) {
+        branchBodySection.upstreamExpression = rootExtra;
+
+        if (
+          !(
+            branchBodySection.content === null ||
+            branchBodySection.content?.singleChild
+          )
+        ) {
+          singleNodeOptimization = false;
+        }
+
+        if (branchTag.node.attributes.length) {
+          mergeReferenceNodes.push(branchTag.node.attributes[0].value);
+        }
+      }
+
+      rootExtra.singleNodeOptimization = singleNodeOptimization;
+      mergeReferences(rootTag, mergeReferenceNodes);
+    }
+  },
+  translate: translateByTarget({
+    html: {
+      enter(tag) {
+        const tagBody = tag.get("body");
+        const bodySection = getSection(tagBody);
+        const rootExtra = getRoot(tag).node.extra!;
+        const isStateful = isStatefulReferences(rootExtra.referencedBindings);
+        const singleNodeOptimization = rootExtra.singleNodeOptimization;
+        setSectionParentIsOwner(bodySection, true);
+
+        if (isRoot(tag)) {
+          walks.visit(tag, WalkCode.Replace);
+        }
+
+        walks.enterShallow(tag);
+        writer.flushBefore(tag);
+
+        if (isStateful && !singleNodeOptimization) {
+          writer.writeTo(tagBody)`${callRuntime(
+            "markResumeScopeStart",
+            getScopeIdIdentifier(bodySection),
+          )}`;
+        }
+      },
+      exit(tag) {
+        const tagBody = tag.get("body");
+        const section = getSection(tag);
+        const bodySection = getSection(tagBody);
+        const [isLast, branches] = getBranches(tag, bodySection);
+        const rootExtra = branches[0][0].node.extra!;
+        const nodeRef = rootExtra[kBinding]!;
+        const isStateful = isStatefulReferences(rootExtra.referencedBindings);
+        const singleNodeOptimization = rootExtra.singleNodeOptimization;
+        const hasStatefulClosures = checkStatefulClosures(bodySection, true);
+
+        if (isStateful || hasStatefulClosures) {
+          setForceResumeScope(bodySection);
+        }
+        writer.flushInto(tag);
+        // TODO: this is a hack to get around the fact that we don't have a way to
+        // know if a scope requires dynamic subscriptions
+        setSubscriberBuilder(tag, (() => {}) as any);
+        writeHTMLResumeStatements(tagBody);
+
+        if (isLast) {
+          const write = writer.writeTo(tag);
+          const nextTag = tag.getNextSibling();
+          const ifScopeIdIdentifier =
+            tag.scope.generateUidIdentifier("ifScopeId");
+          const ifRendererIdentifier =
+            tag.scope.generateUidIdentifier("ifRenderer");
+
+          let statement: t.Statement | undefined;
+          for (let i = branches.length; i--; ) {
+            const [branchTag, branchBodySection] = branches[i];
+            const branchHasStatefulClosures = checkStatefulClosures(
+              branchBodySection,
+              true,
+            );
+
+            if (isStateful) {
+              branchTag.node.body.body.push(
+                t.expressionStatement(
+                  callRuntime(
+                    "register",
+                    t.assignmentExpression(
+                      "=",
+                      ifRendererIdentifier,
+                      callRuntime(
+                        "createRenderer",
+                        t.arrowFunctionExpression([], t.blockStatement([])),
+                      ),
+                    ),
+                    t.stringLiteral(
+                      getResumeRegisterId(branchBodySection, "renderer"),
+                    ),
+                  ),
+                ) as any,
+              );
+            }
+            if (isStateful || branchHasStatefulClosures) {
+              branchTag.node.body.body.push(
+                t.expressionStatement(
+                  t.assignmentExpression(
+                    "=",
+                    ifScopeIdIdentifier,
+                    getScopeIdIdentifier(branchBodySection),
+                  ),
+                ) as any,
+              );
+            }
+
+            const [testAttr] = branchTag.node.attributes;
+            const curStatement = toFirstStatementOrBlock(
+              branchTag.node.body.body,
+            );
+
+            if (testAttr) {
+              statement = t.ifStatement(
+                testAttr.value,
+                curStatement,
+                statement,
+              );
+            } else {
+              statement = curStatement;
+            }
+
+            branchTag.remove();
+          }
+
+          if (!(isStateful || hasStatefulClosures)) {
+            nextTag.insertBefore(statement!);
+          } else {
+            nextTag.insertBefore([
+              t.variableDeclaration(
+                "let",
+                [
+                  t.variableDeclarator(ifScopeIdIdentifier),
+                  isStateful && t.variableDeclarator(ifRendererIdentifier),
+                ].filter(Boolean) as t.VariableDeclarator[],
+              ),
+              statement!,
+            ]);
+            if (isStateful) {
+              if (singleNodeOptimization) {
+                write`${callRuntime(
+                  "markResumeControlSingleNodeEnd",
+                  getScopeIdIdentifier(section),
+                  getScopeAccessorLiteral(nodeRef),
+                  ifScopeIdIdentifier,
+                )}`;
+              } else {
+                write`${callRuntime(
+                  "markResumeControlEnd",
+                  getScopeIdIdentifier(section),
+                  getScopeAccessorLiteral(nodeRef),
+                )}`;
+              }
+              getSerializedScopeProperties(section).set(
+                t.stringLiteral(
+                  getScopeAccessorLiteral(nodeRef).value +
+                    AccessorChar.ConditionalRenderer,
+                ),
+                ifRendererIdentifier,
+              );
+            }
+            getSerializedScopeProperties(section).set(
+              t.stringLiteral(
+                getScopeAccessorLiteral(nodeRef).value +
+                  AccessorChar.ConditionalScope,
+              ),
+              callRuntime("getScopeById", ifScopeIdIdentifier),
+            );
+          }
+        }
+      },
+    },
+    dom: {
+      enter(tag) {
+        setSectionParentIsOwner(getSection(tag.get("body")), true);
+
+        if (isRoot(tag)) {
+          walks.visit(tag, WalkCode.Replace);
+        }
+
+        walks.enterShallow(tag);
+      },
+      exit(tag) {
+        const [isLast, branches] = getBranches(
+          tag,
+          getSection(tag.get("body")),
+        );
+
+        if (isLast) {
+          const section = getSection(tag);
+          const rootExtra = branches[0][0].node.extra!;
+          const nodeRef = rootExtra[kBinding]!;
+          let expr: t.Expression = t.nullLiteral();
+
+          for (let i = branches.length; i--; ) {
+            const [branchTag, branchBodySection] = branches[i];
+            const [testAttr] = branchTag.node.attributes;
+            const id = t.identifier(branchBodySection.name);
+
+            setSubscriberBuilder(branchTag, (subscriber) => {
+              return callRuntime(
+                "inConditionalScope",
+                subscriber,
+                getScopeAccessorLiteral(nodeRef),
+                /*t.identifier(section.name)*/
+              );
+            });
+
+            branchTag.remove();
+            expr = testAttr
+              ? t.conditionalExpression(testAttr.value, id, expr)
+              : id;
+          }
+
+          const signal = getSignal(section, nodeRef, "if");
+          signal.build = () => {
+            return callRuntime(
+              "conditional",
+              getScopeAccessorLiteral(nodeRef),
+              getSignalFn(signal, [scopeIdentifier]),
+            );
+          };
+          signal.hasDownstreamIntersections = () =>
+            branches.some(
+              ([, bodySection]) => getClosures(bodySection).length > 0,
+            );
+          addValue(section, rootExtra.referencedBindings, signal, expr);
+        }
+      },
+    },
+  }),
+  autocomplete: [
+    {
+      snippet: "if=${1:condition}",
+      description: "Use to display content only if the condition is met.",
+      descriptionMoreURL: "https://markojs.com/docs/core-tags/#if-else-if-else",
+    },
+  ],
+} satisfies Tag;
+
+export const ElseIfTag = {
+  ...IfTag,
+  autocomplete: [
+    {
+      snippet: "else-if=${1:condition}",
+      description:
+        "Use after an <if> or <else-if> tag to display content if those conditions do not match and this one does.",
+      descriptionMoreURL: "https://markojs.com/docs/core-tags/#if-else-if-else",
+    },
+  ],
+};
+
+export const ElseTag = {
+  ...IfTag,
+  autocomplete: [
+    {
+      description:
+        "Use after an <if> or <else-if> tag to display content if those conditions do not match.",
+      descriptionMoreURL: "https://markojs.com/docs/core-tags/#if-else-if-else",
+    },
+  ],
+};
+
+function assertValidCondition(tag: t.NodePath<t.MarkoTag>) {
+  assertNoVar(tag);
+  assertNoArgs(tag);
+  assertNoParams(tag);
+  assertHasBody(tag);
+
+  switch (getTagName(tag)) {
+    case "if":
+      assertHasValueAttribute(tag);
+      break;
+    case "else-if":
+      assertHasValueAttribute(tag);
+      assertHasPrecedingCondition(tag);
+      break;
+    case "else":
+      assertOptionalIfAttribute(tag);
+      assertHasPrecedingCondition(tag);
+      break;
+  }
+}
+
+function assertHasPrecedingCondition(tag: t.NodePath<t.MarkoTag>) {
+  let prev = tag.getPrevSibling();
+  while (prev.node && prev.isMarkoComment()) prev = prev.getPrevSibling();
+
+  if (
+    !isConditionTag(prev) ||
+    (getTagName(prev) !== "else" && !prev.node.attributes.length)
+  ) {
+    throw tag.buildCodeFrameError(
+      `The \`<${getTagName(tag)}>\` must have a preceding \`<if=cond>\`, \`<else-if=cond>\`, or \`<else if=cond>\`.`,
+    );
+  }
+}
+
+function assertHasBody(tag: t.NodePath<t.MarkoTag>) {
+  if (!tag.node.body.body.length) {
+    throw tag
+      .get("name")
+      .buildCodeFrameError(
+        `The \`${getTagName(tag)}\` tag requires body content.`,
+      );
+  }
+}
+
+function assertHasValueAttribute(tag: t.NodePath<t.MarkoTag>) {
+  const { node } = tag;
+  const [valueAttr] = node.attributes;
+
+  if (!t.isMarkoAttribute(valueAttr) || !valueAttr.default) {
+    throw tag
+      .get("name")
+      .buildCodeFrameError(`The \`${getTagName(tag)}\` tag requires a value.`);
+  }
+
+  if (node.attributes.length > 1) {
+    const start = node.attributes[1].loc?.start;
+    const end = node.attributes[node.attributes.length - 1].loc?.end;
+    const msg = `The \`${getTagName(tag)}\` tag only supports the \`value\` attribute.`;
+
+    if (start == null || end == null) {
+      throw tag.get("name").buildCodeFrameError(msg);
+    } else {
+      throw tag.hub.buildError(
+        { loc: { start, end } } as unknown as t.Node,
+        msg,
+        Error,
+      );
+    }
+  }
+}
+
+function assertOptionalIfAttribute(tag: t.NodePath<t.MarkoTag>) {
+  const { node } = tag;
+  const [ifAttr] = node.attributes;
+
+  if (
+    node.attributes.length > 1 ||
+    (ifAttr && (ifAttr as t.MarkoAttribute).name !== "if")
+  ) {
+    const start = node.attributes[1].loc?.start;
+    const end = node.attributes[node.attributes.length - 1].loc?.end;
+    const msg = `The \`${getTagName(tag)}\` tag only supports an \`if=\` attribute.`;
+
+    if (start == null || end == null) {
+      throw tag.get("name").buildCodeFrameError(msg);
+    } else {
+      throw tag.hub.buildError(
+        { loc: { start, end } } as unknown as t.Node,
+        msg,
+        Error,
+      );
+    }
+  }
+}
+
+function getBranches(tag: t.NodePath<t.MarkoTag>, bodySection: Section) {
+  const branches = BRANCHES_LOOKUP.get(tag) ?? [];
+  let nextTag = tag.getNextSibling();
+  while (nextTag.isMarkoComment()) nextTag = nextTag.getNextSibling();
+
+  const isLast = !(
+    isCoreTagName(nextTag, "else") || isCoreTagName(nextTag, "else-if")
+  );
+
+  branches.push([tag, bodySection]);
+
+  if (!isLast) {
+    BRANCHES_LOOKUP.set(nextTag as t.NodePath<t.MarkoTag>, branches);
+  }
+
+  return [isLast, branches] as const;
+}
+
+function getRoot(tag: t.NodePath<t.MarkoTag>) {
+  return isRoot(tag) ? tag : BRANCHES_LOOKUP.get(tag)![0][0];
+}
+
+function isRoot(tag: t.NodePath<t.MarkoTag>) {
+  return isCoreTagName(tag, "if");
+}
