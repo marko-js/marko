993e6e08eeb13ff455bca98aabb61c5fec161b04
dpiercey <dpiercey@ebay.com>
2025-02-08 15:01:57 -0700

    fix: include aliases in stateful ref checks

--- packages/runtime-tags/src/__tests__/fixtures/error-const-mutation/__snapshots__/html.expected/template.js
+++ packages/runtime-tags/src/__tests__/fixtures/error-const-mutation/__snapshots__/html.expected/template.js
@@ -7,6 +7,6 @@ const _renderer = /* @__PURE__ */_$.createRenderer(input => {
     lastName: "Martin"
   };
   const fullName = user.fullName = `${user.firstName} ${user.middleName} ${user.lastName}`;
-  _$.write(`<p>${_$.escapeXML(fullName)}${_$.markResumeNode(_scope0_id, "#text/0")}</p>`);
+  _$.write(`<p>${_$.escapeXML(fullName)}</p>`);
 });
 export default /* @__PURE__ */_$.createTemplate("__tests__/template.marko", _renderer);
