da0da1a4aa651aeb31f40a038ab4462c2c85f85e
dpiercey <dpiercey@ebay.com>
2025-03-15 14:30:40 -0700

    fix: optimize html-script and html-style

--- packages/runtime-tags/src/__tests__/fixtures/html-script/__snapshots__/dom.expected/template.hydrate.js
+++ packages/runtime-tags/src/__tests__/fixtures/html-script/__snapshots__/dom.expected/template.hydrate.js
@@ -1,20 +1,14 @@
-// size: 310 (min) 189 (brotli)
-const _count$htmlScript_content = _$.dynamicClosureRead(1);
-_$.registerContent("a1", 0, 0, 0, 0, (_scope) =>
-  _count$htmlScript_content(_scope),
-);
-const _count_closure = _$.dynamicClosure(_count$htmlScript_content),
-  _count_effect = _$.effect("a2", (_scope, { 1: count }) =>
+// size: 209 (min) 150 (brotli)
+const _count_effect = _$.effect("a1", (_scope, { 2: count }) =>
     _$.on(_scope[0], "click", function () {
       _count(_scope, count + 1);
     }),
   ),
-  _count = _$.state(1, (_scope, count) => {
+  _count = _$.state(2, (_scope, count) => {
     _$.textContent(
       _scope[0],
       `\n  {\n    "imports": {\n      "${count}": "https://markojs.com",\n    }\n  }\n`,
     ),
-      _count_closure(_scope),
       _count_effect(_scope);
   });
 init();
