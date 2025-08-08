6a75d80655601e5dadfa4972455bea7d3b4aa28b
dpiercey <dpiercey@ebay.com>
2025-08-08 13:43:52 -0700

    fix: treat optionalCallExpressions as callExpressions for analysis

--- packages/runtime-tags/src/translator/util/is-invoked-function.ts
+++ packages/runtime-tags/src/translator/util/is-invoked-function.ts
@@ -3,13 +3,14 @@ import { types as t } from "@marko/compiler";
 export default function isInvokedFunction(
   expr: t.NodePath<t.Node>,
 ): expr is typeof expr & {
-  parent: t.CallExpression;
+  parent: t.CallExpression | t.OptionalCallExpression;
   parentPath: t.NodePath<t.CallExpression>;
 } {
   let curPath: t.NodePath<t.Node> | null = expr;
   while (curPath) {
     const { parent, node } = curPath;
     switch (parent.type) {
+      case "OptionalCallExpression":
       case "CallExpression":
         return parent.callee === node;
       case "TSNonNullExpression":
