9ba0cab6474e7cd483b93992be5b8b5e7c8df0aa
dpiercey <dpiercey@ebay.com>
2025-08-13 09:12:05 -0700

    chore: update deps, improve static statement source locations

---
+++ .changeset/loud-falcons-travel.md
@@ -0,0 +1,6 @@
+---
+"marko": patch
+"@marko/runtime-tags": patch
+---
+
+Improve source location information for static statements.
--- packages/runtime-class/src/translator/taglib/core/parse-static.js
+++ packages/runtime-class/src/translator/taglib/core/parse-static.js
@@ -7,8 +7,8 @@ export default function (path) {
     hub: { file },
   } = path;
   const { rawValue, end } = node;
-  const code = rawValue.replace(/^static\s*/, "").trim();
-  const start = node.name.start + (rawValue.length - code.length);
+  const code = rawValue.replace(/^static\s*/, "");
+  const start = node.start + (rawValue.length - code.length);
   let body = parseStatements(file, code, start, end);
   if (body.length === 1 && t.isBlockStatement(body[0])) {
     body = body[0].body;
