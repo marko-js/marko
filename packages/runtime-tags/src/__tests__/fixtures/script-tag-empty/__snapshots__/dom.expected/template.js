9fbd40640d4e5466d36f52913227f5d72744f811
dpiercey <dpiercey@ebay.com>
2025-09-04 17:35:37 -0700

    fix: avoid compile error on empty script tag

---
+++ packages/runtime-tags/src/__tests__/fixtures/script-tag-empty/__snapshots__/dom.expected/template.js
@@ -0,0 +1,5 @@
+export const $template = "";
+export const $walks = "";
+export const $setup = () => {};
+import * as _ from "@marko/runtime-tags/debug/dom";
+export default /* @__PURE__ */_._template("__tests__/template.marko", $template, $walks, $setup);
