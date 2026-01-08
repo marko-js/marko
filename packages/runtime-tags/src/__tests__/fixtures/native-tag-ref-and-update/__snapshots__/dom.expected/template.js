f424e4e8a86b57eb83f03160f2bc39cd2aed6a5b
dpiercey <dpiercey@ebay.com>
2026-01-08 09:26:50 -0700

    fix: store section with scope reads

---
+++ packages/runtime-tags/src/__tests__/fixtures/native-tag-ref-and-update/__snapshots__/dom.expected/template.js
@@ -0,0 +1,11 @@
+export const $template = "<button>Initial</button>";
+export const $walks = /* get, over(1) */" b";
+import * as _ from "@marko/runtime-tags/debug/dom";
+const $getbutton = _._el("__tests__/template.marko_0/#button", "#button/0");
+const $count = /* @__PURE__ */_._let("count/1", $scope => _._attr($scope["#button/0"], "data-count", $scope.count));
+const $setup__script = _._script("__tests__/template.marko_0", $scope => ((0, $getbutton($scope))().textContent = "after"));
+export function $setup($scope) {
+  $count($scope, 0);
+  $setup__script($scope);
+}
+export default /* @__PURE__ */_._template("__tests__/template.marko", $template, $walks, $setup);
---
+++ packages/runtime-tags/src/__tests__/fixtures/native-tag-ref-and-update/__snapshots__/resume.expected.md
@@ -0,0 +1,28 @@
+# Render
+```html
+<html>
+  <head />
+  <body>
+    <button
+      data-count="0"
+    >
+      after
+    </button>
+    <!--M_*1 #button/0-->
+    <script>
+      WALKER_RUNTIME("M")("_");
+      M._.r = [_ =&gt; (_.a = [0,
+        {}]),
+        "__tests__/template.marko_0 1"
+      ];
+      M._.w()
+    </script>
+  </body>
+</html>
+```
+
+# Mutations
+```
+REMOVE #text in html/body/button
+INSERT html/body/button/#text
+```
