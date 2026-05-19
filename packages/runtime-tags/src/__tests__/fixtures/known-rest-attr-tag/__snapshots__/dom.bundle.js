c75a92c72a218434bafd9102bfde98ed4fe79052
Voon Wong <voon.wong@smartbear.com>
2026-05-19 14:55:11 +1000

    chore: restore some snapshots

--- packages/runtime-tags/src/__tests__/fixtures/known-rest-attr-tag/__snapshots__/dom.bundle.js
+++
@@ -1,7 +0,0 @@
-// total: 4548 (min) 1935 (brotli)
-// tags/child.marko: 38 (min) 42 (brotli)
-const $htmlInput__script = _script("b0", ($scope) => _attrs_script($scope, "a"));
-
-// template.marko: 30 (min) 34 (brotli)
-function $onClick() {}
-_resume("a0", $onClick);
