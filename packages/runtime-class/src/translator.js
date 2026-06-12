2999ffcb59082b7405fe2641586b5c7bd19781e1
Dylan Piercey <dpiercey@ebay.com>
2026-06-12 14:49:48 -0700

    fix: checking import attributes when created through babels builders

--- packages/runtime-class/src/translator.js
+++ packages/runtime-class/src/translator.js
@@ -3,6 +3,7 @@ import * as tagsAPI from "@marko/runtime-tags/translator";
 import * as classAPI from "./translator/index";
 
 export const {
+  version,
   preferAPI,
   tagDiscoveryDirs,
   transform,
