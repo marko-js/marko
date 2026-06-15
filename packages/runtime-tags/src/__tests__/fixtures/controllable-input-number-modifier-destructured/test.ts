91fbe09a9a60e7ec81817a81dd8fc8efc60a3e47
Dylan Piercey <dpiercey@ebay.com>
2026-06-15 14:49:23 -0700

    fix: issue with bound refinement with const change handler

---
+++ packages/runtime-tags/src/__tests__/fixtures/controllable-input-number-modifier-destructured/test.ts
@@ -0,0 +1,14 @@
+import type { TestConfig } from "../../main.test";
+
+function type(value: string) {
+  return (container: Element) => {
+    const input = container.querySelector("input")!;
+    const window = input.ownerDocument.defaultView!;
+    input.value = value;
+    input.dispatchEvent(new window.Event("input", { bubbles: true }));
+  };
+}
+
+export const config: TestConfig = {
+  steps: [{}, type("1"), type("10")],
+};
