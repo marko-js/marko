2499bb62b95e8cd896eafb09ed9a47c28a7868bd
Dylan Piercey <pierceydylan@gmail.com>
2026-07-27 10:17:58 -0700

    test(runtime-tags): cover a void element as a dynamic tag

---
+++ packages/runtime-tags/src/__tests__/fixtures/dynamic-tag-native-variants/__snapshots__/dom.bundle.debug.js
@@ -0,0 +1,23 @@
+// template.marko
+const $template = "<div><!><select><option value=a>a</option></select><button>Swap</button></div>";
+const $walks = "D%b b l";
+const $dynamicTag = /*@__PURE__*/ _dynamic_tag("#text/0");
+const $tag = /*@__PURE__*/ _let("tag/3", ($scope) => $dynamicTag($scope, $scope.tag));
+const $setup__script = _script("__tests__/template.marko_0", ($scope) => {
+	_attr_select_value_script($scope, "#select/1");
+	_on($scope["#button/2"], "click", function() {
+		$tag($scope, $scope.tag === "input" ? "br" : "input");
+	});
+});
+function $setup($scope) {
+	_attr_select_value($scope, "#select/1", void 0, $valueChange($scope));
+	$tag($scope, "input");
+	$setup__script($scope);
+}
+function $valueChange($scope) {
+	return function(next) {
+		$tag($scope, next);
+	};
+}
+_resume("__tests__/template.marko_0/valueChange", $valueChange);
+var template_default = /*@__PURE__*/ _template("__tests__/template.marko", $template, $walks, $setup);
---
+++ packages/runtime-tags/src/__tests__/fixtures/dynamic-tag-native-variants/__snapshots__/dom.bundle.js
@@ -0,0 +1,15 @@
+// template.marko
+const $dynamicTag = /*@__PURE__*/ _dynamic_tag(0);
+const $tag = /*@__PURE__*/ _let(3, ($scope) => $dynamicTag($scope, $scope.d));
+const $setup__script = _script("a1", ($scope) => {
+	_attr_select_value_script($scope, "b");
+	_on($scope.c, "click", function() {
+		$tag($scope, $scope.d === "input" ? "br" : "input");
+	});
+});
+function $valueChange($scope) {
+	return function(next) {
+		$tag($scope, next);
+	};
+}
+_resume("a0", $valueChange);
