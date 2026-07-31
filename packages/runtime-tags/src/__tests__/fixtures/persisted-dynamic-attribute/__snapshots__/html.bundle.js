b6ca8226769b8b0e761d9a0275c9e8bd0d678d19
Dylan Piercey <pierceydylan@gmail.com>
2026-07-31 11:28:18 -0700

    feat(runtime-tags): add persisted attribute patches

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-dynamic-attribute/__snapshots__/html.bundle.js
@@ -0,0 +1,7 @@
+// template.marko
+var template_default = _template_persisted("a", (input) => {
+	_scope_reason();
+	const $scope0_id = _scope_id();
+	_html(`<a${_patch_attr($scope0_id, "a", "href", input.href)}${_attr("href", input.href)}${_patch_attr($scope0_id, "a", "title", input.title)}${_attr("title", input.title)}${_patch_attr($scope0_id, "a", "hidden", input.hidden)}${_attr("hidden", input.hidden)}>${_patch_text($scope0_id, "b", input.label)}${_escape(input.label)}${_el_resume($scope0_id, "b")}</a>${_el_resume($scope0_id, "a")}`);
+	writeScope($scope0_id, {});
+}, 1);
