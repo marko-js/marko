622777a9f764078d5258057a92cd4045bc8d160f
Dylan Piercey <pierceydylan@gmail.com>
2026-07-31 11:28:18 -0700

    feat(runtime-tags): add persisted attribute patches

--- packages/runtime-tags/src/dom/controllable.ts
+++ packages/runtime-tags/src/dom/controllable.ts
@@ -2,7 +2,7 @@ import {
   assertHandlerIsFunction,
   assertNoValueBindingOnCheckable,
 } from "../common/errors";
-import { isNotVoid } from "../common/helpers";
+import { isNotVoid, normalizeAttrValue } from "../common/helpers";
 import {
   type Accessor,
   AccessorPrefix,
@@ -10,7 +10,7 @@ import {
   ControlledType,
   type Scope,
 } from "../common/types";
-import { _attr, normalizeAttrValue } from "./dom";
+import { _attr } from "./dom";
 import { delegate } from "./event";
 import { pendingEffects, run, runId } from "./queue";
 import { resolveCursorPosition } from "./resolve-cursor-position";
