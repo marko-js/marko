a4a5b2fe4a07802131ad4b5ae0260cebd4295620
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 14:39:06 -0700

    feat(runtime-tags): persisted stateful construction (experimental)

--- packages/runtime-tags/src/__tests__/fixtures/persisted-client-intersections/__snapshots__/dom.bundle.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-client-intersections/__snapshots__/dom.bundle.debug.js
@@ -1,7 +1,7 @@
 // tags/price-card.marko
 const $template$3 = "<section><h2> </h2><button>+</button></section>";
 const $walks$3 = "E l l";
-const $input_label__OR__qty = /*@__PURE__*/ _fillable("__tests__/tags/price-card.marko0", "input_label", /*@__PURE__*/ _or(6, ($scope) => _text($scope["#text/0"], $scope.input_label + " x" + $scope.qty)));
+const $input_label__OR__qty = /*@__PURE__*/ _fill_join("__tests__/tags/price-card.marko0", "input_label", /*@__PURE__*/ _or(6, ($scope) => _text($scope["#text/0"], $scope.input_label + " x" + $scope.qty)));
 const $qty = /*@__PURE__*/ _let("qty/5", $input_label__OR__qty);
 const $setup__script$1 = _script("__tests__/tags/price-card.marko_0", ($scope) => _on($scope["#button/1"], "click", function() {
 	$qty($scope, $scope.qty + 1);
@@ -10,14 +10,14 @@ function $setup$3($scope) {
 	$qty($scope, 1);
 	$setup__script$1($scope);
 }
-const $input_label$1 = /*@__PURE__*/ _const("input_label", $input_label__OR__qty);
+const $input_label$1 = /*@__PURE__*/ _fill_const("__tests__/tags/price-card.marko0", "input_label", $input_label__OR__qty);
 const $input$3 = ($scope, input) => $input_label$1($scope, input.label);
 var price_card_default = /*@__PURE__*/ _template("__tests__/tags/price-card.marko", $template$3, $walks$3, $setup$3, $input$3);
 
 // tags/promo-tag.marko
 const $template$2 = "<aside> </aside><button class=promo>seen</button>";
 const $walks$2 = "D l b";
-const $input_text__OR__seen = /*@__PURE__*/ _fillable("__tests__/tags/promo-tag.marko0", "input_text", /*@__PURE__*/ _or(6, ($scope) => _text($scope["#text/0"], $scope.input_text + " (" + $scope.seen + ")")));
+const $input_text__OR__seen = /*@__PURE__*/ _fill_join("__tests__/tags/promo-tag.marko0", "input_text", /*@__PURE__*/ _or(6, ($scope) => _text($scope["#text/0"], $scope.input_text + " (" + $scope.seen + ")")));
 const $seen = /*@__PURE__*/ _let("seen/5", $input_text__OR__seen);
 const $setup__script = _script("__tests__/tags/promo-tag.marko_0", ($scope) => _on($scope["#button/1"], "click", function() {
 	$seen($scope, $scope.seen + 1);
@@ -26,7 +26,7 @@ function $setup$2($scope) {
 	$seen($scope, 0);
 	$setup__script($scope);
 }
-const $input_text = /*@__PURE__*/ _const("input_text", $input_text__OR__seen);
+const $input_text = /*@__PURE__*/ _fill_const("__tests__/tags/promo-tag.marko0", "input_text", $input_text__OR__seen);
 const $input$2 = ($scope, input) => $input_text($scope, input.text);
 var promo_tag_default = /*@__PURE__*/ _template("__tests__/tags/promo-tag.marko", $template$2, $walks$2, $setup$2, $input$2);
 
