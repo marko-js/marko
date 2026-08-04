33a46fd5ed41f07c8cf377df27be8cc4cdbca771
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 14:39:06 -0700

    feat(runtime-tags): persisted stateful construction (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-mixed/__snapshots__/html.bundle.debug.js
@@ -0,0 +1,29 @@
+// template.marko
+_renderer_shells({ "__tests__/template.marko_1_shell": ",`__tests__/template.marko_1_shell __tests__/template.marko_1;Db%l ;<p>Seen <!></p><button class=inner>+</button>`" });
+var template_default = _template_persisted("__tests__/template.marko", (input) => {
+	const $scope0_reason = _persisted_reason(), $sg__input_show = _serialize_guard($scope0_reason, 0);
+	const $scope0_id = _scope_id();
+	let count = 0;
+	_html(`<div><h1>${_escape(input.title + " #" + count)}${_el_resume($scope0_id, "#text/0")}</h1><button class=root>+</button>${_el_resume($scope0_id, "#button/1")}`);
+	_if(() => {
+		if (input.show) {
+			const $scope1_id = _scope_id();
+			let n = 0;
+			_html(`<p>Seen <!>${_escape(n)}${_el_resume($scope1_id, "#text/0")}</p><button class=inner>+</button>${_el_resume($scope1_id, "#button/1")}`);
+			_script($scope1_id, "__tests__/template.marko_1");
+			_patch_value($scope1_id, "__tests__/template.marko1", n, 1);
+			writeScope($scope1_id, { n }, "__tests__/template.marko", "5:4", { n: "6:10" });
+			return 0;
+		}
+	}, $scope0_id, "#text/2", $sg__input_show, $sg__input_show, $sg__input_show, void 0, void 0, ["__tests__/template.marko_1_shell"]);
+	_html("</div>");
+	_script($scope0_id, "__tests__/template.marko_0");
+	$scope0_reason ? writeScope($scope0_id, {
+		input_title: input.title,
+		count
+	}, "__tests__/template.marko", 0, {
+		input_title: ["input.title"],
+		count: "1:6"
+	}) : _patch_value($scope0_id, "__tests__/template.marko0", input.title);
+	_resume_branch($scope0_id);
+}, 1);
