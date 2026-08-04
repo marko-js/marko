44fa84ba9fa8f167fb7e327ba522f440fe93d06b
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 14:39:06 -0700

    feat(runtime-tags): persisted stateful construction (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-siblings/__snapshots__/patches.js
@@ -0,0 +1,12 @@
+[`a0 a2;Db%l ;<p class=pa>A <!></p><button class=ba>+</button>`,`a1 a3;Db%l ;<p class=pb>B <!></p><button class=bb>+</button>`,{ta:"Store!",bb:[{f:{va0:0}},"a0"],bc:[{f:{va1:10}},"a1"]}]
+
+
+// PATCH
+
+[`a1 a3;Db%l ;<p class=pb>B <!></p><button class=bb>+</button>`,{ta:"Store!",bb:0,bc:[{f:{va1:10}},"a1"]}]
+
+
+// PATCH
+
+[`a0 a2;Db%l ;<p class=pa>A <!></p><button class=ba>+</button>`,`a1 a3;Db%l ;<p class=pb>B <!></p><button class=bb>+</button>`,{ta:"Store!",bb:[{f:{va0:0}},"a0"],bc:[{f:{va1:10}},"a1"]}]
+
