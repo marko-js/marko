2694c3e1131886919a403437b92da6e0c557cf87
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 15:51:46 -0700

    test(runtime-tags): persisted loops with item-local state (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-index/__snapshots__/html.bundle.js
@@ -0,0 +1,17 @@
+// template.marko
+_renderer_shells({ a0: ",`a0 a1;D bD l ;<li> <span> </span><button>+</button></li>`" });
+var template_default = _template_persisted("a", (input) => {
+	const $scope0_reason = _persisted_reason(), $sg__input_labels = _serialize_guard($scope0_reason, 0);
+	const $scope0_id = _scope_id();
+	_html("<ul>");
+	_for_of(input.labels, (label) => {
+		const $scope1_id = _scope_id();
+		let picks = 0;
+		_html(`<li>${_patch_text($scope1_id, "a", label)}${_escape(label)}${_el_resume($scope1_id, "a")}<span>${_escape(picks)}${_el_resume($scope1_id, "b")}</span><button>+</button>${_el_resume($scope1_id, "c")}</li>`);
+		_script($scope1_id, "a1");
+		_patch_value($scope1_id, "a0", picks, 1);
+		writeScope($scope1_id, { f: picks });
+	}, 0, $scope0_id, "a", $sg__input_labels, $sg__input_labels, $sg__input_labels, void 0, void 0, "a0");
+	_html(`</ul>${_el_resume($scope0_id, "a", $sg__input_labels)}`);
+	$scope0_reason && writeScope($scope0_id, {});
+}, 1);
