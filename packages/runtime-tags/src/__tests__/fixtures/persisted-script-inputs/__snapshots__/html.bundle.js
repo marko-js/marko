bbfc889544db525a1bcebd8fea7286983afa39b6
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 16:03:41 -0700

    feat(runtime-tags): persisted effect-read fills (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-script-inputs/__snapshots__/html.bundle.js
@@ -0,0 +1,12 @@
+// template.marko
+var template_default = _template_persisted("a", (input) => {
+	const $scope0_reason = _persisted_reason();
+	const $scope0_id = _scope_id();
+	_html(`<main><h1>${_patch_text($scope0_id, "a", input.title)}${_escape(input.title)}${_el_resume($scope0_id, "a")}</h1></main>`);
+	_script($scope0_id, "a0");
+	_patch_effect($scope0_id, "a0", "e f");
+	$scope0_reason ? writeScope($scope0_id, {
+		e: input.a,
+		f: input.b
+	}) : (_patch_write($scope0_id, "e", input.a), _patch_write($scope0_id, "f", input.b));
+}, 1);
