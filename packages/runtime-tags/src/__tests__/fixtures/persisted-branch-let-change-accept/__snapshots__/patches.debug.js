b6484345299ac1ca7d1ed2f35c144526724f6387
Dylan Piercey <pierceydylan@gmail.com>
2026-08-05 15:29:42 -0700

    chore(runtime-tags): rename PatchFresh entries to PatchSetup (experimental)

--- packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/__snapshots__/patches.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/__snapshots__/patches.debug.js
@@ -8,7 +8,7 @@
 [`packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/template.marko_1_shell packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/template.marko_1;Db%l ;<span>Seen <!></span><button>+</button>`, {
   "PatchText:#text/0": "Store!",
   "PatchBranch:#text/2": [{
-    "PatchFresh:": {
+    "PatchSetup:": {
       "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/template.marko0": 0
     },
     "PatchBind:1": ["packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-accept/template.marko_1/valueChange", "TagVariableChange:count"]
--- packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/__snapshots__/patches.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/__snapshots__/patches.debug.js
@@ -15,7 +15,7 @@
   "PatchBind:1": ["packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/template.marko_0/onCount", "#childScope/3", _.a = "BranchScopes:#text/0", _.b = "TagVariableChange:count"],
   "PatchChild:#childScope/3": {
     "PatchBranch:#text/0": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/tags/counter/index.marko0": 0
       }
     }, _.c = "packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/tags/counter/index.marko_1_shell"]
@@ -23,7 +23,7 @@
   "PatchBind:2": ["packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/template.marko_0/onCount2", "#childScope/4", _.a, _.b],
   "PatchChild:#childScope/4": {
     "PatchBranch:#text/0": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-siblings/tags/counter/index.marko0": 0
       }
     }, _.c]
