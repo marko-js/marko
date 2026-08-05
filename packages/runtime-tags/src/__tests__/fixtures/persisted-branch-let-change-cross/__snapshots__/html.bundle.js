8502a1464347d72f504b0b31a05d674e22e057d0
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 18:57:00 -0700

    fix(runtime-tags): persisted construct wiring binds live scopes (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/__snapshots__/html.bundle.js
@@ -0,0 +1,28 @@
+// template.marko
+var template_default = _template_persisted("a", (input) => {
+	const $scope0_reason = _persisted_reason(), $sg__input_show = _serialize_guard($scope0_reason, 0);
+	const $scope0_id = _scope_id();
+	let last = 0;
+	let handler = _resume((next) => {
+		last = next;
+	}, "a0", $scope0_id);
+	_html(`<main><h1>${_patch_text($scope0_id, "a", input.title)}${_escape(input.title)}${_el_resume($scope0_id, "a")}</h1><p>Last <!>${_escape(last)}${_el_resume($scope0_id, "b")}</p>`);
+	_if(() => {
+		if (input.show) {
+			const $scope1_id = _scope_id();
+			let count = 0;
+			_html(`<span>Seen <!>${_escape(count)}${_el_resume($scope1_id, "a")}</span><button>+</button>${_el_resume($scope1_id, "b")}`);
+			_script($scope1_id, "a2");
+			_patch_value($scope1_id, "a0", count, 1);
+			_patch_bind($scope1_id, "d", handler || void 0);
+			writeScope($scope1_id, {
+				c: count,
+				d: handler || void 0
+			});
+			return 0;
+		}
+	}, $scope0_id, "c", $sg__input_show, $sg__input_show, $sg__input_show, void 0, void 0, [0]);
+	_html("</main>");
+	$scope0_reason && writeScope($scope0_id, { i: handler });
+	_resume_branch($scope0_id);
+}, 1);
---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/__snapshots__/patches.js
@@ -0,0 +1,12 @@
+{ta:"Store!",bc:[{f:{va0:0,wd:_._.a0}}]}
+
+
+// PATCH
+
+{ta:"Store!",bc:0}
+
+
+// PATCH
+
+{ta:"Store!",bc:[{f:{va0:0,wd:_._.a0}}]}
+
