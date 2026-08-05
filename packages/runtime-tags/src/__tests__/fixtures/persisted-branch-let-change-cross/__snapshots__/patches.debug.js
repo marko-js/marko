eb78ecb03a66a35b9405fd02a9a27e40cb7f37b2
Dylan Piercey <pierceydylan@gmail.com>
2026-08-04 18:57:00 -0700

    fix(runtime-tags): persisted construct wiring binds live scopes (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/__snapshots__/patches.debug.js
@@ -0,0 +1,12 @@
+{"PatchText:#text/0":"Store!","PatchBranch:#text/2":[{"PatchFresh:":{"PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/template.marko0":0,"PatchWrite:TagVariableChange:count":_._["packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/template.marko_0/handler"]}}]}
+
+
+// PATCH
+
+{"PatchText:#text/0":"Store!","PatchBranch:#text/2":0}
+
+
+// PATCH
+
+{"PatchText:#text/0":"Store!","PatchBranch:#text/2":[{"PatchFresh:":{"PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/template.marko0":0,"PatchWrite:TagVariableChange:count":_._["packages/runtime-tags/src/__tests__/fixtures/persisted-branch-let-change-cross/template.marko_0/handler"]}}]}
+
