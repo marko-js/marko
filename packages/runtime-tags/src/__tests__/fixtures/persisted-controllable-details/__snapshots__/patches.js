0edde61b5f4d08acbd8241d15303474a41caf640
Dylan Piercey <pierceydylan@gmail.com>
2026-08-05 12:11:05 -0700

    feat(runtime-tags): persisted patches for controllable native tags (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-controllable-details/__snapshots__/patches.js
@@ -0,0 +1,6 @@
+// PATCH
+{
+  ta: "Store!",
+  wEb: _._.a0,
+  n4b: !0
+}
---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-controllable-value/__snapshots__/html.bundle.debug.js
@@ -0,0 +1,7 @@
+// template.marko
+var template_default = _template_persisted("__tests__/template.marko", (input) => {
+	const $scope0_reason = _persisted_reason();
+	const $scope0_id = _scope_id();
+	_html(`<main><h1>${_patch_text($scope0_id, "#text/0", input.title)}${_escape(input.title)}${_el_resume($scope0_id, "#text/0")}</h1><input${_attr_input_value($scope0_id, "#input/1", input.value)}${_patch_control($scope0_id, "#input/1", 2, input.value)}>${_el_resume($scope0_id, "#input/1")}</main>`);
+	$scope0_reason && writeScope($scope0_id, {}, "__tests__/template.marko", 0);
+}, 1);
