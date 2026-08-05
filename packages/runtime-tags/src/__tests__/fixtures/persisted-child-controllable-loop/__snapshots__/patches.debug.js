79d8de3c19d71e6c77cee6b9dbad33be964c2d4c
Dylan Piercey <pierceydylan@gmail.com>
2026-08-05 15:29:42 -0700

    chore(runtime-tags): rename PatchFresh entries to PatchSetup (experimental)

--- packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/__snapshots__/patches.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/__snapshots__/patches.debug.js
@@ -6,7 +6,7 @@
     "PatchChild:#childScope/2": {
       "PatchBranch:#text/0": 0
     },
-    "PatchFresh:": {
+    "PatchSetup:": {
       "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/template.marko0": 0
     }
   }, {
@@ -14,7 +14,7 @@
     "PatchChild:#childScope/2": {
       "PatchBranch:#text/0": 0
     },
-    "PatchFresh:": {
+    "PatchSetup:": {
       "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/template.marko0": 0
     }
   }]
@@ -28,12 +28,12 @@
     "PatchBind:1": [_.a = "packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/template.marko_1/onCount", _.b = "#childScope/2", _.c = "BranchScopes:#text/0", _.d = "TagVariableChange:count"],
     "PatchChild:#childScope/2": {
       "PatchBranch:#text/0": [{
-        "PatchFresh:": {
+        "PatchSetup:": {
           "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/tags/counter/index.marko0": 0
         }
       }, _.e = "packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/tags/counter/index.marko_1_shell"]
     },
-    "PatchFresh:": {
+    "PatchSetup:": {
       "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/template.marko0": 0
     }
   }, {
@@ -41,12 +41,12 @@
     "PatchBind:2": [_.a, _.b, _.c, _.d],
     "PatchChild:#childScope/2": {
       "PatchBranch:#text/0": [{
-        "PatchFresh:": {
+        "PatchSetup:": {
           "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/tags/counter/index.marko0": 0
         }
       }, _.e]
     },
-    "PatchFresh:": {
+    "PatchSetup:": {
       "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-child-controllable-loop/template.marko0": 0
     }
   }]
--- packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/__snapshots__/patches.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/__snapshots__/patches.debug.js
@@ -3,7 +3,7 @@
   "PatchLoop:#ul/0": [1, {
     "PatchText:#text/0": "Fuji Apples",
     "PatchBranch:#text/1": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko0": 0
       }
     }, "packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko_2_shell"]
@@ -23,7 +23,7 @@
   "PatchLoop:#ul/0": [1, {
     "PatchText:#text/0": "Fuji Apples",
     "PatchBranch:#text/1": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko0": 0
       }
     }, "packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko_2_shell"]
@@ -35,14 +35,14 @@
   "PatchLoop:#ul/0": [2, {
     "PatchText:#text/0": "Milk",
     "PatchBranch:#text/1": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko0": 0
       }
     }, _.a = "packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko_2_shell"]
   }, 1, {
     "PatchText:#text/0": "Fuji Apples",
     "PatchBranch:#text/1": [{
-      "PatchFresh:": {
+      "PatchSetup:": {
         "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-loop-let-nested/template.marko0": 0
       }
     }, _.a]
