f6f596deac36b10cddfedfcacb66de7bb3f34d7c
Dylan Piercey <pierceydylan@gmail.com>
2026-08-06 10:50:24 -0700

    feat(runtime-tags): persisted fills deliver through composed branch chains (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/__snapshots__/patches.debug.js
@@ -0,0 +1,15 @@
+// PATCH
+[`packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko_1_shell;b%;<!><!><!>`, {
+  "PatchBranch:#text/0": [{
+    "PatchBranch:#text/0": 1
+  }, "packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko_1_shell"],
+  "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko0": "Store!"
+}]
+
+// PATCH
+[`packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko_1_shell;b%;<!><!><!>`, {
+  "PatchBranch:#text/0": [{
+    "PatchBranch:#text/0": 0
+  }, "packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko_1_shell"],
+  "PatchValue:packages/runtime-tags/src/__tests__/fixtures/persisted-branch-intersection-deep/template.marko0": "Store?"
+}]
---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-loop-intersection-deep/__snapshots__/dom.bundle.debug.js
@@ -0,0 +1,35 @@
+// template.marko
+const $template = "<main><!><button>+</button></main>";
+const $walks = "D%b l";
+const $for_content__input_suffix__OR__count__OR__item = /*@__PURE__*/ _fill_join("__tests__/template.marko0", "input_suffix", /*@__PURE__*/ _or(3, ($scope) => _text($scope["#text/0"], $scope.item + ":" + $scope._._.input_suffix + "@" + $scope._._.count), 2), ($join) => /*@__PURE__*/ _if_closure("#text/0", 0, /*@__PURE__*/ _for_closure("#text/0", $join)));
+const $for_content__input_suffix = /*@__PURE__*/ _closure_get("input_suffix", $for_content__input_suffix__OR__count__OR__item, ($scope) => $scope._._);
+const $for_content__setup = ($scope) => {
+	$for_content__input_suffix($scope);
+	$for_content__count($scope);
+};
+const $for_content__count = /*@__PURE__*/ _closure_get("count", $for_content__input_suffix__OR__count__OR__item, ($scope) => $scope._._);
+const $for_content__item = /*@__PURE__*/ _const("item", $for_content__input_suffix__OR__count__OR__item);
+const $for_content__$params = ($scope, $params2) => $for_content__item($scope, $params2[0]);
+const $if_content__for = /*@__PURE__*/ _for_of("#text/0", "<p> </p>", "D ", $for_content__setup, $for_content__$params);
+const $if_content__input_items = /*@__PURE__*/ _if_closure("#text/0", 0, ($scope) => $if_content__for($scope, [$scope._.input_items, (item) => item]));
+const $if_content__setup = $if_content__input_items;
+const $count__closure = /*@__PURE__*/ _closure($for_content__count);
+const $count = /*@__PURE__*/ _let("count/7", $count__closure);
+const $setup__script = _script("__tests__/template.marko_0", ($scope) => _on($scope["#button/1"], "click", function() {
+	$count($scope, $scope.count + 1);
+}));
+function $setup($scope) {
+	$count($scope, 0);
+	$setup__script($scope);
+}
+const $if = /*@__PURE__*/ _if("#text/0", "<!><!><!>", "b%", $if_content__setup);
+const $input_show = ($scope, input_show) => $if($scope, input_show ? 0 : 1);
+const $input = ($scope, input) => {
+	$input_show($scope, input.show);
+	$input_items($scope, input.items);
+	$input_suffix($scope, input.suffix);
+};
+const $input_items = /*@__PURE__*/ _const("input_items", $if_content__input_items);
+const $input_suffix__closure = /*@__PURE__*/ _closure($for_content__input_suffix);
+const $input_suffix = /*@__PURE__*/ _fill_const("__tests__/template.marko0", "input_suffix", $input_suffix__closure);
+var template_default = /*@__PURE__*/ _template("__tests__/template.marko", $template, $walks, $setup, $input);
