7803094a75fdbbfd0633e01970e10376da82ac0d
Dylan Piercey <pierceydylan@gmail.com>
2026-08-06 10:50:24 -0700

    feat(runtime-tags): persisted fills deliver through composed branch chains (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-loop-branch-intersection/__snapshots__/html.bundle.js
@@ -0,0 +1,35 @@
+// template.marko
+_renderer_shells({ a0: ",`a0;b%;<!><!><!>`" });
+var template_default = _template_persisted("a", (input) => {
+	const $scope0_owned = _persisted_ownership(), $scope0_reason = _persisted_reason(), $sg__input_flag = _source_guard($scope0_reason, 2);
+	const $scope0_id = _scope_id();
+	const $input_suffix__closures = /* @__PURE__ */ new Set();
+	const $count__closures = /* @__PURE__ */ new Set();
+	let count = 0;
+	_html("<main>");
+	_for_of(input.items, (item) => {
+		const $scope1_id = _scope_id();
+		_if(() => {
+			if (input.flag) {
+				const $scope2_id = _scope_id();
+				_html(`<p>${_escape(item + ":" + input.suffix + "@0")}${_el_resume($scope2_id, "a")}</p>`);
+				_subscribe($count__closures, _subscribe(_source_if($scope0_reason, 3) && $input_suffix__closures, writeScope($scope2_id, { _: _scope_with_id($scope1_id) })));
+				return 0;
+			}
+		}, $scope1_id, "a", 1, $sg__input_flag, $sg__input_flag, void 0, void 0, [0]);
+		writeScope($scope1_id, {
+			c: item,
+			_: _scope_with_id($scope0_id)
+		});
+	}, (item) => item, $scope0_id, "a", 1, _source_guard($scope0_reason, 0), _source_guard($scope0_reason, 1), void 0, void 0, "a0");
+	_html(`<button>+</button>${_el_resume($scope0_id, "b")}</main>`);
+	_script($scope0_id, "a2");
+	$scope0_reason ? writeScope($scope0_id, {
+		f: input.flag,
+		g: input.suffix,
+		h: count,
+		j: $input_suffix__closures,
+		k: $count__closures
+	}) : _owned_guard($scope0_owned, 3) && _patch_value($scope0_id, "a0", input.suffix);
+	_resume_branch($scope0_id);
+}, 1);
