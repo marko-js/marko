74761f112859ae5b70d2e92ceaa70eb8280eb9d6
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 10:21:56 -0700

    feat(runtime-tags): mixed selectors classify client-owned (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-branch-mixed-selector/__snapshots__/dom.bundle.js
@@ -0,0 +1,7 @@
+// template.marko
+const $if = /*@__PURE__*/ _if(0, "<p>big</p>");
+const $input_min__OR__count = /*@__PURE__*/ _fill_join("a0", 4, /*@__PURE__*/ _or(6, ($scope) => $if($scope, $scope.f > $scope.e ? 0 : 1)));
+const $count = /*@__PURE__*/ _let(5, $input_min__OR__count);
+const $setup__script = _script("a0", ($scope) => _on($scope.b, "click", function() {
+	$count($scope, $scope.f + 1);
+}));
