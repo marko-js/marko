9567e069d5057b2347da6bef42b560da4efd5e8c
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 11:03:02 -0700

    fix: error in debug builds when a for range from/step is a non-number

---
+++ packages/runtime-tags/src/__tests__/fixtures/error-for-from-non-number/__snapshots__/dom.bundle.debug.js
@@ -0,0 +1,13 @@
+// template.marko
+const $template = "<!><!><!>";
+const $walks = "b%c";
+const $setup = () => {};
+const $for_content__setup = ($scope) => _text($scope["#text/0"], $scope["#LoopKey"]);
+const $for = /*@__PURE__*/ _for_to("#text/0", "<li> </li>", "D ", $for_content__setup);
+const $input_from = ($scope, input_from) => $for($scope, [
+	5,
+	input_from,
+	1
+]);
+const $input = ($scope, input) => $input_from($scope, input.from);
+var template_default = /*@__PURE__*/ _template("__tests__/template.marko", $template, "b%c", $setup, $input);
