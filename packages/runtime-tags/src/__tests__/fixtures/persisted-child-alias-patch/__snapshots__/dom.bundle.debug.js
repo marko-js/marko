818ed0ba1e21d454d8e4b8f443993552320181b3
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 14:32:16 -0700

    fix(runtime-tags): pure-rest child inputs map feeders and declare before their alias

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-alias-patch/__snapshots__/dom.bundle.debug.js
@@ -0,0 +1,16 @@
+// tags/echo/index.marko
+const $template$1 = "<em> </em>";
+const $walks$1 = "D l";
+const $setup$1 = () => {};
+const $input_label$1 = ($scope, x_label) => _text($scope["#text/0"], x_label);
+const $input2 = ($scope, input) => $input_label$1($scope, input?.label);
+const $input$1 = $input2;
+var echo_default = /*@__PURE__*/ _template("__tests__/tags/echo/index.marko", $template$1, "D l", $setup$1, $input$1);
+
+// template.marko
+const $template = /*@__PURE__*/ ((_w0) => `<main>${_w0}</main>`)($template$1);
+const $walks = /*@__PURE__*/ ((_w0) => `D/${_w0}&l`)("D l");
+const $setup = () => {};
+const $input_label = ($scope, input_label) => $input$1($scope["#childScope/0"], { label: input_label });
+const $input = ($scope, input) => $input_label($scope, input.label);
+var template_default = /*@__PURE__*/ _template("__tests__/template.marko", $template, $walks, $setup, $input);
