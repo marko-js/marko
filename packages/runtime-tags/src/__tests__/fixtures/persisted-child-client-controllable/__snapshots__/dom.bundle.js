2a4a4ccbfc745559611e0b2d126349b306ebcd60
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 21:38:08 -0700

    fix(runtime-tags): reject server-derived change handlers feeding client-owned structure

--- packages/runtime-tags/src/__tests__/fixtures/persisted-child-client-controllable/__snapshots__/dom.bundle.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-client-controllable/__snapshots__/dom.bundle.js
@@ -1,7 +1,11 @@
 // tags/field/index.marko
-const $template = "<input>";
-const $v = /*@__PURE__*/ _let(1, ($scope) => _attr_input_value($scope, "a", $scope.b, $valueChange($scope)));
-const $setup__script$1 = _script("c1", ($scope) => _attr_input_value_script($scope, "a"));
+const $template = "<input><em> </em>";
+const $walks = " bD l";
+const $v = /*@__PURE__*/ _let(2, ($scope) => {
+	_attr_input_value($scope, "a", $scope.c, $valueChange($scope));
+	_text($scope.b, $scope.c);
+});
+const $setup__script$1 = _script("b1", ($scope) => _attr_input_value_script($scope, "a"));
 function $setup($scope) {
 	$v($scope, "");
 	$setup__script$1($scope);
@@ -11,13 +15,13 @@ function $valueChange($scope) {
 		$v($scope, _new_v);
 	};
 }
-_resume("c0", $valueChange);
+_resume("b0", $valueChange);
 
 // template.marko
 const $if_content__setup = ($scope) => {
 	$setup($scope.a);
 };
-const $if = /*@__PURE__*/ _if(0, $template, /*@__PURE__*/ ((_w0) => `/${_w0}&`)(" b"), $if_content__setup);
+const $if = /*@__PURE__*/ _if(0, $template, /*@__PURE__*/ ((_w0) => `/${_w0}&`)($walks), $if_content__setup);
 const $show = /*@__PURE__*/ _let(2, ($scope) => $if($scope, $scope.c ? 0 : 1));
 const $setup__script = _script("a0", ($scope) => _on($scope.b, "click", function() {
 	$show($scope, !$scope.c);
