5d853e105da61cdf1c5be7418b8fe243ba50dc74
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 17:45:54 -0700

    feat(runtime-tags): component trees render inside client-owned structure (experimental)

---
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-grandchild/__snapshots__/html.bundle.js
@@ -0,0 +1,37 @@
+// tags/widget/tags/inner/index.marko
+var inner_default = _template_persisted("c", (input) => {
+	_persisted_reason();
+	_scope_id();
+	_html("<em>leaf</em>");
+}, 0, 0);
+
+// tags/widget/index.marko
+var widget_default = _template_persisted("b", (input) => {
+	const $scope0_reason = _persisted_reason();
+	const $scope0_id = _scope_id();
+	const $childScope = _peek_scope_id();
+	_patch_child($scope0_id, "a", $childScope);
+	inner_default({});
+	$scope0_reason && writeScope($scope0_id, { a: _existing_scope($childScope) });
+}, 0, () => [inner_default]);
+
+// template.marko
+var template_default = _template_persisted("a", (input) => {
+	const $scope0_reason = _persisted_reason();
+	const $scope0_id = _scope_id();
+	let show = true;
+	_html("<main>");
+	if ($scope0_reason) _if(() => {
+		{
+			const $scope1_id = _scope_id();
+			const $childScope = _peek_scope_id();
+			widget_default({});
+			writeScope($scope1_id, { a: _existing_scope($childScope) });
+			return 0;
+		}
+	}, $scope0_id, "a", 1, 1, 1, 0, 1);
+	_html(`<button>t</button>${_el_resume($scope0_id, "b")}</main>`);
+	_script($scope0_id, "a0");
+	$scope0_reason && writeScope($scope0_id, { c: show });
+	_resume_branch($scope0_id);
+}, 1, () => [widget_default]);
