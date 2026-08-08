b853f0bb892fd9970e692a9025bd47104dd9b8e5
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 21:38:08 -0700

    fix(runtime-tags): reject server-derived change handlers feeding client-owned structure

--- packages/runtime-tags/src/__tests__/fixtures/persisted-child-client-controllable/__snapshots__/html.bundle.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-client-controllable/__snapshots__/html.bundle.js
@@ -1,14 +1,14 @@
 // tags/field/index.marko
-var field_default = _template_persisted("c", (input) => {
+var field_default = _template_persisted("b", (input) => {
 	const $scope0_reason = _persisted_reason();
 	const $scope0_id = _scope_id();
 	let v = "";
 	_html(`<input${_attr_input_value($scope0_id, "a", v, _resume((_new_v) => {
 		v = _new_v;
-	}, "c0", $scope0_id))}${_patch_bind($scope0_id, "Ea", _resume((_new_v) => {
+	}, "b0", $scope0_id))}${_patch_bind($scope0_id, "Ea", _resume((_new_v) => {
 		v = _new_v;
-	}, "c0", $scope0_id))}${_patch_control($scope0_id, "a", 2, v)}>${_el_resume($scope0_id, "a")}`);
-	_script($scope0_id, "c1");
+	}, "b0", $scope0_id))}${_patch_control($scope0_id, "a", 2, v)}>${_el_resume($scope0_id, "a")}<em>${_escape(v)}${_el_resume($scope0_id, "b")}</em>`);
+	_script($scope0_id, "b1");
 	$scope0_reason && writeScope($scope0_id, {});
 	_resume_branch($scope0_id);
 }, 0, 0);
@@ -27,7 +27,7 @@ var template_default = _template_persisted("a", (input) => {
 			writeScope($scope1_id, { a: _existing_scope($childScope) });
 			return 0;
 		}
-	}, $scope0_id, "a", 1, 1, 1, 0, 1);
+	}, $scope0_id, "a");
 	_html(`<button>t</button>${_el_resume($scope0_id, "b")}</main>`);
 	_script($scope0_id, "a0");
 	$scope0_reason && writeScope($scope0_id, { c: show });
