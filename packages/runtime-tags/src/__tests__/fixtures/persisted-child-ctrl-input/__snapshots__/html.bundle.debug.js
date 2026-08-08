5115ff9876a4a0aa45d528d44a496538b3a6e07b
Dylan Piercey <pierceydylan@gmail.com>
2026-08-07 21:38:08 -0700

    fix(runtime-tags): reject server-derived change handlers feeding client-owned structure

--- packages/runtime-tags/src/__tests__/fixtures/persisted-child-ctrl-input/__snapshots__/html.bundle.debug.js
+++ packages/runtime-tags/src/__tests__/fixtures/persisted-child-ctrl-input/__snapshots__/html.bundle.debug.js
@@ -18,7 +18,7 @@ var template_default = _template_persisted("__tests__/template.marko", (input) =
 	const $scope0_reason = _persisted_reason();
 	const $scope0_id = _scope_id();
 	let text = "hi";
-	let open = false;
+	let open = true;
 	_html("<main>");
 	if ($scope0_reason) _if(() => {
 		if (open) {
